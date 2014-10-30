003e1757e53560172477b9ae04bf03fc9cd777de
David Vorick <david.vorick@gmail.com>
2014-10-30 12:18:42 -0400

    remove unused code

--- sia/mine.go
+++ sia/mine.go
@@ -1,23 +1,8 @@
 package sia
 
 import (
-	"bytes"
-	"crypto/rand"
 	"time"
 )
-
-// Hashcash brute-forces a nonce that produces a hash less than target.
-func Hashcash(target Hash) (nonce []byte, i int) {
-	nonce = make([]byte, 8)
-	for {
-		i++
-		rand.Read(nonce)
-		h := HashBytes(nonce)
-		if bytes.Compare(h[:], target[:]) < 0 {
-			return
-		}
-	}
-}
 
 // Creates a new block.  This function creates a new block given a previous
 // block, isn't happy with being interrupted.  Need a different thread that can
