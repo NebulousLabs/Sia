dc5d55226c25400bcfa0caa6a169026b8bf68c41
David Vorick <david.vorick@gmail.com>
2014-10-31 21:39:21 -0400

    add a sign and verify function

--- sia/signatures.go
+++ sia/signatures.go
@@ -14,3 +14,14 @@ func GenerateKeyPair() (sk *ecdsa.PrivateKey, pk PublicKey, err error) {
 	pk = PublicKey(sk.PublicKey)
 	return
 }
+
+func SignBytes(data []byte, sk *ecdsa.PrivateKey) (sig Signature, err error) {
+	hash := HashBytes(data)
+	sig.R, sig.S, err = ecdsa.Sign(rand.Reader, sk, hash[:])
+	return
+}
+
+func VerifyBytes(data []byte, pubKey PublicKey, sig Signature) bool {
+	hash := HashBytes(data)
+	return ecdsa.Verify((*ecdsa.PublicKey)(&pubKey), hash[:], sig.R, sig.S)
+}
