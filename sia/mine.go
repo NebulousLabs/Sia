a8a40a88cbf25b94c06eaf39f1b1f94113972b7a
David Vorick <david.vorick@gmail.com>
2014-11-03 15:58:58 -0500

    clean up GenerateBlock()

--- sia/mine.go
+++ sia/mine.go
@@ -1,7 +1,6 @@
 package sia
 
 import (
-	"errors"
 	"time"
 )
 
@@ -11,6 +10,7 @@ const (
 	IterationsPerAttempt = 10 * 1000 * 1000
 )
 
+// Creates a block that is ready for nonce grinding.
 func (s *State) blockForWork(minerAddress CoinAddress) (b *Block, target Target) {
 	b = &Block{
 		ParentBlock:  s.ConsensusState.CurrentBlock,
@@ -30,32 +30,31 @@ func (s *State) blockForWork(minerAddress CoinAddress) (b *Block, target Target)
 	return
 }
 
-func solveBlock(b *Block, target Target) (err error) {
+// Tries to find a solution by increasing the nonce and checking the hash
+// repeatedly.
+func solveBlock(b *Block, target Target) bool {
 	for i := 0; i < IterationsPerAttempt; i++ {
 		if b.checkTarget(target) {
-			return
+			return true
 		}
 
 		b.Nonce++
 	}
 
-	err = errors.New("did not find winning block")
-	return
+	return false
 }
 
 // Creates a new block.  This function creates a new block given a previous
 // block, isn't happy with being interrupted.  Need a different thread that can
 // be updated by listening on channels or something.
 func (s *State) GenerateBlock(minerAddress CoinAddress) (b *Block) {
-	var target Target
-	err := errors.New("getting started")
-	for err != nil {
+	for {
 		s.Lock()
-		b, target = s.blockForWork(minerAddress)
+		b, target := s.blockForWork(minerAddress)
 		s.Unlock()
 
-		err = solveBlock(b, target)
+		if solveBlock(b, target) {
+			return b
+		}
 	}
-
-	return
 }
