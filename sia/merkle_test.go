f3f2ac8ccf0d757e664931db29e0b89807b97f2e
lukechampine <luke.champine@gmail.com>
2014-11-04 01:53:22 -0500

    add more MerkleRoot tests

--- sia/merkle_test.go
+++ sia/merkle_test.go
@@ -7,11 +7,20 @@ import (
 )
 
 func TestMerkleRoot(t *testing.T) {
-	// compare MerkleRoot fn to manual hashing of a 7-leaf tree
-	leaves := []Hash{Hash{0}, Hash{1}, Hash{2}, Hash{3}, Hash{4}, Hash{5}, Hash{6}}
+	// compare MerkleRoot fn to manual hashing
+	leaves := []Hash{Hash{0}, Hash{1}, Hash{2}, Hash{3}, Hash{4}, Hash{5}, Hash{6}, Hash{7}}
 
-	// calculate root manually
-	manualRoot := joinHash(
+	root8 := joinHash(
+		joinHash(
+			joinHash(leaves[0], leaves[1]),
+			joinHash(leaves[2], leaves[3]),
+		),
+		joinHash(
+			joinHash(leaves[4], leaves[5]),
+			joinHash(leaves[6], leaves[7]),
+		),
+	)
+	root7 := joinHash(
 		joinHash(
 			joinHash(leaves[0], leaves[1]),
 			joinHash(leaves[2], leaves[3]),
@@ -21,9 +30,32 @@ func TestMerkleRoot(t *testing.T) {
 			leaves[6],
 		),
 	)
+	root6 := joinHash(
+		joinHash(
+			joinHash(leaves[0], leaves[1]),
+			joinHash(leaves[2], leaves[3]),
+		),
+		joinHash(leaves[4], leaves[5]),
+	)
+	root5 := joinHash(
+		joinHash(
+			joinHash(leaves[0], leaves[1]),
+			joinHash(leaves[2], leaves[3]),
+		),
+		leaves[4],
+	)
 
-	if manualRoot != MerkleRoot(leaves) {
-		t.Fatal("MerkleRoot hash does not match manual hash")
+	if root8 != MerkleRoot(leaves) {
+		t.Fatal("MerkleRoot hash does not match manual hash (8 leaves)")
+	}
+	if root7 != MerkleRoot(leaves[:7]) {
+		t.Fatal("MerkleRoot hash does not match manual hash (7 leaves)")
+	}
+	if root6 != MerkleRoot(leaves[:6]) {
+		t.Fatal("MerkleRoot hash does not match manual hash (6 leaves)")
+	}
+	if root5 != MerkleRoot(leaves[:5]) {
+		t.Fatal("MerkleRoot hash does not match manual hash (5 leaves)")
 	}
 }
 
