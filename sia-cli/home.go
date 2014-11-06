a3fdd29825e7f75c5c37c84849aef0e464c05433
David Vorick <david.vorick@gmail.com>
2014-11-06 13:50:25 -0500

    add cli for sending coins to an address

--- sia-cli/home.go
+++ sia-cli/home.go
@@ -2,7 +2,46 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/NebulousLabs/Andromeda/sia"
 )
+
+// sendCoinsWalkthorugh uses the wallets in the environment to send coins to an
+// address that is provided through the command line.
+func sendCoinsWalkthrough(env *walletEnvironment) (err error) {
+	fmt.Println("Send Coins Walkthrough:")
+
+	fmt.Print("Amount to send: ")
+	var amount int
+	_, err = fmt.Scanln(&amount)
+	if err != nil {
+		return
+	}
+
+	fmt.Print("Amount to use as Miner Fee: ")
+	var minerFee int
+	_, err = fmt.Scanln(&minerFee)
+	if err != nil {
+		return
+	}
+
+	fmt.Print("Address of Receiving Wallet: ")
+	var addressBytes []byte
+	_, err = fmt.Scanf("%x", &addressBytes)
+	if err != nil {
+		return
+	}
+
+	// Convert the address to a sia.CoinAddress
+	var address sia.CoinAddress
+	copy(address[:], addressBytes)
+
+	// Use the wallet api to send. ==> Only uses wallets[0] for the time being.
+	fmt.Printf("Sending %v coins with miner fee of %v to address %x", amount, minerFee, address[:])
+	env.wallets[0].SpendCoins(sia.Currency(amount), sia.Currency(minerFee), address, env.state)
+
+	return
+}
 
 // displayHomeHelp lists all of the options available at the home screen, with
 // descriptions.
@@ -42,8 +81,13 @@ func pollHome(env *walletEnvironment) {
 					saveWalletEnvironmentWalkthorugh()
 			*/
 
-		case "s", "send"
-			sendCoinsWalkthrough()
+		case "s", "send":
+			err = sendCoinsWalkthrough(env)
+		}
+
+		if err != nil {
+			fmt.Println("Error:", err)
+			err = nil
 		}
 	}
 }
