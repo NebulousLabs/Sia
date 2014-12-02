32cccabda3fd0b25cf8bd719ab96f38ed1e2d2e3
David Vorick <david.vorick@gmail.com>
2014-12-02 03:29:32 -0500

    merge outputs.go with state.go

--- siacore/state.go
+++ siacore/state.go
@@ -209,6 +209,30 @@ func (s *State) CurrentTarget() Target {
 	return s.currentBlockNode().Target
 }
 
+// ScanOutputs takes a map of coin addresses as input and returns every output
+// in the set of unspent outputs that matches the list of addresses.
+func (s *State) ScanOutputs(addresses map[CoinAddress]struct{}) (outputIDs []OutputID) {
+	for id, output := range s.unspentOutputs {
+		if _, exists := addresses[output.SpendHash]; exists {
+			outputIDs = append(outputIDs, id)
+		}
+	}
+
+	return
+}
+
+// State.Output returns the Output associated with the id provided for input,
+// but only if the output is a part of the utxo set.
+func (s *State) Output(id OutputID) (output Output, err error) {
+	output, exists := s.unspentOutputs[id]
+	if exists {
+		return
+	}
+
+	err = errors.New("output not in utxo set")
+	return
+}
+
 // Sorted UtxoSet returns all of the unspent transaction outputs sorted
 // according to the numerical value of their id.
 func (s *State) SortedUtxoSet() (sortedOutputs []OutputID) {
--- siacore/transactionpool.go
+++ siacore/transactionpool.go
@@ -1,5 +1,29 @@
 package siacore
 
+// ScanOutputs takes a map of coin addresses as input and returns every output
+// in the set of unspent outputs that matches the list of addresses.
+func (s *State) ScanOutputs(addresses map[CoinAddress]struct{}) (outputIDs []OutputID) {
+	for id, output := range s.unspentOutputs {
+		if _, exists := addresses[output.SpendHash]; exists {
+			outputIDs = append(outputIDs, id)
+		}
+	}
+
+	return
+}
+
+// State.Output returns the Output associated with the id provided for input,
+// but only if the output is a part of the utxo set.
+func (s *State) Output(id OutputID) (output Output, err error) {
+	output, exists := s.unspentOutputs[id]
+	if exists {
+		return
+	}
+
+	err = errors.New("output not in utxo set")
+	return
+}
+
 // TransactionPoolDump() returns the list of transactions that are valid but
 // haven't yet appeared in a block.
 func (s *State) TransactionPoolDump() (transactions []Transaction) {
