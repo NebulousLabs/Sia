8111b865903253e1e494c65c2a2837ac375315c8
lukechampine <luke.champine@gmail.com>
2014-12-06 04:51:37 -0500

    implement some helper functions

--- siac/cmds.go
+++ siac/cmds.go
@@ -10,6 +10,36 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var hostname = "http://localhost:9980"
+
+// helper function for reading http GET responses
+func getResponse(handler string) string {
+	resp, err := http.Get(hostname + handler)
+	if err != nil {
+		return err.Error()
+	}
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err.Error()
+	}
+	return string(data)
+}
+
+// helper function for reading http POST responses
+func postResponse(handler string, vals url.Values) string {
+	resp, err := http.PostForm(hostname+handler, vals)
+	if err != nil {
+		return err.Error()
+	}
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err.Error()
+	}
+	return string(data)
+}
+
 func startcmd(cmd *cobra.Command, args []string) {
 	if len(args) != 0 {
 		fmt.Println(cmd.Usage())
@@ -25,7 +55,7 @@ func stopcmd(cmd *cobra.Command, args []string) {
 		fmt.Println(cmd.Usage())
 		return
 	}
-	http.Get("http://localhost:9980/stop")
+	getResponse("/stop")
 }
 
 func minecmd(cmd *cobra.Command, args []string) {
@@ -34,11 +64,7 @@ func minecmd(cmd *cobra.Command, args []string) {
 		return
 	}
 	// TODO: need start/stop
-	resp, err := http.Get("http://localhost:9980/mine")
-	resp.Body.Close()
-	if err != nil {
-		fmt.Println(err)
-	}
+	getResponse("/mine")
 }
 
 func sendcmd(cmd *cobra.Command, args []string) {
@@ -46,11 +72,11 @@ func sendcmd(cmd *cobra.Command, args []string) {
 		fmt.Println(cmd.Usage())
 		return
 	}
-	http.PostForm("http://localhost:9980/send", url.Values{
+	fmt.Println(postResponse("/send", url.Values{
 		"amount": {args[0]},
 		"fee":    {args[1]},
 		"dest":   {args[2]},
-	})
+	}))
 }
 
 func hostcmd(cmd *cobra.Command, args []string) {
@@ -58,12 +84,12 @@ func hostcmd(cmd *cobra.Command, args []string) {
 		fmt.Println(cmd.Usage())
 		return
 	}
-	http.PostForm("http://localhost:9980/host", url.Values{
+	fmt.Println(postResponse("/host", url.Values{
 		"MB":           {args[0]},
 		"price":        {args[1]},
 		"freezecoins":  {args[2]},
 		"freezeblocks": {args[3]},
-	})
+	}))
 }
 
 func rentcmd(cmd *cobra.Command, args []string) {
@@ -71,9 +97,9 @@ func rentcmd(cmd *cobra.Command, args []string) {
 		fmt.Println(cmd.Usage())
 		return
 	}
-	http.PostForm("http://localhost:9980/rent", url.Values{
+	fmt.Println(postResponse("/rent", url.Values{
 		"filename": {args[0]},
-	})
+	}))
 }
 
 func downloadcmd(cmd *cobra.Command, args []string) {
@@ -81,9 +107,9 @@ func downloadcmd(cmd *cobra.Command, args []string) {
 		fmt.Println(cmd.Usage())
 		return
 	}
-	http.PostForm("http://localhost:9980/download", url.Values{
+	fmt.Println(postResponse("/download", url.Values{
 		"filename": {args[0]},
-	})
+	}))
 }
 
 func savecmd(cmd *cobra.Command, args []string) {
@@ -91,9 +117,9 @@ func savecmd(cmd *cobra.Command, args []string) {
 		fmt.Println(cmd.Usage())
 		return
 	}
-	http.PostForm("http://localhost:9980/save", url.Values{
+	fmt.Println(postResponse("/save", url.Values{
 		"filename": {args[0]},
-	})
+	}))
 }
 
 func loadcmd(cmd *cobra.Command, args []string) {
@@ -101,10 +127,10 @@ func loadcmd(cmd *cobra.Command, args []string) {
 		fmt.Println(cmd.Usage())
 		return
 	}
-	http.PostForm("http://localhost:9980/load", url.Values{
+	fmt.Println(postResponse("/load", url.Values{
 		"filename":   {args[0]},
 		"friendname": {args[1]},
-	})
+	}))
 }
 
 func statuscmd(cmd *cobra.Command, args []string) {
@@ -112,16 +138,5 @@ func statuscmd(cmd *cobra.Command, args []string) {
 		fmt.Println(cmd.Usage())
 		return
 	}
-	resp, err := http.Get("http://localhost:9980/status")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer resp.Body.Close()
-	all, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(string(all))
-	}
+	fmt.Println(getResponse("/status"))
 }
