a7b5b43cf53eac8f1be6d6c3fad41d696b0740dc
David Vorick <david.vorick@gmail.com>
2014-12-15 15:44:48 -0500

    implement NewTransaction and RegisterTransaction

--- siacore/wallet/wallet.go
+++ siacore/wallet/wallet.go
@@ -1,6 +1,7 @@
 package wallet
 
 import (
+	"strconv"
 	"sync"
 
 	"github.com/NebulousLabs/Andromeda/consensus"
@@ -17,13 +18,16 @@ import (
 // things that need to do the lookups. (and type consensus.State would
 // implement the interface fulfilling that abstraction)
 type Wallet struct {
-	SecretKey       signatures.SecretKey
-	SpendConditions consensus.SpendConditions
+	secretKey       signatures.SecretKey
+	spendConditions consensus.SpendConditions
 
-	CoinsRemaining consensus.Currency
-	OwnedOutputs   map[consensus.OutputID]struct{}
-	SpentOutputs   map[consensus.OutputID]struct{}
-	OutputMap      map[consensus.OutputID]*consensus.Output
+	balance      consensus.Currency
+	ownedOutputs map[consensus.OutputID]struct{}
+	spentOutputs map[consensus.OutputID]struct{}
+	outputs      map[consensus.OutputID]*consensus.Output
+
+	transactionCounter int
+	transactions       map[string]*consensus.Transaction
 
 	sync.RWMutex
 }
@@ -36,14 +40,15 @@ func New() (w *Wallet, err error) {
 	}
 
 	w = &Wallet{
-		SecretKey: sk,
-		SpendConditions: consensus.SpendConditions{
+		secretKey: sk,
+		spendConditions: consensus.SpendConditions{
 			NumSignatures: 1,
 			PublicKeys:    []signatures.PublicKey{pk},
 		},
-		OwnedOutputs: make(map[consensus.OutputID]struct{}),
-		SpentOutputs: make(map[consensus.OutputID]struct{}),
-		OutputMap:    make(map[consensus.OutputID]*consensus.Output),
+		ownedOutputs: make(map[consensus.OutputID]struct{}),
+		spentOutputs: make(map[consensus.OutputID]struct{}),
+		outputs:      make(map[consensus.OutputID]*consensus.Output),
+		transactions: make(map[string]*consensus.Transaction),
 	}
 	return
 }
@@ -53,7 +58,7 @@ func (w *Wallet) Update(rewound []consensus.Block, applied []consensus.Block) er
 	w.Lock()
 	defer w.Unlock()
 
-	ca := w.SpendConditions.CoinAddress()
+	ca := w.spendConditions.CoinAddress()
 
 	// Remove all of the owned outputs created in the rewound blocks. Do not
 	// change the spent outputs map.
@@ -62,14 +67,14 @@ func (w *Wallet) Update(rewound []consensus.Block, applied []consensus.Block) er
 			// Remove all outputs that got created by this block.
 			for j, _ := range b.Transactions[i].Outputs {
 				id := b.Transactions[i].OutputID(j)
-				delete(w.OwnedOutputs, id)
+				delete(w.ownedOutputs, id)
 			}
 
 			// Re-add all inputs that got consumed by this block.
 			for _, input := range b.Transactions[i].Inputs {
 				if ca == input.SpendConditions.CoinAddress() {
-					w.CoinsRemaining += w.OutputMap[input.OutputID].Value
-					w.OwnedOutputs[input.OutputID] = struct{}{}
+					w.balance += w.outputs[input.OutputID].Value
+					w.ownedOutputs[input.OutputID] = struct{}{}
 				}
 			}
 		}
@@ -81,16 +86,16 @@ func (w *Wallet) Update(rewound []consensus.Block, applied []consensus.Block) er
 		for _, t := range b.Transactions {
 			// Remove all the outputs that got consumed by this block.
 			for _, input := range t.Inputs {
-				delete(w.OwnedOutputs, input.OutputID)
+				delete(w.ownedOutputs, input.OutputID)
 			}
 
 			// Add all of the outputs that got created by this block.
 			for i, output := range t.Outputs {
 				if ca == output.SpendHash {
 					id := t.OutputID(i)
-					w.OwnedOutputs[id] = struct{}{}
-					w.OutputMap[id] = &output
-					w.CoinsRemaining += output.Value
+					w.ownedOutputs[id] = struct{}{}
+					w.outputs[id] = &output
+					w.balance += output.Value
 				}
 			}
 		}
@@ -104,19 +109,41 @@ func (w *Wallet) Reset() error {
 	w.Lock()
 	defer w.Unlock()
 
-	for id := range w.SpentOutputs {
+	for id := range w.spentOutputs {
 		// Add the spent output back into the balance if it's currently an
 		// owned output.
-		if _, exists := w.OwnedOutputs[id]; exists {
-			w.CoinsRemaining += w.OutputMap[id].Value
+		if _, exists := w.ownedOutputs[id]; exists {
+			w.balance += w.outputs[id].Value
 		}
-		delete(w.SpentOutputs, id)
+		delete(w.spentOutputs, id)
 	}
 	return nil
 }
 
+// Balance implements the core.Wallet interface.
 func (w *Wallet) Balance() (consensus.Currency, error) {
 	w.RLock()
 	defer w.RUnlock()
-	return w.CoinsRemaining, nil
+	return w.balance, nil
+}
+
+// NewTransaction implements the core.Wallet interface.
+func (w *Wallet) NewTransaction() (id string, err error) {
+	w.Lock()
+	defer w.Unlock()
+
+	id = strconv.Itoa(w.transactionCounter)
+	w.transactionCounter++
+	w.transactions[id] = new(consensus.Transaction)
+	return
+}
+
+func (w *Wallet) RegisterTransaction(t *consensus.Transaction) (id string, err error) {
+	w.Lock()
+	defer w.Unlock()
+
+	id = strconv.Itoa(w.transactionCounter)
+	w.transactionCounter++
+	w.transactions[id] = t
+	return
 }
