43d0560bde076f25c0294f7b7f7eb76e8369b55b
David Vorick <david.vorick@gmail.com>
2014-12-18 17:29:33 -0500

    add tests for sending a transaction

--- siacore/mine_test.go
+++ siacore/mine_test.go
@@ -2,6 +2,8 @@ package siacore
 
 import (
 	"testing"
+
+	"github.com/NebulousLabs/Sia/consensus"
 )
 
 // mineSingleBlock mines a single block and then uses the blocking function
@@ -40,63 +42,30 @@ func testEmptyBlock(t *testing.T, e *Environment) {
 // testTransactionBlock creates a transaction and checks that it makes it into
 // the utxo set.
 func testTransactionBlock(t *testing.T, e *Environment) {
-
-}
-
-/*
-// testToggleMining tests that enabling and disabling mining works without
-// problems.
-func testToggleMining(te *testEnv) {
-	// Check that mining is not already enabled.
-	if te.e0.mining {
-		te.t.Error("Miner is already mining! - testToggleMining prereqs not met!")
+	if e.wallet.Balance(false) == 0 {
+		t.Error("e.wallet is empty.")
 		return
 	}
 
-	// Enable mining for a second, which should be more than enough to mine a
-	// block in the testing environment.
-	prevHeight := te.e0.Height()
-	te.e0.StartMining()
-	time.Sleep(300 * time.Millisecond)
-	te.e0.StopMining()
-	time.Sleep(300 * time.Millisecond)
-
-	// Test the height, wait another second (to allow an incorrectly running
-	// miner to mine another block) and test the height again.
-	newHeight := te.e0.Height()
-	if newHeight == prevHeight {
-		te.t.Error("height did not increase after mining for a second")
-	}
-	time.Sleep(300 * time.Millisecond)
-	if te.e0.Height() != newHeight {
-		te.t.Error("height still increasing after disabling mining...")
-	}
-}
-
-// testDualMining has both environments mine at the same time, and then
-// verifies that they maintain consistency.
-func testDualMining(te *testEnv) {
-	if te.e0.mining || te.e1.mining {
-		te.t.Error("one of the miners is already mining - testDualMining prereqs failed!")
+	// Send all coins to the `1` address.
+	dest := consensus.CoinAddress{1}
+	_, err := e.SpendCoins(e.wallet.Balance(false), dest)
+	if err != nil {
+		t.Error(err)
 		return
 	}
 
-	// Enable mining on each miner for long enough that each should mine
-	// multiple blocks. Then give the miners time to synchronize.
-	te.e0.StartMining()
-	te.e1.StartMining()
-	time.Sleep(300 * time.Millisecond)
-	te.e0.StopMining()
-	te.e1.StopMining()
-	time.Sleep(500 * time.Millisecond)
+	// Mine the block and see if the outputs moved.
+	mineSingleBlock(t, e)
+	sortedSet := e.state.SortedUtxoSet()
+	if len(sortedSet) != 2 {
+		t.Error("expecting sortedSet to be len 2, got", len(sortedSet))
+		return
+	}
 
-		// Compare the state hash for equality.
-		info0 := te.e0.StateInfo()
-		info1 := te.e1.StateInfo()
-
-		if info0.StateHash != info1.StateHash {
-			te.t.Error("state hashes do not match after dual mining.")
-			return
-		}
+	// At least one of the outputs should belong to address `1`.
+	if sortedSet[0].SpendHash != dest && sortedSet[1].SpendHash != dest {
+		t.Error("neither of the outputs is the correct output.")
+		return
+	}
 }
-*/
