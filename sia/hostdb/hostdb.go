fd67356ea7a2341de8868c0af6d9e214fe9cb85e
David Vorick <david.vorick@gmail.com>
2014-12-27 10:11:15 -0500

    uncommenct HostAnnouncement and HostEntry

--- sia/hostdb/hostdb.go
+++ sia/hostdb/hostdb.go
@@ -1,28 +1,27 @@
 package hostdb
 
-/*
 import (
-	"crypto/rand"
-	"errors"
+	// "crypto/rand"
+	// "errors"
 	"math"
-	"math/big"
-	"sync"
+	// "math/big"
+	// "sync"
 
 	"github.com/NebulousLabs/Sia/consensus"
-	"github.com/NebulousLabs/Sia/encoding"
+	// "github.com/NebulousLabs/Sia/encoding"
 	"github.com/NebulousLabs/Sia/network"
-	"github.com/NebulousLabs/Sia/sia"
+	// "github.com/NebulousLabs/Sia/sia"
 )
 
-const (
-	HostAnnouncementPrefix = uint64(1)
-)
-
+/*
+// Need to be easily able to swap hosts in and out of an active and inactive
+// list.
 type HostDatabase struct {
 	HostList    []HostEntry
 	TotalWeight consensus.Currency
 	sync.RWMutex
 }
+*/
 
 // A HostAnnouncement is a struct that can appear in the arbitrary data field.
 // It is preceded by 8 bytes that decode to the integer 1.
@@ -38,7 +37,7 @@ type HostAnnouncement struct {
 	MinTolerance       uint64
 	Price              consensus.Currency
 	Burn               consensus.Currency
-	CoinAddress        consensus.CoinAddress // Might be unneeded.
+	CoinAddress        consensus.CoinAddress // Host may want to give different addresses to each client.
 
 	SpendConditions consensus.SpendConditions
 	FreezeIndex     uint64 // The index of the output that froze coins.
@@ -60,17 +59,38 @@ type HostEntry struct {
 	CoinAddress consensus.CoinAddress
 }
 
+/*
 func CreateHostDatabase() (hdb *HostDatabase) {
 	hdb = new(HostDatabase)
 	return
 }
+*/
 
-// host.Weight() determines the weight of a specific host.
+// host.Weight() determines the weight of a specific host, which is:
+//
+//		Freeze * Burn / square(Price).
+//
+// Freeze has to be linear, because any non-linear freeze will invite sybil
+// attacks.
+//
+// For now, Burn is also linear because an increased burn means increased risk
+// for the host (Freeze on the other hand has no risk). It might be better to
+// make burn grow sublinearly, such as taking sqrt(Burn) or burn^(4/5).
+//
+// We take the square of the price to heavily emphasize hosts that have a low
+// price. This is also a bit simplistic however, because we're not sure what
+// the host might be charging for bandwidth.
 func (h *HostEntry) Weight() consensus.Currency {
-	adjustedBurn := math.Sqrt(float64(h.Burn))
-	return consensus.Currency(float64(h.Freeze) * adjustedBurn / float64(h.Price))
+	// adjustedBurn := math.Sqrt(float64(h.Burn))
+	adjustedBurn := float64(h.Burn)
+	adjustedFreeze := float64(h.Freeze)
+	adjustedPrice := math.Sqrt(float64(h.Price))
+
+	weight := adjustedFreeze * adjustedBurn / adjustedPrice
+	return consensus.Currency(weight)
 }
 
+/*
 // pullHostEntryFromArbitraryData is one of the most cleverly named functions
 // in the Galaxy. Any attempt to ridicule such a glorious name will result in
 // immediate deprication of all clothes. Needs to be under a hostdb and state
