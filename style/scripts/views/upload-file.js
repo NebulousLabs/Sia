2c96ff1b69f2d758893e91abea8bda1c11dd513d
seveibar <seveibar@gmail.com>
2015-01-16 20:46:05 -0500

    semi-functional file uploading

--- style/scripts/views/upload-file.js
+++ style/scripts/views/upload-file.js
@@ -1,9 +1,26 @@
 ui._uploadFile = ui["_upload-file"] = (function(){
 
+    var view;
+
     var privacyType; // "public" or "private"
 
     function init(){
-
+        view = $("#upload-file");
+        $(function(){
+            $("#fileupload").fileupload({
+                datatype: "plaintext",
+                add: function(e, data){
+                    console.log("File Added");
+                    view.find(".button.upload").off("click").click(function(){
+                        console.log("Attempting upload");
+                        data.submit();
+                    });
+                },
+                done: function(e, data){
+                    console.log("Upload Done");
+                }
+            });
+        });
     }
 
     function setPrivacy(_privacyType){
@@ -11,7 +28,6 @@ ui._uploadFile = ui["_upload-file"] = (function(){
     }
 
     function onViewOpened(data){
-
     }
 
 
