56391cf032eb11fd88e1af51694952f6996775f0
David Vorick <david.vorick@gmail.com>
2015-01-19 11:23:09 -0500

    add a constant for calculating the median timestamp

--- consensus/buildrelease.go
+++ consensus/buildrelease.go
@@ -9,12 +9,13 @@ import (
 // Though these are variables, they should never be changed during runtime.
 // They get altered during testing.
 var (
-	BlockSizeLimit  = 1024 * 1024 * 1024     // Blocks cannot be more than 1MB.
-	BlockFrequency  = Timestamp(600)         // In seconds.
-	TargetWindow    = BlockHeight(2000)      // Number of blocks to use when calculating the target.
-	FutureThreshold = Timestamp(3 * 60 * 60) // Seconds into the future block timestamps are valid.
-	RootTarget      = Target{0, 0, 0, 64}
-	RootDepth       = Target{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255}
+	BlockSizeLimit        = 1024 * 1024 * 1024     // Blocks cannot be more than 1MB.
+	BlockFrequency        = Timestamp(600)         // In seconds.
+	TargetWindow          = BlockHeight(2000)      // Number of blocks to use when calculating the target.
+	MedianTimestampWindow = 11                     // Number of blocks that get considered when determining if a timestamp is valid.
+	FutureThreshold       = Timestamp(3 * 60 * 60) // Seconds into the future block timestamps are valid.
+	RootTarget            = Target{0, 0, 0, 64}
+	RootDepth             = Target{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255}
 
 	MaxAdjustmentUp   = big.NewRat(1001, 1000)
 	MaxAdjustmentDown = big.NewRat(999, 1000)
