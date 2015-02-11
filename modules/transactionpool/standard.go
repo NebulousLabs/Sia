3c04220a590edb03b762314629951a6b06a4a366
David Vorick <david.vorick@gmail.com>
2015-02-11 05:28:11 -0500

    limit signature types with IsStandardTransaction

--- modules/transactionpool/standard.go
+++ modules/transactionpool/standard.go
@@ -11,18 +11,49 @@ const (
 	TransactionSizeLimit = 16 * 1024
 )
 
+// checkUnlockConditions looks at the UnlockConditions and verifies that all
+// public keys are recognized.
+func checkUnlockConditions(uc consensus.UnlockConditions) error {
+	for _, pk := range uc.PublicKeys {
+		if pk.Algorithm != consensus.SignatureEntropy &&
+			pk.Algorithm != consensus.SignatureEd25519 {
+			return errors.New("unrecognized key type in transaction")
+		}
+	}
+
+	return nil
+}
+
 // standard implements the rules outlined in Standard.md, and will return an
 // error if any of the rules are violated.
 func (tp *TransactionPool) IsStandardTransaction(t consensus.Transaction) (err error) {
 	// Check that the size of the transaction does not exceed the standard
 	// established in Standard.md
 	if len(encoding.Marshal(t)) > TransactionSizeLimit {
-		err = errors.New("transaction is too large")
-		return
+		return errors.New("transaction is too large")
 	}
 
-	// TODO: Check that the public keys in every transaction use only
-	// algorithms that are recognized by the consensus package.
+	// Check that all public keys are of a recognized type. Need to check all
+	// of the UnlockConditions, which currently can appear in 3 separate fields
+	// of the transaction.
+	for _, sci := range t.SiacoinInputs {
+		err = checkUnlockConditions(sci.UnlockConditions)
+		if err != nil {
+			return
+		}
+	}
+	for _, fct := range t.FileContractTerminations {
+		err = checkUnlockConditions(fct.TerminationConditions)
+		if err != nil {
+			return
+		}
+	}
+	for _, sfi := range t.SiafundInputs {
+		err = checkUnlockConditions(sfi.UnlockConditions)
+		if err != nil {
+			return
+		}
+	}
 
 	// TODO: Check that the arbitrary data is either prefixed with 'NonSia' or
 	// is prefixed with 'HostAnnouncement' plus follows rules for making a host
