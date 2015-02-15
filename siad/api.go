e3c40bdba439c932f7caba5c2b7120cbf6f3c2d6
seveibar <seveibar@gmail.com>
2015-02-15 17:05:09 -0500

    logging for all api requests

--- siad/api.go
+++ siad/api.go
@@ -9,41 +9,48 @@ import (
 
 const apiTimeout = 5e9 // 5 seconds
 
+func logWrapper(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		logger.Printf("%s %s", req.Method, req.URL)
+		f(w, req)
+	}
+}
+
 func (d *daemon) listen(addr string) {
 	mux := http.NewServeMux()
 
 	// Host API Calls
-	mux.HandleFunc("/host/config", d.hostConfigHandler)
-	mux.HandleFunc("/host/announce", d.hostAnnounceHandler)
-	mux.HandleFunc("/host/status", d.hostStatusHandler)
+	mux.HandleFunc("/host/config", logWrapper(d.hostConfigHandler))
+	mux.HandleFunc("/host/announce", logWrapper(d.hostAnnounceHandler))
+	mux.HandleFunc("/host/status", logWrapper(d.hostStatusHandler))
 
 	// Miner API Calls
-	mux.HandleFunc("/miner/start", d.minerStartHandler)
-	mux.HandleFunc("/miner/status", d.minerStatusHandler)
-	mux.HandleFunc("/miner/stop", d.minerStopHandler)
+	mux.HandleFunc("/miner/start", logWrapper(d.minerStartHandler))
+	mux.HandleFunc("/miner/status", logWrapper(d.minerStatusHandler))
+	mux.HandleFunc("/miner/stop", logWrapper(d.minerStopHandler))
 
 	// Wallet API Calls
-	mux.HandleFunc("/wallet/address", d.walletAddressHandler)
-	mux.HandleFunc("/wallet/send", d.walletSendHandler)
-	mux.HandleFunc("/wallet/status", d.walletStatusHandler)
+	mux.HandleFunc("/wallet/address", logWrapper(d.walletAddressHandler))
+	mux.HandleFunc("/wallet/send", logWrapper(d.walletSendHandler))
+	mux.HandleFunc("/wallet/status", logWrapper(d.walletStatusHandler))
 
 	// File API Calls
-	mux.HandleFunc("/file/upload", d.fileUploadHandler)
-	mux.HandleFunc("/file/uploadpath", d.fileUploadPathHandler)
-	mux.HandleFunc("/file/download", d.fileDownloadHandler)
-	mux.HandleFunc("/file/status", d.fileStatusHandler)
+	mux.HandleFunc("/file/upload", logWrapper(d.fileUploadHandler))
+	mux.HandleFunc("/file/uploadpath", logWrapper(d.fileUploadPathHandler))
+	mux.HandleFunc("/file/download", logWrapper(d.fileDownloadHandler))
+	mux.HandleFunc("/file/status", logWrapper(d.fileStatusHandler))
 
 	// Peer API Calls
-	mux.HandleFunc("/peer/add", d.peerAddHandler)
-	mux.HandleFunc("/peer/remove", d.peerRemoveHandler)
-	mux.HandleFunc("/peer/status", d.peerStatusHandler)
+	mux.HandleFunc("/peer/add", logWrapper(d.peerAddHandler))
+	mux.HandleFunc("/peer/remove", logWrapper(d.peerRemoveHandler))
+	mux.HandleFunc("/peer/status", logWrapper(d.peerStatusHandler))
 
 	// Misc. API Calls
-	mux.HandleFunc("/update/check", d.updateCheckHandler)
-	mux.HandleFunc("/update/apply", d.updateApplyHandler)
-	mux.HandleFunc("/status", d.statusHandler)
-	mux.HandleFunc("/stop", d.stopHandler)
-	mux.HandleFunc("/sync", d.syncHandler)
+	mux.HandleFunc("/update/check", logWrapper(d.updateCheckHandler))
+	mux.HandleFunc("/update/apply", logWrapper(d.updateApplyHandler))
+	mux.HandleFunc("/status", logWrapper(d.statusHandler))
+	mux.HandleFunc("/stop", logWrapper(d.stopHandler))
+	mux.HandleFunc("/sync", logWrapper(d.syncHandler))
 
 	// Debugging API Calls
 	mux.HandleFunc("/debug/constants", d.debugConstantsHandler)
--- siad/main.go
+++ siad/main.go
@@ -15,7 +15,7 @@ var (
 	config  Config
 	homeDir string
 	siaDir  string
-	logger  *Logger
+	logger  *log.Logger
 )
 
 type Config struct {
@@ -146,15 +146,15 @@ func main() {
 	root.PersistentFlags().StringVarP(&config.Siad.DownloadDirectory, "download-dir", "d", defaultDownloadDir, "location of downloaded files")
 	root.PersistentFlags().StringVarP(&config.Siad.WalletFile, "wallet-file", "w", defaultWalletFile, "location of the wallet file")
 
-	// Create a Logger for the Siad api
-	e, err := os.OpenFile("error.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	// Create a Logger for this package
+	logFile, err := os.OpenFile("info.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		fmt.Printf("error opening file: %v", err)
+		fmt.Printf("error opening log file: %v", err)
 		os.Exit(1)
 	}
-	defer e.Close()
-	errLog := log.New(e, ">>>", log.Ldate|log.Ltime)
-	errLog.Printf("TEST ERROR")
+	defer logFile.Close()
+	logger = log.New(logFile, ">>>", log.Ldate|log.Ltime)
+	logger.Printf("Logger Initialized")
 
 	// Load the config file, which will overwrite the default values.
 	if exists(config.Siad.ConfigFilename) {
