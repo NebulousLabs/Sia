21de12a6630d26f9b579e01128775b727abec140
David Vorick <david.vorick@gmail.com>
2015-02-16 19:00:17 -0500

    add applying fork testing

--- consensus/complete_test.go
+++ consensus/complete_test.go
@@ -134,34 +134,6 @@ func TestEverything(t *testing.T) {
 	s0 := CreateGenesisState(genesisTime)
 	s1 := CreateGenesisState(genesisTime)
 
-	// Verify that the genesis state creation is consistent.
-	if s0.stateHash() != s1.stateHash() {
-		t.Fatal("state hashes are different after calling CreateGenesisState")
-	}
-
-	// Get each on a separate fork.
-	b0, err := mineTestingBlock(s0.CurrentBlock().ID(), currentTime()-1, nullMinerPayouts(s0.Height()+1), nil, s0.CurrentTarget())
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = s0.AcceptBlock(b0)
-	if err != nil {
-		t.Fatal(err)
-	}
-	b1, err := mineTestingBlock(s1.CurrentBlock().ID(), currentTime(), nullMinerPayouts(s1.Height()+1), nil, s1.CurrentTarget())
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = s1.AcceptBlock(b1)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Verify that each state is on a separate fork.
-	if s0.stateHash() == s1.stateHash() {
-		t.Fatal("states have the same hash when they need to be in different places")
-	}
-
 	// Call orderedTestBattery on each state.
 	orderedTestBattery(t, s0, s1)
 
--- consensus/fork_test.go
+++ consensus/fork_test.go
@@ -4,9 +4,9 @@ import (
 	"testing"
 )
 
-// TestRewindingFork creates a situation where multiple blocks need to be
-// removed during a fork.
-func TestRewindingFork(t *testing.T) {
+// TestComplexForking creates multiple states and sets up multiple forking
+// scenarios between them to check consistency during forking.
+func TestComplexForking(t *testing.T) {
 	if testing.Short() {
 		t.Skip()
 	}
@@ -16,15 +16,21 @@ func TestRewindingFork(t *testing.T) {
 	time := CurrentTime()
 	s1 := createGenesisState(time, ZeroUnlockHash, ZeroUnlockHash)
 	s2 := createGenesisState(time, ZeroUnlockHash, ZeroUnlockHash)
+	s3 := createGenesisState(time, ZeroUnlockHash, ZeroUnlockHash)
 	a1 := NewAssistant(t, s1)
 	a2 := NewAssistant(t, s2)
+	a3 := NewAssistant(t, s3)
 
-	// Verify that the two states have the same initial hash.
+	// Verify that the three states have the same initial hash.
 	if s1.StateHash() != s2.StateHash() {
 		t.Fatal("starting states have different hashes - can't run test")
 	}
+	if s1.StateHash() != s3.StateHash() {
+		t.Fatal("starting states have different hashes - can't run test")
+	}
 
-	// Get each state on a different fork.
+	// Get state1 and state2 on different forks, s3 will follow s1 at this
+	// point.
 	b1, err := MineTestingBlock(s1.CurrentBlock().ID(), time, a1.Payouts(s1.Height()+1, nil), nil, s1.CurrentTarget())
 	if err != nil {
 		t.Fatal(err)
@@ -44,6 +50,10 @@ func TestRewindingFork(t *testing.T) {
 	if s1.StateHash() == s2.StateHash() {
 		t.Fatal("failed to get states on different forks")
 	}
+	err = s3.AcceptBlock(b1)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	// Mine several blocks on each state.
 	for i := 0; i < 3; i++ {
@@ -53,6 +63,10 @@ func TestRewindingFork(t *testing.T) {
 			t.Fatal(err)
 		}
 		err = s1.AcceptBlock(b1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = s3.AcceptBlock(b1)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -69,7 +83,7 @@ func TestRewindingFork(t *testing.T) {
 	}
 
 	// Put state2 ahead of state1 and then give all of the state2 blocks to
-	// state1, causing state1 to fork.
+	// state1, causing state1 to fork. State3 is left alone.
 	for i := 0; i < 3; i++ {
 		b2, err = a2.MineCurrentBlock(nil)
 		if err != nil {
@@ -91,8 +105,40 @@ func TestRewindingFork(t *testing.T) {
 		}
 	}
 
-	// State1 hash should not match state2 hash.
+	// State1 hash should match state2 hash.
 	if s1.StateHash() != s2.StateHash() {
 		t.Fatal("hashes don't match after trying to force a rewinding fork")
+	}
+
+	// Put state3 ahead of state 1&2 on state1's original block path. Then feed
+	// all of state 3's blocks to state1, which will cause state1 to fork to
+	// state3. State1 will be applying diffs for many of the blocks instead of
+	// generating them, which means a different codepath will be followed from
+	// the previous fork.
+	s3InitialHeight := s3.Height()
+	for i := 0; i < 6; i++ {
+		b3, err := a3.MineCurrentBlock(nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = s3.AcceptBlock(b3)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	for i := s3InitialHeight + 1; i <= s3.Height(); i++ {
+		b, exists := s3.BlockAtHeight(i)
+		if !exists {
+			t.Fatal("error when moving blocks from s3 to s1")
+		}
+		err = s1.AcceptBlock(b)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// State1 hash should match state3 hash.
+	if s1.StateHash() != s3.StateHash() {
+		t.Fatal("hashes don't match after trying to force an applying fork")
 	}
 }
