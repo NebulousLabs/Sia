2cda83b80bb1b451be2ce8186a56a283ab1592f8
David Vorick <david.vorick@gmail.com>
2015-02-20 03:54:44 -0500

    embed transaction pool testing parts

--- modules/transactionpool/accept_test.go
+++ modules/transactionpool/accept_test.go
@@ -6,29 +6,25 @@ import (
 	"github.com/NebulousLabs/Sia/consensus"
 )
 
-// TODO: Check that each component of apply works correctly. Then check that a
-// blockchain rewind/fork correctly handles removing transactions and dealing
-// with conflicts, dependents, and adding existing transacitons.
-
 // addSiacoinTransactionToPool creates a transaction with a single siacoin
 // input and output and adds it to the transcation pool, returning the
 // transaction that was created and added.
-func (tpt *tpoolTester) addSiacoinTransactionToPool() (txn consensus.Transaction) {
+func (tpt *TpoolTester) addSiacoinTransactionToPool() (txn consensus.Transaction) {
 	// Add a siacoin input to the transaction.
-	siacoinInput, value := tpt.assistant.FindSpendableSiacoinInput()
-	txn = tpt.assistant.AddSiacoinInputToTransaction(consensus.Transaction{}, siacoinInput)
+	siacoinInput, value := tpt.FindSpendableSiacoinInput()
+	txn = tpt.AddSiacoinInputToTransaction(consensus.Transaction{}, siacoinInput)
 
 	// Add a siacoin output to the transaction.
 	sco := consensus.SiacoinOutput{
 		Value:      value,
-		UnlockHash: tpt.assistant.UnlockHash,
+		UnlockHash: tpt.UnlockHash,
 	}
 	txn.SiacoinOutputs = append(txn.SiacoinOutputs, sco)
 
 	// Put the transaction into the transaction pool.
-	err := tpt.transactionPool.AcceptTransaction(txn)
+	err := tpt.AcceptTransaction(txn)
 	if err != nil {
-		tpt.assistant.Error(err)
+		tpt.Error(err)
 	}
 
 	return
@@ -37,33 +33,33 @@ func (tpt *tpoolTester) addSiacoinTransactionToPool() (txn consensus.Transaction
 // addDependentSiacoinTransactionToPool adds a transaction to the pool with a
 // siacoin output, and then adds a second transaction to the pool that requires
 // the unconfirmed siacoin output.
-func (tpt *tpoolTester) addDependentSiacoinTransactionToPool() (firstTxn, dependentTxn consensus.Transaction) {
+func (tpt *TpoolTester) addDependentSiacoinTransactionToPool() (firstTxn, dependentTxn consensus.Transaction) {
 	// Grab the first transaction and then create a second transaction.
 	firstTxn = tpt.addSiacoinTransactionToPool()
 	dependentTxn = consensus.Transaction{}
 	sci := consensus.SiacoinInput{
 		ParentID:         firstTxn.SiacoinOutputID(0),
-		UnlockConditions: tpt.assistant.UnlockConditions,
+		UnlockConditions: tpt.UnlockConditions,
 	}
-	dependentTxn = tpt.assistant.AddSiacoinInputToTransaction(dependentTxn, sci)
+	dependentTxn = tpt.AddSiacoinInputToTransaction(dependentTxn, sci)
 	dependentTxn.MinerFees = append(dependentTxn.MinerFees, firstTxn.SiacoinOutputs[0].Value)
 
-	err := tpt.transactionPool.AcceptTransaction(dependentTxn)
+	err := tpt.AcceptTransaction(dependentTxn)
 	if err != nil {
-		tpt.assistant.Error(err)
+		tpt.Error(err)
 	}
 
 	return
 }
 
-// TestAddSiacoinTransactionToPool creates a tpoolTester and uses it to call
+// TestAddSiacoinTransactionToPool creates a TpoolTester and uses it to call
 // addSiacoinTransactionToPool.
 func TestAddSiacoinTransactionToPool(t *testing.T) {
 	tpt := CreateTpoolTester(t)
 	tpt.addSiacoinTransactionToPool()
 }
 
-// TestAddDependentSiacoinTransactionToPool creates a tpoolTester and uses it
+// TestAddDependentSiacoinTransactionToPool creates a TpoolTester and uses it
 // to cal addDependentSiacoinTransactionToPool.
 func TestAddDependentSiacoinTransactionToPool(t *testing.T) {
 	tpt := CreateTpoolTester(t)
--- modules/transactionpool/dump_test.go
+++ modules/transactionpool/dump_test.go
@@ -7,26 +7,26 @@ import (
 // testTransactionDumping calls TransactionSet and puts the transactions in a
 // block that gets submitted to the state. If there is an error, the
 // transaction set is known to be invalid.
-func (tpt *tpoolTester) testTransactionDumping() {
+func (tpt *TpoolTester) testTransactionDumping() {
 	// Get the transaction set.
-	tset, err := tpt.transactionPool.TransactionSet()
+	tset, err := tpt.TransactionSet()
 	if err != nil {
-		tpt.assistant.Error(err)
+		tpt.Error(err)
 	}
 
 	// Add the transaction set to a block and check that it is valid in the
 	// state by adding it to the state.
-	b := tpt.assistant.MineCurrentBlock(tset)
-	err = tpt.assistant.State.AcceptBlock(b)
+	b := tpt.MineCurrentBlock(tset)
+	err = tpt.State.AcceptBlock(b)
 	if err != nil {
-		tpt.assistant.Error(err)
+		tpt.Error(err)
 	}
 }
 
 // testSiacoinTransactionDump adds a handful of siacoin transactions to the
 // transaction pool and then runs testTransactionDumping to see that the pool
 // set follows the rules of the blockchain.
-func (tpt *tpoolTester) testSiacoinTransactionDump() {
+func (tpt *TpoolTester) testSiacoinTransactionDump() {
 	tpt.addDependentSiacoinTransactionToPool()
 	tpt.testTransactionDumping()
 }
@@ -34,22 +34,22 @@ func (tpt *tpoolTester) testSiacoinTransactionDump() {
 // testUnconfirmedSiacoinOutputDiffs adds some unconfirmed transactions to the
 // transaction pool and then checks the diffs. Then a block is put into the
 // state with the transactions and the diffs are checked again.
-func (tpt *tpoolTester) testUnconfirmedSiacoinOutputDiffs() {
+func (tpt *TpoolTester) testUnconfirmedSiacoinOutputDiffs() {
 	tpt.addDependentSiacoinTransactionToPool()
-	diffs := tpt.transactionPool.UnconfirmedSiacoinOutputDiffs()
+	diffs := tpt.UnconfirmedSiacoinOutputDiffs()
 	if len(diffs) != 3 {
-		tpt.assistant.Error("wrong number of diffs")
+		tpt.Error("wrong number of diffs")
 	}
 }
 
-// TestSiacoinTransactionDump creates a tpoolTester and uses it to call
+// TestSiacoinTransactionDump creates a TpoolTester and uses it to call
 // testSiacoinTransactionDump.
 func TestSiacoinTransactionDump(t *testing.T) {
 	tpt := CreateTpoolTester(t)
 	tpt.testSiacoinTransactionDump()
 }
 
-// TestUnconfirmedSiacoinOutputDiffs creates a tpoolTester and uses it to call
+// TestUnconfirmedSiacoinOutputDiffs creates a TpoolTester and uses it to call
 // testUnconfirmedSiacoinOutputDiffs.
 func TestUnconfirmedSiacoinOutputDiffs(t *testing.T) {
 	tpt := CreateTpoolTester(t)
