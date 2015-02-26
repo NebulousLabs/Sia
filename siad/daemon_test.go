97e98a49c113be637f21046cf9ca8bec718b42cb
lukechampine <luke.champine@gmail.com>
2015-02-26 18:08:15 -0500

    create daemonTester

--- siad/daemon_test.go
+++ siad/daemon_test.go
@@ -1,6 +1,18 @@
 package main
 
-func testingDaemon() (d *daemon, err error) {
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
+
+type daemonTester struct {
+	*daemon
+	*testing.T
+}
+
+func newDaemonTester(t *testing.T) *daemonTester {
 	dc := DaemonConfig{
 		APIAddr: ":9020",
 		RPCAddr: ":9021",
@@ -14,11 +26,54 @@ func testingDaemon() (d *daemon, err error) {
 		WalletDir: "walletDir",
 	}
 
-	d, err = newDaemon(dc)
+	d, err := newDaemon(dc)
 	if err != nil {
-		return
+		t.Fatal("Could not create daemon:", err)
 	}
+	go func() {
+		listenErr := d.listen()
+		if listenErr != nil {
+			t.Fatal("API server quit:", listenErr)
+		}
+	}()
 
-	go d.listen(dc.APIAddr)
+	return &daemonTester{d, t}
+}
+
+// get wraps a GET request with a status code check, such that if the GET does
+// not return 200, the error will be read and returned. The response body is
+// not closed.
+func (dt *daemonTester) get(call string) (resp *http.Response) {
+	resp, err := http.Get("http://localhost" + dt.apiServer.Addr + call)
+	if err != nil {
+		dt.Fatalf("GET %s failed: %v", call, err)
+	}
+	// check error code
+	if resp.StatusCode != http.StatusOK {
+		errResp, _ := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		dt.Fatalf("GET %s returned error %v: %v", resp.StatusCode, errResp)
+	}
 	return
+}
+
+// getAPI makes an API call and decodes the response.
+func (dt *daemonTester) getAPI(call string, obj interface{}) {
+	resp := dt.get(call)
+	defer resp.Body.Close()
+	err := json.NewDecoder(resp.Body).Decode(obj)
+	if err != nil {
+		dt.Fatalf("Could not decode API response: %s", call)
+	}
+	return
+}
+
+// callAPI makes an API call and discards the response.
+func (dt *daemonTester) callAPI(call string) {
+	dt.get(call).Body.Close()
+}
+
+// TestCreateDaemon creates a daemonTester and immediately stops it.
+func TestCreateDaemon(t *testing.T) {
+	newDaemonTester(t).callAPI("/stop")
 }
