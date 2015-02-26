9aeac213f09438e65a38b568ab3e51fb914c700d
lukechampine <luke.champine@gmail.com>
2015-02-25 20:32:08 -0500

    implement host save/load

--- modules/host/host_test.go
+++ modules/host/host_test.go
@@ -56,8 +56,16 @@ func CreateHostTester(t *testing.T) (ht *HostTester) {
 	return
 }
 
-// TestCreateHostTester is a temporary function to call CreateHostTester during
-// testing.
-func TestCreateHostTester(t *testing.T) {
-	_ = CreateHostTester(t)
+// TestSaveLoad tests that saving and loading a Host restores its data.
+// TODO: expand this once Host testing is fleshed out.
+func TestSaveLoad(t *testing.T) {
+	ht := CreateHostTester(t)
+	err := ht.save("../../hostdata_test")
+	if err != nil {
+		ht.Fatal(err)
+	}
+	err = ht.load("../../hostdata_test")
+	if err != nil {
+		ht.Fatal(err)
+	}
 }
