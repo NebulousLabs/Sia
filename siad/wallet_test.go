f4a9813f331ad9ebce59a17edf472747b6370bae
lukechampine <luke.champine@gmail.com>
2015-02-27 23:57:10 -0500

    add siad wallet test

--- siad/wallet_test.go
+++ siad/wallet_test.go
@@ -1,59 +1,38 @@
 package main
 
-/*
 import (
 	"testing"
 
-	"github.com/NebulousLabs/Sia/consensus"
+	"github.com/NebulousLabs/Sia/modules"
 )
 
-// testSendToSelf does a send from the wallet to itself, and checks that all of
-// the balance reporting at each step makes sense, and then checks that all of
-// the coins are still sendable.
-func testSendToSelf(t *testing.T, c *Core) {
-	if c.wallet.Balance(false) == 0 {
-		t.Error("c.wallet is empty.")
-		return
+func (dt *daemonTester) testSendCoins() {
+	// get current balance
+	var oldstatus modules.WalletInfo
+	dt.getAPI("/wallet/status", &oldstatus)
+	// get a new address
+	var addr struct {
+		Address string
 	}
-	originalBalance := c.wallet.Balance(false)
-
-	// Get a new coin address from the wallet and send the coins to yourself.
-	dest, _, err := c.wallet.CoinAddress()
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	_, err = c.wallet.SpendCoins(c.wallet.Balance(false)-10, dest)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	// Process the transaction and check the balance, which should now be 0.
-	//
-	// TODO: This error checking is hacky, instead should use some sort of
-	// synchronization technique.
-	if c.wallet.Balance(false) != 0 {
-		t.Error("Expecting a balance of 0, got", c.wallet.Balance(false))
-	}
-
-	// Mine the block and check the balance, which should now be
-	// originalBalance + Coinbase.
-	// mineSingleBlock(t, c)
-	if c.wallet.Balance(false) != originalBalance+consensus.CalculateCoinbase(c.state.Height()) {
-		t.Errorf("Expecting a balance of %v, got %v", originalBalance+consensus.CalculateCoinbase(c.state.Height()), c.wallet.Balance(false))
-	}
-	if c.wallet.Balance(false) != c.wallet.Balance(true) {
-		t.Errorf("Expecting balance and full balance to be equal, but instead they are false: %v, full: %v", c.wallet.Balance(false), c.wallet.Balance(true))
+	dt.getAPI("/wallet/address", &addr)
+	// send 3e4 coins to the address
+	dt.callAPI("/wallet/send?amount=30000&dest=" + addr.Address)
+	// get updated balance
+	var newstatus modules.WalletInfo
+	dt.getAPI("/wallet/status", &newstatus)
+	// compare balances
+	// TODO: need a better way to test this
+	if newstatus.FullBalance.Cmp(oldstatus.FullBalance) != 0 {
+		dt.Fatal("Balance should not have changed:\n\told: %v\n\tnew: %v", newstatus.FullBalance.Big(), oldstatus.FullBalance.Big())
 	}
 }
 
-// testWalletInfo calles wallet.Info to see if an error is thrown. Also make sure
-// there is no deadlock.
-func testWalletInfo(t *testing.T, c *Core) {
-	_, err := c.wallet.WalletInfo()
-	if err != nil {
-		t.Error(err)
-	}
+// TestSendCoins creates two addresses and sends coins from one to the other.
+// The first balance should decrease, and the second balance should increase
+// proportionally.
+func TestSendCoins(t *testing.T) {
+	dt := newDaemonTester(t)
+	// need to mine a few coins first
+	dt.testMining()
+	dt.testSendCoins()
 }
-*/
