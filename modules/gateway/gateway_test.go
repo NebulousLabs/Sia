293b50027679a86bb1dd884c616925e38b41f222
lukechampine <luke.champine@gmail.com>
2015-03-05 04:15:01 -0500

    update gateway tests

---
+++ modules/gateway/gateway_test.go
@@ -0,0 +1,74 @@
+package gateway
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/NebulousLabs/Sia/consensus"
+	"github.com/NebulousLabs/Sia/modules"
+)
+
+var port int = 9001
+
+func newTestGateway(t *testing.T) *Gateway {
+	g, err := New(":"+strconv.Itoa(port), consensus.CreateGenesisState())
+	if err != nil {
+		t.Fatal(err)
+	}
+	port++
+	return g
+}
+
+func TestTableTennis(t *testing.T) {
+	g := newTestGateway(t)
+	if !g.Ping(g.myAddr) {
+		t.Fatal("gateway did not respond to ping")
+	}
+}
+
+func TestRPC(t *testing.T) {
+	g := newTestGateway(t)
+
+	g.RegisterRPC("Foo", func(conn modules.NetConn) error {
+		var i uint64
+		err := conn.ReadObject(&i, 8)
+		if err != nil {
+			t.Error(err)
+			return err
+		} else if i == 0xdeadbeef {
+			return conn.WriteObject("foo")
+		} else {
+			return conn.WriteObject("bar")
+		}
+	})
+
+	var foo string
+	err := g.RPC(g.myAddr, "Foo", func(conn modules.NetConn) error {
+		err := conn.WriteObject(0xdeadbeef)
+		if err != nil {
+			return err
+		}
+		return conn.ReadObject(&foo, 11)
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if foo != "foo" {
+		t.Fatal("Foo gave wrong response:", foo)
+	}
+
+	// wrong number should produce an error
+	err = g.RPC(g.myAddr, "Foo", func(conn modules.NetConn) error {
+		err := conn.WriteObject(0xbadbeef)
+		if err != nil {
+			return err
+		}
+		return conn.ReadObject(&foo, 11)
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if foo != "bar" {
+		t.Fatal("Foo gave wrong response:", foo)
+	}
+}
--- modules/gateway/peer_test.go
+++ modules/gateway/peer_test.go
@@ -3,57 +3,44 @@ package gateway
 import (
 	"testing"
 
-	"github.com/NebulousLabs/Sia/consensus"
-	"github.com/NebulousLabs/Sia/network"
+	"github.com/NebulousLabs/Sia/modules"
 )
 
 func TestPeerSharing(t *testing.T) {
-	// create server
-	tcps, err := network.NewTCPServer(":9002")
-	if err != nil {
-		t.Fatal(err)
-	}
-	myAddr := tcps.Address()
-	g, err := New(tcps, consensus.CreateGenesisState())
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// register SharePeers RPC
-	tcps.RegisterRPC("SharePeers", g.SharePeers)
+	g := newTestGateway(t)
 
 	// add a peer
-	peer := network.Address("foo:9001")
+	peer := modules.NetAddress("foo:9001")
 	g.addPeer(peer)
 	// gateway only has one peer, so randomPeer should return peer
 	if p, err := g.randomPeer(); err != nil || p != peer {
-		t.Fatal("server has bad peer list:", g.Info())
+		t.Fatal("gateway has bad peer list:", g.Info())
 	}
 
-	// ask tcps for peers
-	var resp []network.Address
-	err = myAddr.RPC("SharePeers", nil, &resp)
+	// ask gateway for peers
+	var peers []modules.NetAddress
+	err := g.RPC(g.myAddr, "SharePeers", modules.ReaderRPC(&peers, 1024))
 	if err != nil {
 		t.Fatal(err)
 	}
-	// resp should be exactly []Address{peer}
-	if len(resp) != 1 || resp[0] != peer {
-		t.Fatal("server gave bad peer list:", resp)
+	// response should be exactly []Address{peer}
+	if len(peers) != 1 || peers[0] != peer {
+		t.Fatal("gateway gave bad peer list:", peers)
 	}
 
 	// add a couple more peers
-	g.addPeer(network.Address("bar:9002"))
-	g.addPeer(network.Address("baz:9003"))
-	g.addPeer(network.Address("quux:9004"))
-	err = myAddr.RPC("SharePeers", nil, &resp)
+	g.addPeer("bar:9002")
+	g.addPeer("baz:9003")
+	g.addPeer("quux:9004")
+	err = g.RPC(g.myAddr, "SharePeers", modules.ReaderRPC(&peers, 1024))
 	if err != nil {
 		t.Fatal(err)
 	}
-	// resp should now contain 4 distinct addresses
-	for i := 0; i < len(resp); i++ {
-		for j := i + 1; j < len(resp); j++ {
-			if resp[i] == resp[j] {
-				t.Fatal("resp contains duplicate addresses:", resp)
+	// peers should now contain 4 distinct addresses
+	for i := 0; i < len(peers); i++ {
+		for j := i + 1; j < len(peers); j++ {
+			if peers[i] == peers[j] {
+				t.Fatal("gateway gave duplicate addresses:", peers)
 			}
 		}
 	}
