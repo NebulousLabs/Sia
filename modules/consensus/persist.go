48315dc0be77ce631365847d02d7ebe0a82d8ea9
lukechampine <luke.champine@gmail.com>
2015-04-10 15:38:15 -0400

    don't write to the database during startup

--- modules/consensus/persist.go
+++ modules/consensus/persist.go
@@ -8,29 +8,37 @@ import (
 )
 
 func (s *State) load(saveDir string) error {
-	var err error
-	s.db, err = blockdb.Open(filepath.Join(saveDir, "chain.db"))
+	db, err := blockdb.Open(filepath.Join(saveDir, "chain.db"))
 	if err != nil {
 		return err
 	}
-	height, err := s.db.Height()
+	height, err := db.Height()
 	if err != nil {
 		return err
 	}
 	if height == 0 {
 		// add genesis block
-		return s.db.AddBlock(s.blockMap[s.currentPath[0]].block)
+		s.db = db
+		return db.AddBlock(s.blockMap[s.currentPath[0]].block)
 	}
-	for i := types.BlockHeight(0); i < height; i++ {
-		b, err := s.db.Block(i)
+	// load blocks from the db, starting after the genesis block
+	// NOTE: during load, the state uses the NilDB. This prevents AcceptBlock
+	// from adding duplicate blocks to the real database.
+	s.db = blockdb.NilDB
+	for i := types.BlockHeight(1); i < height; i++ {
+		b, err := db.Block(i)
 		if err != nil {
 			// should never happen
 			return err
 		}
 		err = s.AcceptBlock(b)
-		if err != nil {
+		if err == ErrBlockKnown {
+			println(i)
+		} else if err != nil {
 			return err
 		}
 	}
+	// start using the real db
+	s.db = db
 	return nil
 }
