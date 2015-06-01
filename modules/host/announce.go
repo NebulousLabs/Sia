c22ef556438da25575aa65267be5afcc42657dce
lukechampine <luke.champine@gmail.com>
2015-06-01 13:31:32 -0400

    return error if host address not reachable

--- modules/host/announce.go
+++ modules/host/announce.go
@@ -2,11 +2,24 @@ package host
 
 import (
 	"errors"
+	"net"
+	"time"
 
 	"github.com/NebulousLabs/Sia/encoding"
 	"github.com/NebulousLabs/Sia/modules"
 	"github.com/NebulousLabs/Sia/types"
 )
+
+// ping establishes a connection to addr and then immediately closes it. It is
+// used to verify that an address is connectible.
+func ping(addr modules.NetAddress) bool {
+	conn, err := net.DialTimeout("tcp", string(addr), time.Second)
+	if err != nil {
+		return false
+	}
+	conn.Close()
+	return true
+}
 
 // Announce creates a host announcement transaction, adding information to the
 // arbitrary data, signing the transaction, and submitting it to the
@@ -19,17 +32,21 @@ func (h *Host) Announce() error {
 		return err
 	}
 
+	// check that our address is reachable
+	lockID := h.mu.RLock()
+	addr := h.myAddr
+	h.mu.RUnlock(lockID)
+	if addr.Host() == "" {
+		return errors.New("can't announce without knowing external IP")
+	} else if !ping(addr) {
+		return errors.New("host address not reachable; ensure you have forwarded port " + addr.Port())
+	}
+
 	// create and encode the announcement and add it to the arbitrary data of
 	// the transaction.
-	lockID := h.mu.RLock()
-	if h.myAddr.Host() == "" {
-		h.mu.RUnlock(lockID)
-		return errors.New("can't announce without knowing external IP")
-	}
 	announcement := encoding.Marshal(modules.HostAnnouncement{
-		IPAddress: h.myAddr,
+		IPAddress: addr,
 	})
-	h.mu.RUnlock(lockID)
 	_, _, err = h.wallet.AddArbitraryData(id, modules.PrefixHostAnnouncement+string(announcement))
 	if err != nil {
 		return err
