810a02dfe404fdf5b4f2960928fed4ffc98993e3
lukechampine <luke.champine@gmail.com>
2015-06-03 12:37:03 -0400

    use persist in host

--- modules/host/persist.go
+++ modules/host/persist.go
@@ -6,6 +6,7 @@ import (
 	"github.com/NebulousLabs/Sia/modules"
 	"github.com/NebulousLabs/Sia/persist"
 	"github.com/NebulousLabs/Sia/types"
+	"github.com/NebulousLabs/Sia/persist"
 )
 
 var persistMetadata = persist.Metadata{
