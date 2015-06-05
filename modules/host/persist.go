4bc0f070cea016fa436422cfe022d8593e42fe4d
Mingling94 <mcluo1994@gmail.com>
2015-06-05 18:35:48 -0400

    not sure

--- modules/host/persist.go
+++ modules/host/persist.go
@@ -6,8 +6,6 @@ import (
 	"github.com/NebulousLabs/Sia/modules"
 	"github.com/NebulousLabs/Sia/persist"
 	"github.com/NebulousLabs/Sia/types"
-	"github.com/NebulousLabs/Sia/persist"
-	"github.com/NebulousLabs/Sia/types"
 )
 
 var persistMetadata = persist.Metadata{
