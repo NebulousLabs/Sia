e8351237d93a21988a71f57bf34b2c64b194941f
Mingling94 <mcluo1994@gmail.com>
2015-06-10 12:10:16 -0400

    Undid the transactionSet() removal
    Test cases weren't passing

--- modules/transactionpool/transactions.go
+++ modules/transactionpool/transactions.go
@@ -9,11 +9,21 @@ import (
 // altered so that they are instead dependent on subscriptions. To my
 // knowledge, only siad still needs to be transitioned.
 
+// transactionSet returns the set of unconfirmed transactions in the order
+// they are required to appear in a block. This function will not limit the
+// volume of transactions to fit in a single block.
+func (tp *TransactionPool) transactionSet() (set []types.Transaction) {
+	for _, txn := range tp.transactionList {
+		set = append(set, txn)
+	}
+	return
+}
+
 // TransactionSet returns the set of unconfirmed transactions in the order
 // they are required to appear in a block. This function will not limit the
 // volume of transactions to fit in a single block.
 func (tp *TransactionPool) TransactionSet() []types.Transaction {
 	id := tp.mu.RLock()
 	defer tp.mu.RUnlock(id)
-	return tp.transactionList
+	return tp.transactionSet()
 }
