f71d8b3318095da3daa2a6572ebc51f5c6632b59
lukechampine <luke.champine@gmail.com>
2015-06-15 17:50:49 -0400

    add siafund support to siac

--- siac/main.go
+++ siac/main.go
@@ -167,7 +167,8 @@ func main() {
 	minerCmd.AddCommand(minerStartCmd, minerStopCmd, minerStatusCmd)
 
 	root.AddCommand(walletCmd)
-	walletCmd.AddCommand(walletAddressCmd, walletSendCmd, walletStatusCmd)
+	walletCmd.AddCommand(walletAddressCmd, walletSendCmd, walletSiafundsCmd, walletStatusCmd)
+	walletSiafundsCmd.AddCommand(walletSiafundsTrackCmd)
 
 	root.AddCommand(renterCmd)
 	renterCmd.AddCommand(renterDownloadQueueCmd, renterFilesDeleteCmd, renterFilesDownloadCmd,
