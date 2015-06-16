d7a601eb21fa2b07f7488ad5b270500a3e4e385a
lukechampine <luke.champine@gmail.com>
2015-06-16 17:17:38 -0400

    add dev homefolder

--- persist/persist.go
+++ persist/persist.go
@@ -9,15 +9,18 @@ import (
 )
 
 var HomeFolder = func() string {
-	// use a special folder during testing
-	if build.Release == "testing" {
-		return filepath.Join(build.SiaTestingDir, "home")
-	}
-
 	home, err := homedir.Dir()
 	if err != nil {
 		println("could not find homedir: " + err.Error()) // TODO: better error handling
 		return ""
 	}
-	return filepath.Join(home, ".config", "Sia")
+
+	switch build.Release {
+	case "testing":
+		return filepath.Join(build.SiaTestingDir, "home")
+	case "dev":
+		return filepath.Join(home, ".config", "SiaDev")
+	default:
+		return filepath.Join(home, ".config", "Sia")
+	}
 }()
