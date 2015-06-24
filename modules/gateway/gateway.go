5aaf12fd00714957987eeefc9bed50812c37d10d
lukechampine <luke.champine@gmail.com>
2015-06-24 15:45:30 -0400

    remove externalIP fn from gateway

--- modules/gateway/gateway.go
+++ modules/gateway/gateway.go
@@ -2,10 +2,8 @@ package gateway
 
 import (
 	"errors"
-	"io"
 	"log"
 	"net"
-	"net/http"
 	"os"
 
 	"github.com/NebulousLabs/Sia/modules"
@@ -115,23 +113,6 @@ func New(addr string, persistDir string) (g *Gateway, err error) {
 	go g.makeOutboundConnections()
 
 	return
-}
-
-// getExternalIP learns the server's hostname from a centralized service,
-// myexternalip.com.
-func getExternalIP() (string, error) {
-	resp, err := http.Get("http://myexternalip.com/raw")
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-	buf := make([]byte, 64)
-	n, err := resp.Body.Read(buf)
-	if err != nil && err != io.EOF {
-		return "", err
-	}
-	hostname := string(buf[:n-1]) // trim newline
-	return hostname, nil
 }
 
 // enforce that Gateway satisfies the modules.Gateway interface
