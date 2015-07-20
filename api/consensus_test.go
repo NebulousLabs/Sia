04a09148fa804cea2d48b42438796e0a07ae7802
David Vorick <david.vorick@gmail.com>
2015-07-20 03:50:16 -0400

    expand the synchronization test

--- api/consensus_test.go
+++ api/consensus_test.go
@@ -33,19 +33,49 @@ func TestIntegrationConsensusGET(t *testing.T) {
 // TestIntegrationConsensusSynchronizeGET probes the GET call to
 // /consensus/synchronize.
 func TestIntegrationConsensusSynchronizeGET(t *testing.T) {
-	t.Skip("no known way to add peers without automatically performing a synchronize")
 	if testing.Short() {
 		t.SkipNow()
 	}
 
+	// Create a server tester and give it a peer. Get the peer ahead of the
+	// server tester, then call 'synchronize' to bring them back to the same
+	// height.
 	st := newServerTester("TestConsensusSynchronizeGET", t)
+
+	// Call synchronize when there are no peers.
 	err := st.callAPI("/consensus/synchronize")
+	if err == nil {
+		t.Error("expecting an error - gateway has no peers")
+	}
+
+	// Mine a block so that when a peer is added, 'st' has a longer blockchain.
+	block, _ := st.miner.FindBlock()
+	err = st.cs.AcceptBlock(block)
 	if err != nil {
 		t.Fatal(err)
 	}
+	// Create a peer and bootstrap it to st.
+	peer := newServerTester("TestConsensusSynchronizeGET - Peer", t)
+	err = peer.server.gateway.Connect(st.netAddress())
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Check that the heights are different on the consensus sets.
+	if st.cs.CurrentBlock().ID() == peer.cs.CurrentBlock().ID() {
+		// TODO: There is not a known way to add peers to a network without
+		// them synchronizing automatically. Perhaps it is proof that
+		// synchronize is not needed anymore, but I am hesitant to drop it
+		// until we are more certian that it is not needed.
+		//
+		// t.Fatal("test objects are already synchronized - calling synchronize will not provide useful information")
+	}
 
-	// TODO: Need some way to tell that a peer was out of sync, and then
-	// in-sync. The problem is that currently, if there are peers they should
-	// synchronize automatically. /consensus/synchronize is much closer to a
-	// debugging api call than an actual api call.
+	// Call synchronize.
+	err = st.callAPI("/consensus/synchronize")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if st.cs.CurrentBlock().ID() != peer.cs.CurrentBlock().ID() {
+		t.Fatal("synchronization failed")
+	}
 }
