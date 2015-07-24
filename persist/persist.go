508b0a41b049b1ec97f4fd65e7c4f5e6c284deb7
David Vorick <david.vorick@gmail.com>
2015-07-24 04:00:04 -0400

    refine persist package

--- persist/persist.go
+++ persist/persist.go
@@ -1,6 +1,7 @@
 package persist
 
 import (
+	"errors"
 	"path/filepath"
 
 	"github.com/mitchellh/go-homedir"
@@ -8,19 +9,29 @@ import (
 	"github.com/NebulousLabs/Sia/build"
 )
 
-var HomeFolder = func() string {
-	home, err := homedir.Dir()
-	if err != nil {
-		println("could not find homedir: " + err.Error()) // TODO: better error handling
-		return ""
-	}
+var (
+	ErrBadVersion = errors.New("incompatible version")
+	ErrBadHeader  = errors.New("wrong header")
 
-	switch build.Release {
-	case "testing":
-		return filepath.Join(build.SiaTestingDir, "home")
-	case "dev":
-		return filepath.Join(home, ".config", "SiaDev")
-	default:
-		return filepath.Join(home, ".config", "Sia")
-	}
-}()
+	HomeFolder = func() string {
+		home, err := homedir.Dir()
+		if err != nil {
+			println("could not find homedir: " + err.Error()) // TODO: better error handling
+			return ""
+		}
+
+		switch build.Release {
+		case "testing":
+			return filepath.Join(build.SiaTestingDir, "home")
+		case "dev":
+			return filepath.Join(home, ".config", "SiaDev")
+		default:
+			return filepath.Join(home, ".config", "Sia")
+		}
+	}()
+)
+
+// Metadata contains the header and version of the data being stored.
+type Metadata struct {
+	Header, Version string
+}
