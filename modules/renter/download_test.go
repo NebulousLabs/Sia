03815b00050a33fd4d4dbd6e925a456163e8b34e
lukechampine <luke.champine@gmail.com>
2015-08-04 15:22:10 -0400

    create downloader object

--- modules/renter/download_test.go
+++ modules/renter/download_test.go
@@ -9,6 +9,36 @@ import (
 	"github.com/NebulousLabs/Sia/modules"
 )
 
+type downloader struct {
+	ecc       modules.ECC
+	chunkSize int
+	remaining int
+	hosts     []fileHost
+	reqChans  []chan int
+	respChans []chan []byte
+}
+
+func newDownloader(ecc modules.ECC, chunkSize, fileSize int, hosts []fileHost) *downloader {
+	// create channels
+	reqChans := make([]chan int, len(hosts))
+	for i := range reqChans {
+		reqChans[i] = make(chan int)
+	}
+	respChans := make([]chan []byte, ecc.NumPieces())
+	for i := range respChans {
+		respChans[i] = make(chan []byte)
+	}
+
+	return &downloader{
+		ecc:       ecc,
+		chunkSize: chunkSize,
+		remaining: fileSize,
+		hosts:     hosts,
+		reqChans:  reqChans,
+		respChans: respChans,
+	}
+}
+
 type pieceData struct {
 	piece  int
 	offset int
@@ -33,50 +63,56 @@ func (h testHost) fetch(p pieceData) ([]byte, error) {
 	return h.data[p.offset : p.offset+p.length], nil
 }
 
-func retrieve(host fileHost, reqChan chan int, respChans []chan []byte) {
+func (d *downloader) worker(host fileHost, reqChan chan int) {
 	for chunkIndex := range reqChan {
 		for _, p := range host.pieces(chunkIndex) {
 			data, err := host.fetch(p)
 			if err != nil {
 				data = nil
 			}
-			respChans[p.piece] <- data
+			d.respChans[p.piece] <- data
 		}
 	}
 }
 
-func orchestrate(ecc modules.ECC, dataSize int, chunkSize int, reqChans []chan int, respChans []chan []byte) ([]byte, error) {
+func (d *downloader) run(w io.Writer) error {
+	// spawn download workers
+	for i, h := range d.hosts {
+		go d.worker(h, d.reqChans[i])
+	}
+
 	defer func() {
 		// close request channels, terminating the worker goroutines
-		for _, ch := range reqChans {
+		for _, ch := range d.reqChans {
 			close(ch)
 		}
 	}()
 
-	buf := new(bytes.Buffer) // recovered data will be written here
-	chunk := make([][]byte, len(respChans))
-	bytesLeft := dataSize
-	for i := 0; bytesLeft > 0; i++ {
+	chunk := make([][]byte, d.ecc.NumPieces())
+	for i := 0; d.remaining > 0; i++ {
 		// tell all workers to download chunk i
-		for _, ch := range reqChans {
+		for _, ch := range d.reqChans {
 			ch <- i
 		}
 		// load pieces into chunk
-		for j, ch := range respChans {
+		for j, ch := range d.respChans {
 			chunk[j] = <-ch
 		}
 
-		// write pieces to buf
-		err := ecc.Recover(chunk, uint64(chunkSize), buf)
-		if err != nil {
-			return nil, err
+		// Write pieces to w. We always write chunkSize bytes unless this is
+		// the last chunk; in that case, we write the remainder.
+		n := d.chunkSize
+		if n > d.remaining {
+			n = d.remaining
 		}
-		bytesLeft -= chunkSize
+		err := d.ecc.Recover(chunk, uint64(n), w)
+		if err != nil {
+			return err
+		}
+		d.remaining -= d.chunkSize
 	}
 
-	buf.Truncate(int(dataSize))
-
-	return buf.Bytes(), nil
+	return nil
 }
 
 // TestErasureDownload tests parallel downloading of erasure-coded data.
@@ -120,28 +156,21 @@ func TestErasureDownload(t *testing.T) {
 		}
 	}
 
-	// create communication channels
-	reqChans := make([]chan int, len(hosts))
-	for i := range reqChans {
-		reqChans[i] = make(chan int)
-	}
-	respChans := make([]chan []byte, ecc.NumPieces())
-	for i := range respChans {
-		respChans[i] = make(chan []byte)
-	}
-
-	// spawn download workers
-	for i, h := range hosts {
-		go retrieve(h, reqChans[i], respChans)
+	// annoying -- have to convert to proper interface
+	var hs []fileHost
+	for i := range hosts {
+		hs = append(hs, &hosts[i])
 	}
 
 	// download data
-	rec, err := orchestrate(ecc, dataSize, chunkSize, reqChans, respChans)
+	d := newDownloader(ecc, chunkSize, dataSize, hs)
+	buf := new(bytes.Buffer)
+	err = d.run(buf)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if len(rec) < len(data) || !bytes.Equal(rec[:len(data)], data) {
+	if !bytes.Equal(buf.Bytes(), data) {
 		t.Fatal("recovered data does not match original")
 	}
 }
