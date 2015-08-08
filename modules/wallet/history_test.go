9e91f961ded9d830c6e40176b5292914c4d04a3e
David Vorick <david.vorick@gmail.com>
2015-08-08 05:31:54 -0400

    add address history test

--- modules/wallet/history_test.go
+++ modules/wallet/history_test.go
@@ -6,17 +6,17 @@ import (
 	"github.com/NebulousLabs/Sia/types"
 )
 
-// TestTransactionHistory checks that the transaction history is being
-// correctly recorded and extended.
-func TestTransactionHistory(t *testing.T) {
+// TestIntegrationTransactionHistory checks that the transaction history is
+// being correctly recorded and extended.
+func TestIntegrationTransactionHistory(t *testing.T) {
 	if testing.Short() {
 		t.SkipNow()
 	}
-
 	wt, err := createWalletTester("TestTransactionHistory")
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	// Creating the wallet tester results in blocks being mined until the miner
 	// has money, which means types.MaturityDelay+1 blocks are created, and
 	// each block is going to have a transaction (the miner payout) going to
@@ -60,5 +60,47 @@ func TestTransactionHistory(t *testing.T) {
 	// send that occured.
 	if len(txns) != 5 {
 		t.Error(len(txns))
+	}
+}
+
+// TestIntegrationAddressHistory checks grabbing the history for a single
+// address.
+func TestIntegrationAddressHistory(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+	wt, err := createWalletTester("TestTransactionHistory")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Grab an address and send it money.
+	uc, err := wt.wallet.NextAddress()
+	addr := uc.UnlockHash()
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = wt.wallet.SendSiacoins(types.NewCurrency64(5005), addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Check the confirmed balance of the address.
+	if len(wt.wallet.AddressTransactionHistory(addr)) != 0 {
+		t.Error("address history should be empty")
+	}
+	if len(wt.wallet.AddressUnconfirmedTransactions(addr)) == 0 {
+		t.Error("addresses unconfirmed transactions should not be empty")
+	}
+	b, _ := wt.miner.FindBlock()
+	err = wt.cs.AcceptBlock(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(wt.wallet.AddressTransactionHistory(addr)) == 0 {
+		t.Error("address history should have some transactions")
+	}
+	if len(wt.wallet.AddressUnconfirmedTransactions(addr)) != 0 {
+		t.Error("addresses unconfirmed transactions should be empty")
 	}
 }
