7db3d86792f409acac5e7ce3e8d8fc2fe23ce5aa
David Vorick <david.vorick@gmail.com>
2015-08-13 18:54:52 -0400

    add rudimentary timing tool to profile package

---
+++ profile/timing.go
@@ -0,0 +1,36 @@
+package profile
+
+import (
+	"fmt"
+	"time"
+)
+
+var (
+	times        map[string]int64
+	activeTimers map[string]int64
+)
+
+// PrintTimes prints how much time has passed at each timer.
+func PrintTimes() (s string) {
+	for name, time := range times {
+		s += fmt.Sprintf("\t%v: %v\n", name, time)
+	}
+	return
+}
+
+// ToggleTimer actives a timer known by a given string. If the timer does not
+// yet exist, it is created.
+func ToggleTimer(s string) {
+	if times == nil {
+		times = make(map[string]int64)
+		activeTimers = make(map[string]int64)
+	}
+
+	toggleTime, exists := activeTimers[s]
+	if exists {
+		times[s] = times[s] + (time.Now().UnixNano() - toggleTime)
+		delete(activeTimers, s)
+	} else {
+		activeTimers[s] = time.Now().UnixNano()
+	}
+}
