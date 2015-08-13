9bf1f68324cf2adb7ad4993fd5b7452409f0fae4
David Vorick <david.vorick@gmail.com>
2015-08-13 06:50:09 -0400

    cleaner types in database.go

--- modules/consensus/database_test.go
+++ modules/consensus/database_test.go
@@ -7,21 +7,21 @@ import (
 
 // lenSiacoinOutputs returns the size of the siacoin outputs bucket
 func (db *setDB) lenSiacoinOutputs() uint64 {
-	return db.lenBucket("SiacoinOutputs")
+	return db.lenBucket(SiacoinOutputs)
 }
 
 // lenFileContracts returns the number of file contracts in the consensus set
 func (db *setDB) lenFileContracts() uint64 {
-	return db.lenBucket("FileContracts")
+	return db.lenBucket(FileContracts)
 }
 
 // lenFCExpirationsHeight returns the number of file contracts which expire at a given height
 func (db *setDB) lenFCExpirationsHeight(h types.BlockHeight) uint64 {
 	bucketID := append(prefix_fcex, encoding.Marshal(h)...)
-	return db.lenBucket(string(bucketID))
+	return db.lenBucket(bucketID)
 }
 
 // lenSiafundOutputs returns the size of the SiafundOutputs bucket
 func (db *setDB) lenSiafundOutputs() uint64 {
-	return db.lenBucket("SiafundOutputs")
+	return db.lenBucket(SiafundOutputs)
 }
