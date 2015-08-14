2d120115776cdea5ac16a971b8213443315ba9bb
lukechampine <luke.champine@gmail.com>
2015-08-14 01:40:44 -0400

    add safeFile to persist

--- persist/persist.go
+++ persist/persist.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rand"
 	"encoding/base32"
 	"errors"
+	"os"
 )
 
 var (
@@ -24,4 +25,31 @@ func RandomSuffix() string {
 	rand.Read(randBytes)
 	str := base32.StdEncoding.EncodeToString(randBytes)
 	return str[:20]
+}
+
+// A safeFile is a file that is stored under a temporary filename. When the
+// safeFile is closed, it will be renamed to the "final" filename. This allows
+// for atomic updating of files; otherwise, an unexpected shutdown could leave
+// a valuable file in a corrupted state.
+type safeFile struct {
+	*os.File
+	finalName string
+}
+
+// Close renames the file to the intended final filename, and then closes the
+// file handle.
+func (sf *safeFile) Close() error {
+	err := os.Rename(sf.finalName+"_temp", sf.finalName)
+	if err != nil {
+		return err
+	}
+	return sf.File.Close()
+}
+
+func NewSafeFile(filename string) (*safeFile, error) {
+	file, err := os.Create(filename + "_temp")
+	if err != nil {
+		return nil, err
+	}
+	return &safeFile{file, filename}, nil
 }
