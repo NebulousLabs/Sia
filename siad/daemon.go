0de9d84151989eefec456d3ad1032912d08257de
JoshVorick <josh.vorick@gmail.com>
2015-08-21 16:35:19 -0400

    Don't run a miningpool by default

--- siad/daemon.go
+++ siad/daemon.go
@@ -13,7 +13,6 @@ import (
 	"github.com/NebulousLabs/Sia/modules/host"
 	"github.com/NebulousLabs/Sia/modules/hostdb"
 	"github.com/NebulousLabs/Sia/modules/miner"
-	"github.com/NebulousLabs/Sia/modules/miningpool"
 	"github.com/NebulousLabs/Sia/modules/renter"
 	"github.com/NebulousLabs/Sia/modules/transactionpool"
 	"github.com/NebulousLabs/Sia/modules/wallet"
@@ -55,10 +54,6 @@ func startDaemon() error {
 	if err != nil {
 		return err
 	}
-	miningpool, err := miningpool.New(state, tpool, wallet, config.Siad.MiningPoolAddr, filepath.Join(config.Siad.SiaDir, modules.MinerDir))
-	if err != nil {
-		return err
-	}
 	hostdb, err := hostdb.New(state, gateway)
 	if err != nil {
 		return err
@@ -71,7 +66,7 @@ func startDaemon() error {
 	if err != nil {
 		return err
 	}
-	srv, err := api.NewServer(config.Siad.APIaddr, state, gateway, host, hostdb, miner, miningpool, renter, tpool, wallet, nil)
+	srv, err := api.NewServer(config.Siad.APIaddr, state, gateway, host, hostdb, miner, nil, renter, tpool, wallet, nil)
 	if err != nil {
 		return err
 	}
