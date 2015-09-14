56576bb943e924e0750b48369baf30ae7306ae9d
David Vorick <david.vorick@gmail.com>
2015-09-14 18:45:59 -0400

    more agressive height checking for types package
    I realized that the height in the types package would not be set
    correctly during large reorgs of the consensus set - only during the
    first reorg. I don't think it's likely to be a problem on the live
    network because reorgs are fairly rare, but the code is more correct
    now.

--- modules/consensus/diffs.go
+++ modules/consensus/diffs.go
@@ -224,6 +224,18 @@ func updateCurrentPath(tx *bolt.Tx, pb *processedBlock, dir modules.DiffDirectio
 
 // commitDiffSet applies or reverts the diffs in a blockNode.
 func commitDiffSet(tx *bolt.Tx, pb *processedBlock, dir modules.DiffDirection) error {
+	// COMPATv0.4.0
+	//
+	// When validating/accepting a block, the types height needs to be set to
+	// the height of the block that's being analyzed. After analysis is
+	// finished, the height needs to be set to the height of the current block.
+	//
+	// This is so that the Tax() logic correctly handles the hardfork that
+	// changes the tax from a float64 to a big.Rat during computation.
+	types.CurrentHeightLock.Lock()
+	types.CurrentHeight = pb.Height
+	types.CurrentHeightLock.Unlock()
+
 	// Sanity checks - there are a few so they were moved to another function.
 	if build.DEBUG {
 		commitDiffSetSanity(tx, pb, dir)
@@ -251,6 +263,18 @@ func commitDiffSet(tx *bolt.Tx, pb *processedBlock, dir modules.DiffDirection) e
 // transaction is valid unless we have applied all of the previous transactions
 // in the block, which means we need to apply while we verify.
 func generateAndApplyDiff(tx *bolt.Tx, pb *processedBlock) error {
+	// COMPATv0.4.0
+	//
+	// When validating/accepting a block, the types height needs to be set to
+	// the height of the block that's being analyzed. After analysis is
+	// finished, the height needs to be set to the height of the current block.
+	//
+	// This is so that the Tax() logic correctly handles the hardfork that
+	// changes the tax from a float64 to a big.Rat during computation.
+	types.CurrentHeightLock.Lock()
+	types.CurrentHeight = pb.Height
+	types.CurrentHeightLock.Unlock()
+
 	// Sanity check - the block being applied should have the current block as
 	// a parent.
 	if build.DEBUG && pb.Parent != currentBlockID(tx) {
