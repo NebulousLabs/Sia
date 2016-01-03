c313fad97ce0c369159438b4a4e3171ce3aedfdf
lukechampine <luke.champine@gmail.com>
2016-01-03 02:59:25 -0500

    proper json for tpool API

--- api/transactionpool.go
+++ api/transactionpool.go
@@ -3,11 +3,17 @@ package api
 import (
 	"net/http"
 
+	"github.com/NebulousLabs/Sia/types"
+
 	"github.com/julienschmidt/httprouter"
 )
+
+type TransactionPoolGET struct {
+	Transactions []types.Transaction `json:"transactions"`
+}
 
 // transactionpoolTransactionsHandler handles the API call to get the
 // transaction pool trasactions.
 func (srv *Server) transactionpoolTransactionsHandler(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-	writeJSON(w, srv.tpool.TransactionList())
+	writeJSON(w, TransactionPoolGET{Transactions: srv.tpool.TransactionList()})
 }
