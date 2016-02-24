d40c49aee3e81aa3bba65cf10f7b5186e7aa2794
David Vorick <david.vorick@gmail.com>
2016-02-24 08:10:09 -0500

    cleaner build.Critical construction

--- build/critical.go
+++ build/critical.go
@@ -8,11 +8,8 @@ import (
 // Critical will print a message to os.Stderr unless DEBUG has been set, in
 // which case panic will be called instead.
 func Critical(v ...interface{}) {
-	s := fmt.Sprintln(v...)
-	if Release != "testing" {
-		os.Stderr.WriteString(s)
-	}
 	if DEBUG {
 		panic(s)
 	}
+	os.Stderr.WriteString(s)
 }
