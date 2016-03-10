7c0f03becfbf421fc21a279f2e58b1735c2d372f
lukechampine <luke.champine@gmail.com>
2016-03-10 01:56:55 -0500

    real hostdb persistence

--- modules/renter/hostdb/persist.go
+++ modules/renter/hostdb/persist.go
@@ -2,15 +2,34 @@ package hostdb
 
 // hdbPersist defines what HostDB data persists across sessions.
 type hdbPersist struct {
-	// TODO: save hosts
+	AllHosts    []hostEntry
+	ActiveHosts []hostEntry
 }
 
 // save saves the hostdb persistence data to disk.
 func (hdb *HostDB) save() error {
-	return nil
+	var data hdbPersist
+	for _, entry := range hdb.allHosts {
+		data.AllHosts = append(data.AllHosts, *entry)
+	}
+	for _, node := range hdb.activeHosts {
+		data.ActiveHosts = append(data.ActiveHosts, *node.hostEntry)
+	}
+	return hdb.persist.save(data)
 }
 
 // load loads the hostdb persistence data from disk.
 func (hdb *HostDB) load() error {
+	var data hdbPersist
+	err := hdb.persist.load(&data)
+	if err != nil {
+		return err
+	}
+	for _, entry := range data.AllHosts {
+		hdb.allHosts[entry.NetAddress] = &entry
+	}
+	for _, entry := range data.ActiveHosts {
+		hdb.insertNode(&entry)
+	}
 	return nil
 }
