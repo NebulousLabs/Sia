c421bb5ab87565b463acedb2f0bd4723fe343e14
Jordan Horwich <jordan@voidingwarranties.com>
2016-05-11 01:55:42 -0400

    Share dialback port to peers on connect
    in preparation for removing the RelayNode RPC
    and bump version const to v0.6.1

--- build/version.go
+++ build/version.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Version is the current version of siad.
-const Version = "0.6.0"
+const Version = "0.6.1"
 
 // IsVersion returns whether str is a valid version number.
 func IsVersion(str string) bool {
