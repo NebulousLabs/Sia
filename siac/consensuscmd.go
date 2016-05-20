586a0451ce59193c88944398ff197e82c9ef7785
Jordan Horwich <jordan@voidingwarranties.com>
2016-05-20 16:04:16 -0400

    Add IBD progress estimation
    The blockchain height can be estimated using the block time (10 minutes)
    and the time passed since a block at a known height and time.
    
    Closes #929

--- siac/consensuscmd.go
+++ siac/consensuscmd.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/spf13/cobra"
 
@@ -25,9 +26,22 @@ func consensuscmd() {
 	if err != nil {
 		die("Could not get current consensus state:", err)
 	}
-	fmt.Printf(`Synced: %v
+	if cg.Synced {
+		fmt.Printf(`Synced: %v
 Block:  %v
 Height: %v
 Target: %v
 `, yesNo(cg.Synced), cg.CurrentBlock, cg.Height, cg.Target)
+	} else {
+		// Estimate the height of the blockchain by calculating the minutes since a
+		// known block, and dividing by 10 minutes (the block time).
+		block50000Timestamp := time.Date(2016, time.May, 11, 19, 33, 0, 0, time.UTC)
+		diff := time.Since(block50000Timestamp)
+		estimatedHeight := 50000 + (diff.Minutes() / 10)
+		estimatedProgress := float64(cg.Height) / estimatedHeight * 100
+		fmt.Printf(`Synced: %v
+Height: %v
+Progress (estimated): %.f%%
+`, yesNo(cg.Synced), cg.Height, estimatedProgress)
+	}
 }
