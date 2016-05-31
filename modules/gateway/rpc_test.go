9467a39ecf599a435d40f988778660a70ff277bb
Jordan Horwich <jordan@voidingwarranties.com>
2016-05-31 17:25:15 -0400

    Increase timeout in TestCallingRPCFromRPC
    fixes #1099

--- modules/gateway/rpc_test.go
+++ modules/gateway/rpc_test.go
@@ -502,13 +502,13 @@ func TestCallingRPCFromRPC(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-	case <-time.After(200 * time.Millisecond):
+	case <-time.After(500 * time.Millisecond):
 		t.Fatal("expected FOO RPC to be called")
 	}
 
 	select {
 	case <-barChan:
-	case <-time.After(200 * time.Millisecond):
+	case <-time.After(500 * time.Millisecond):
 		t.Fatal("expected BAR RPC to be called")
 	}
 }
