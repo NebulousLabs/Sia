3e7e2d57245c8e8d9f5790a2eeb72bb6b39af4aa
Jane Maunsell <maunsell@mit.edu>
2016-06-05 19:16:04 -0400

    use View instead of Begin/Commit for ErrTxNotWritable; handle error in TestOpenDatabase

--- persist/boltdb_test.go
+++ persist/boltdb_test.go
@@ -10,41 +10,41 @@ import (
 	"github.com/NebulousLabs/Sia/build"
 	"github.com/NebulousLabs/bolt"
 )
- 
+
 // testInputs and testFilenames are global variables because most tests require
-// a variety of metadata and filename inputs (although only TestCheckMetadata 
-// and TestIntegratedCheckMetadata use testInput.newMd and testInput.err). 
+// a variety of metadata and filename inputs (although only TestCheckMetadata
+// and TestIntegratedCheckMetadata use testInput.newMd and testInput.err).
 // Weird strings are from https://github.com/minimaxir/big-ltist-of-naughty-strings
 var (
-    testInputs = []struct{
-        md		Metadata
-        newMd	Metadata
-        err     error
-    }{
+	testInputs = []struct {
+		md    Metadata
+		newMd Metadata
+		err   error
+	}{
 		{Metadata{"1sadf23", "12253"}, Metadata{"1sa-df23", "12253"}, ErrBadHeader},
 		{Metadata{"$@#$%^&", "$@#$%^&"}, Metadata{"$@#$%^&", "$@#$%!^&"}, ErrBadVersion},
 		{Metadata{"//", "//"}, Metadata{"////", "//"}, ErrBadHeader},
 		{Metadata{":]", ":)"}, Metadata{":]", ":("}, ErrBadVersion},
-		{Metadata{"¯|_(ツ)_|¯","_|¯(ツ)¯|_"}, Metadata{"¯|_(ツ)_|¯","_|¯(ツ)_|¯"}, ErrBadVersion},
+		{Metadata{"¯|_(ツ)_|¯", "_|¯(ツ)¯|_"}, Metadata{"¯|_(ツ)_|¯", "_|¯(ツ)_|¯"}, ErrBadVersion},
 		{Metadata{"世界", "怎么办呢"}, Metadata{"世界", "怎么好呢"}, ErrBadVersion},
-		{Metadata{"     ","     "}, Metadata{"\t","     "}, ErrBadHeader},
-        {Metadata{"",""}, Metadata{"asdf",""}, ErrBadHeader},
-        {Metadata{"","_"}, Metadata{"",""}, ErrBadVersion},
-        {Metadata{"%&*","#@$"}, Metadata{"","#@$"}, ErrBadHeader},
-        {Metadata{"a.sdf","0.30.2"}, Metadata{"a.sdf", "0.3.02" }, ErrBadVersion},
-        {Metadata{"/","/"}, Metadata{"//","/"}, ErrBadHeader},
-        {Metadata{"%*.*s","%d"}, Metadata{"%*.*s","%    d"}, ErrBadVersion},
-        {Metadata{" ",""}, Metadata{"   ",""}, ErrBadHeader},
-        {Metadata{"⒯⒣⒠ ⒬⒰⒤⒞⒦ ⒝⒭⒪⒲⒩ ⒡⒪⒳ ⒥⒰⒨⒫⒮ ⒪⒱⒠⒭ ⒯⒣⒠ ⒧⒜⒵⒴ ⒟⒪⒢","undefined"}, Metadata{"⒯⒣⒠ ⒬⒰⒤⒞⒦ ⒝⒭⒪⒲⒩ ⒡⒪⒳ ⒥⒰⒨⒫⒮ ⒪⒱⒠⒭ ⒯⒣⒠ ⒧⒜⒵⒴ ⒟⒪⒢","␢undefined"}, ErrBadVersion},
-        {Metadata{" ","  "}, Metadata{"  ","  "}, ErrBadHeader},
-        {Metadata{"\xF0\x9F\x98\x8F","\xF0\x9F\x98\xBE"},Metadata{"\xF0\x9F\x98\x8F"," \xF0\x9F\x98\xBE"}, ErrBadVersion},
-        {Metadata{"'",""}, Metadata{"`",""}, ErrBadHeader},
-        {Metadata{"","-"}, Metadata{"","-␡"}, ErrBadVersion},
-		{Metadata{"<foo val=“bar” />","(ﾉಥ益ಥ ┻━┻"}, Metadata{"<foo val=“bar” />","(ﾉ\nಥ益ಥ ┻━┻"}, ErrBadVersion},
-		{Metadata{"\n\n","Ṱ̺̺o͞ ̷i̲̬n̝̗v̟̜o̶̙kè͚̮ ̖t̝͕h̼͓e͇̣ ̢̼h͚͎i̦̲v̻͍e̺̭-m̢iͅn̖̺d̵̼ ̞̥r̛̗e͙p͠r̼̞e̺̠s̘͇e͉̥ǹ̬͎t͍̬i̪̱n͠g̴͉ ͏͉c̬̟h͡a̫̻o̫̟s̗̦.̨̹"}, Metadata{"\n\n","Ṱ̺̺o͞ ̷i̲̬n̝̗v̟̜o̶̙kè͚̮ t̝͕h̼͓e͇̣ ̢̼h͚͎i̦̲v̻͍e̺̭-m̢iͅn̖̺d̵̼ ̞̥r̛̗e͙p͠r̼̞e̺̠s̘͇e͉̥ǹ̬͎t͍̬i̪̱n͠g̴͉ ͏͉c̬̟h͡a̫̻o̫̟s̗̦.̨̹"}, ErrBadVersion},
-    }
+		{Metadata{"     ", "     "}, Metadata{"\t", "     "}, ErrBadHeader},
+		{Metadata{"", ""}, Metadata{"asdf", ""}, ErrBadHeader},
+		{Metadata{"", "_"}, Metadata{"", ""}, ErrBadVersion},
+		{Metadata{"%&*", "#@$"}, Metadata{"", "#@$"}, ErrBadHeader},
+		{Metadata{"a.sdf", "0.30.2"}, Metadata{"a.sdf", "0.3.02"}, ErrBadVersion},
+		{Metadata{"/", "/"}, Metadata{"//", "/"}, ErrBadHeader},
+		{Metadata{"%*.*s", "%d"}, Metadata{"%*.*s", "%    d"}, ErrBadVersion},
+		{Metadata{" ", ""}, Metadata{"   ", ""}, ErrBadHeader},
+		{Metadata{"⒯⒣⒠ ⒬⒰⒤⒞⒦ ⒝⒭⒪⒲⒩ ⒡⒪⒳ ⒥⒰⒨⒫⒮ ⒪⒱⒠⒭ ⒯⒣⒠ ⒧⒜⒵⒴ ⒟⒪⒢", "undefined"}, Metadata{"⒯⒣⒠ ⒬⒰⒤⒞⒦ ⒝⒭⒪⒲⒩ ⒡⒪⒳ ⒥⒰⒨⒫⒮ ⒪⒱⒠⒭ ⒯⒣⒠ ⒧⒜⒵⒴ ⒟⒪⒢", "␢undefined"}, ErrBadVersion},
+		{Metadata{" ", "  "}, Metadata{"  ", "  "}, ErrBadHeader},
+		{Metadata{"\xF0\x9F\x98\x8F", "\xF0\x9F\x98\xBE"}, Metadata{"\xF0\x9F\x98\x8F", " \xF0\x9F\x98\xBE"}, ErrBadVersion},
+		{Metadata{"'", ""}, Metadata{"`", ""}, ErrBadHeader},
+		{Metadata{"", "-"}, Metadata{"", "-␡"}, ErrBadVersion},
+		{Metadata{"<foo val=“bar” />", "(ﾉಥ益ಥ ┻━┻"}, Metadata{"<foo val=“bar” />", "(ﾉ\nಥ益ಥ ┻━┻"}, ErrBadVersion},
+		{Metadata{"\n\n", "Ṱ̺̺o͞ ̷i̲̬n̝̗v̟̜o̶̙kè͚̮ ̖t̝͕h̼͓e͇̣ ̢̼h͚͎i̦̲v̻͍e̺̭-m̢iͅn̖̺d̵̼ ̞̥r̛̗e͙p͠r̼̞e̺̠s̘͇e͉̥ǹ̬͎t͍̬i̪̱n͠g̴͉ ͏͉c̬̟h͡a̫̻o̫̟s̗̦.̨̹"}, Metadata{"\n\n", "Ṱ̺̺o͞ ̷i̲̬n̝̗v̟̜o̶̙kè͚̮ t̝͕h̼͓e͇̣ ̢̼h͚͎i̦̲v̻͍e̺̭-m̢iͅn̖̺d̵̼ ̞̥r̛̗e͙p͠r̼̞e̺̠s̘͇e͉̥ǹ̬͎t͍̬i̪̱n͠g̴͉ ͏͉c̬̟h͡a̫̻o̫̟s̗̦.̨̹"}, ErrBadVersion},
+	}
 
-    testFilenames = []string{
+	testFilenames = []string{
 		" ",
 		"_",
 		"-",
@@ -59,9 +59,8 @@ var (
 		",.;'[]-=",
 		"A:",
 		"%s",
-    }
+	}
 )
-
 
 // TestOpenDatabase tests calling OpenDatabase on the following types of
 // database:
@@ -93,10 +92,10 @@ func TestOpenDatabase(t *testing.T) {
 	// Create a folder for the database file. If a folder by that name exists
 	// already, it will be replaced by an empty folder.
 	testDir := build.TempDir(persistDir, "TestOpenNewDatabase")
-		err := os.MkdirAll(testDir, 0700)
-		if err != nil {
-			t.Fatal(err)
-		}
+	err := os.MkdirAll(testDir, 0700)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	for i, in := range testInputs {
 		dbFilename := testFilenames[i%len(testFilenames)]
@@ -125,23 +124,28 @@ func TestOpenDatabase(t *testing.T) {
 			for _, testBucket := range testBuckets {
 				_, err := tx.CreateBucketIfNotExists(testBucket)
 				if err != nil {
-					t.Fatalf("db.Update failed on bucket name %v for metadata %v, filename %v; error was %v", testBucket, in.md, dbFilename, err)
+					t.Errorf("db.Update failed on bucket name %v for metadata %v, filename %v; error was %v", testBucket, in.md, dbFilename, err)
 					return err
 				}
 			}
 			return nil
 		})
+
 		if err != nil {
+			t.Fatal(err)
 		}
 
 		// Make sure CreateBucketIfNotExists method handles invalid (nil)
 		// bucket name.
 		err = db.Update(func(tx *bolt.Tx) error {
 			_, err := tx.CreateBucketIfNotExists(nil)
-			return err				
+			return err
 		})
+
+		//
 		if err != bolt.ErrBucketNameRequired {
-			t.Fatalf("the CreateBucketIfNotExists method returned wrong error when fed nil byteslice (metadata was %v, filename was %v); expected %v, got %v", in.md, dbFilename, bolt.ErrBucketNameRequired, err)
+			t.Errorf("the CreateBucketIfNotExists method returned wrong error when fed nil byteslice (metadata was %v, filename was %v); expected %v, got %v", in.md, dbFilename, bolt.ErrBucketNameRequired, err)
+			continue
 		}
 
 		// Fill each bucket with a random number (0-9, inclusive) of key/value
@@ -158,15 +162,17 @@ func TestOpenDatabase(t *testing.T) {
 					rand.Read(v)
 					err := b.Put(k, v)
 					if err != nil {
+						t.Errorf("db.Update failed to fill bucket %v for metadata %v, filename %v; error was %v", testBucket, in.md, dbFilename, err)
 						return err
 					}
-				}	
+				}
 			}
-		return nil
-		})		
+			return nil
+		})
+
 		if err != nil {
 			t.Fatal(err)
-		}	
+		}
 
 		// Close the newly-filled database.
 		err = db.Close()
@@ -208,7 +214,6 @@ func TestOpenDatabase(t *testing.T) {
 	}
 }
 
-
 // TestErrPermissionOpenDatabase tests calling OpenDatabase on a database file
 // with the wrong filemode (< 0600), which should result in an os.ErrPermission
 // error.
@@ -223,7 +228,7 @@ func TestErrPermissionOpenDatabase(t *testing.T) {
 		dbFilename = "Fake Filename"
 	)
 
-	// Create a folder for the database file. If a folder by that name exists 
+	// Create a folder for the database file. If a folder by that name exists
 	// already, it will be replaced by an empty folder.
 	testDir := build.TempDir(persistDir, "TestErrPermissionOpenDatabase")
 	err := os.MkdirAll(testDir, 0700)
@@ -238,8 +243,8 @@ func TestErrPermissionOpenDatabase(t *testing.T) {
 	for _, mode := range badFileModes {
 		// Create a file named dbFilename in directory testDir with the wrong
 		// permissions (mode < 0600).
-		_, err := os.OpenFile(dbFilepath, os.O_RDWR|os.O_CREATE, mode) 
-		if err != nil { 
+		_, err := os.OpenFile(dbFilepath, os.O_RDWR|os.O_CREATE, mode)
+		if err != nil {
 			t.Fatal(err)
 		}
 
@@ -254,11 +259,10 @@ func TestErrPermissionOpenDatabase(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
-	}        
+	}
 }
 
-
-// TestErrTxNotWritable checks that updateMetadata returns an error when called 
+// TestErrTxNotWritable checks that updateMetadata returns an error when called
 // from a read-only transaction.
 func TestErrTxNotWritable(t *testing.T) {
 	if testing.Short() {
@@ -282,18 +286,23 @@ func TestErrTxNotWritable(t *testing.T) {
 
 		boltDB := &BoltDatabase{
 			Metadata: in.md,
-			DB: db,
+			DB:       db,
 		}
 
-		tx, err := db.Begin(false)
-		// Should return an error since tx is a read-only transaction.
-		err = boltDB.updateMetadata(tx)
+		err = db.View(func(tx *bolt.Tx) error {
+			err = boltDB.updateMetadata(tx)
+			return err
+		})
+
 		if err != bolt.ErrTxNotWritable {
 			t.Errorf("updateMetadata returned wrong error for input %v, filename %v; expected tx not writable, got %v", in.md, dbFilename, err)
 		}
 
-		tx.Commit()
-		boltDB.Close()
+		err = boltDB.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+
 		err = os.Remove(dbFilepath)
 		if err != nil {
 			t.Fatal(err)
@@ -301,8 +310,7 @@ func TestErrTxNotWritable(t *testing.T) {
 	}
 }
 
-
-// TestErrDatabaseNotOpen tests that checkMetadata returns an error when called 
+// TestErrDatabaseNotOpen tests that checkMetadata returns an error when called
 // on a BoltDatabase that is closed.
 func TestErrDatabaseNotOpen(t *testing.T) {
 	if testing.Short() {
@@ -316,7 +324,7 @@ func TestErrDatabaseNotOpen(t *testing.T) {
 	}
 
 	dbFilepath := filepath.Join(testDir, "fake_filename")
-	md := Metadata{"Fake Header","Fake Version"}
+	md := Metadata{"Fake Header", "Fake Version"}
 
 	db, err := bolt.Open(dbFilepath, 0600, &bolt.Options{Timeout: 3 * time.Second})
 	if err != nil {
@@ -325,8 +333,8 @@ func TestErrDatabaseNotOpen(t *testing.T) {
 
 	boltDB := &BoltDatabase{
 		Metadata: md,
-		DB: db,
-	}	
+		DB:       db,
+	}
 
 	err = boltDB.Close()
 	if err != nil {
@@ -345,8 +353,7 @@ func TestErrDatabaseNotOpen(t *testing.T) {
 	}
 }
 
-
-// TestErrCheckMetadata tests that checkMetadata returns an error when called 
+// TestErrCheckMetadata tests that checkMetadata returns an error when called
 // on a BoltDatabase whose metadata has been changed.
 func TestErrCheckMetadata(t *testing.T) {
 	if testing.Short() {
@@ -359,7 +366,7 @@ func TestErrCheckMetadata(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	for i, in := range testInputs {		
+	for i, in := range testInputs {
 		dbFilename := testFilenames[i%len(testFilenames)]
 		dbFilepath := filepath.Join(testDir, dbFilename)
 
@@ -367,10 +374,10 @@ func TestErrCheckMetadata(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		
+
 		boltDB := &BoltDatabase{
-			Metadata: 	in.md,
-			DB: 		db,
+			Metadata: in.md,
+			DB:       db,
 		}
 
 		err = db.Update(func(tx *bolt.Tx) error {
@@ -383,26 +390,26 @@ func TestErrCheckMetadata(t *testing.T) {
 			if err != nil {
 				return err
 			}
-			
+
 			err = bucket.Put([]byte("Version"), []byte(in.newMd.Version))
 			if err != nil {
 				return err
-				}
+			}
 			return nil
 		})
 
 		if err != nil {
 			t.Errorf("Put method failed for input %v, filename %v with error %v", in, dbFilename, err)
 			continue
-		}	
-	
-		// Should return an error because boltDB's metadata 
-		// now differs from its original metadata. 
-		err = (*boltDB).checkMetadata(in.md) 
-		if err != in.err { 
-			t.Errorf("expected %v, got %v for input %v -> %v", in.err, err, in.md, in.newMd)	
 		}
-	
+
+		// Should return an error because boltDB's metadata
+		// now differs from its original metadata.
+		err = (*boltDB).checkMetadata(in.md)
+		if err != in.err {
+			t.Errorf("expected %v, got %v for input %v -> %v", in.err, err, in.md, in.newMd)
+		}
+
 		err = boltDB.Close()
 		if err != nil {
 			t.Fatal(err)
@@ -415,9 +422,8 @@ func TestErrCheckMetadata(t *testing.T) {
 	}
 }
 
-
 // TestErrIntegratedCheckMetadata checks that checkMetadata returns an error
-// within OpenDatabase when OpenDatabase is called on a BoltDatabase that has 
+// within OpenDatabase when OpenDatabase is called on a BoltDatabase that has
 // already been set up with different metadata.
 func TestErrIntegratedCheckMetadata(t *testing.T) {
 	if testing.Short() {
@@ -431,14 +437,14 @@ func TestErrIntegratedCheckMetadata(t *testing.T) {
 	}
 
 	for i, in := range testInputs {
-		dbFilename := testFilenames[i%len(testFilenames)]	
+		dbFilename := testFilenames[i%len(testFilenames)]
 		dbFilepath := filepath.Join(testDir, dbFilename)
 
 		boltDB, err := OpenDatabase(in.md, dbFilepath)
 		if err != nil {
 			t.Errorf("OpenDatabase failed on input %v, filename %v; error was %v", in, dbFilename, err)
 		}
-		
+
 		err = boltDB.Close()
 		if err != nil {
 			t.Fatal(err)
