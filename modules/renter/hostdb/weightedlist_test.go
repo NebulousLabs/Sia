e3fadf663014d25c6cb06d2ccd67ca0280b050bc
David Vorick <david.vorick@gmail.com>
2016-06-28 18:23:41 -0400

    add a scanning consistency test
    The test is failing as of this commit, as a part of TDD we wanted to
    make sure we had confirmed the problem.
    
    Test should be passing again as of the next commit

--- modules/renter/hostdb/weightedlist_test.go
+++ modules/renter/hostdb/weightedlist_test.go
@@ -2,7 +2,6 @@ package hostdb
 
 import (
 	"crypto/rand"
-	"errors"
 	"math/big"
 	"strconv"
 	"testing"
@@ -15,73 +14,6 @@ import (
 // addresses are needed in order to satisfy the HostDB's "1 host per IP" rule.
 func fakeAddr(n uint8) modules.NetAddress {
 	return modules.NetAddress("127.0.0." + strconv.Itoa(int(n)) + ":1")
-}
-
-// uniformTreeVerification checks that everything makes sense in the tree given
-// the number of entries that the tree is supposed to have and also given that
-// every entropy has the same weight.
-func uniformTreeVerification(hdb *HostDB, numEntries int) error {
-	// Check that the weight of the hostTree is what is expected.
-	expectedWeight := hdb.hostTree.hostEntry.Weight.Mul64(uint64(numEntries))
-	if hdb.hostTree.weight.Cmp(expectedWeight) != 0 {
-		return errors.New("expected weight is incorrect")
-	}
-
-	// Check that the length of activeHosts and the count of hostTree are
-	// consistent.
-	if len(hdb.activeHosts) != numEntries {
-		return errors.New("unexpected number of active hosts")
-	}
-
-	// Select many random hosts and do naive statistical analysis on the
-	// results.
-	if !testing.Short() {
-		// Pull a bunch of random hosts and count how many times we pull each
-		// host.
-		selectionMap := make(map[modules.NetAddress]int)
-		expected := 100
-		for i := 0; i < expected*numEntries; i++ {
-			entries := hdb.RandomHosts(1, nil)
-			if len(entries) == 0 {
-				return errors.New("no hosts")
-			}
-			selectionMap[entries[0].NetAddress]++
-		}
-
-		// See if each host was selected enough times.
-		errorBound := 64 // Pretty large, but will still detect if something is seriously wrong.
-		for _, count := range selectionMap {
-			if count < expected-errorBound || count > expected+errorBound {
-				return errors.New("error bound was breached")
-			}
-		}
-	}
-
-	// Try removing an re-adding all hosts.
-	var removedEntries []*hostEntry
-	for {
-		if hdb.hostTree.weight.IsZero() {
-			break
-		}
-		randWeight, err := rand.Int(rand.Reader, hdb.hostTree.weight.Big())
-		if err != nil {
-			break
-		}
-		node, err := hdb.hostTree.nodeAtWeight(types.NewCurrency(randWeight))
-		if err != nil {
-			break
-		}
-		node.removeNode()
-		delete(hdb.activeHosts, node.hostEntry.NetAddress)
-
-		// remove the entry from the hostdb so it won't be selected as a
-		// repeat.
-		removedEntries = append(removedEntries, node.hostEntry)
-	}
-	for _, entry := range removedEntries {
-		hdb.insertNode(entry)
-	}
-	return nil
 }
 
 // TestWeightedList inserts and removes nodes in a semi-random manner and
