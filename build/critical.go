dd99da3ec4eb18edeeb68b948a3d24568d2ae778
Jordan Horwich <jordan@voidingwarranties.com>
2016-06-28 13:23:39 -0400

    Direct users to file a bug report on build.Criticals

--- build/critical.go
+++ build/critical.go
@@ -8,7 +8,7 @@ import (
 // Critical will print a message to os.Stderr unless DEBUG has been set, in
 // which case panic will be called instead.
 func Critical(v ...interface{}) {
-	s := fmt.Sprintln(v...)
+	s := "Critical error: " + fmt.Sprintln(v...) + "Please submit a bug report here: https://github.com/NebulousLabs/Sia/issues\n"
 	if DEBUG {
 		panic(s)
 	}
