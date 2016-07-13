d3a5bdb3edc0ed0cb0eda783410365bbbc90611c
David Vorick <david.vorick@gmail.com>
2016-07-13 18:11:54 -0400

    remove renter share and load siac commands

--- siac/main.go
+++ siac/main.go
@@ -266,9 +266,8 @@ func main() {
 	root.AddCommand(renterCmd)
 	renterCmd.AddCommand(renterFilesDeleteCmd, renterFilesDownloadCmd,
 		renterDownloadsCmd, renterAllowanceCmd, renterSetAllowanceCmd,
-		renterContractsCmd, renterFilesListCmd, renterFilesLoadCmd,
-		renterFilesLoadASCIICmd, renterFilesRenameCmd, renterFilesShareCmd,
-		renterFilesShareASCIICmd, renterFilesUploadCmd, renterUploadsCmd)
+		renterContractsCmd, renterFilesListCmd, renterFilesRenameCmd,
+		renterFilesUploadCmd, renterUploadsCmd)
 	renterCmd.Flags().BoolVarP(&renterListVerbose, "verbose", "v", false, "Show additional file info such as redundancy")
 	renterDownloadsCmd.Flags().BoolVarP(&renterShowHistory, "history", "H", false, "Show download history in addition to the download queue")
 	renterFilesListCmd.Flags().BoolVarP(&renterListVerbose, "verbose", "v", false, "Show additional file info such as redundancy")
