0eea77ff526c785f491b75b3d55062ea68f0da47
lukechampine <luke.champine@gmail.com>
2016-07-28 17:55:51 -0400

    store historic fields in database

--- modules/wallet/update.go
+++ modules/wallet/update.go
@@ -6,6 +6,8 @@ import (
 	"github.com/NebulousLabs/Sia/build"
 	"github.com/NebulousLabs/Sia/modules"
 	"github.com/NebulousLabs/Sia/types"
+
+	"github.com/NebulousLabs/bolt"
 )
 
 // updateConfirmedSet uses a consensus change to update the confirmed set of
@@ -94,115 +96,151 @@ func (w *Wallet) revertHistory(cc modules.ConsensusChange) {
 // applyHistory applies any transaction history that was introduced by the
 // applied blocks.
 func (w *Wallet) applyHistory(cc modules.ConsensusChange) {
-	for _, block := range cc.AppliedBlocks {
-		w.consensusSetHeight++
-		// Apply the miner payout transaction if applicable.
-		minerPT := modules.ProcessedTransaction{
-			Transaction:           types.Transaction{},
-			TransactionID:         types.TransactionID(block.ID()),
-			ConfirmationHeight:    w.consensusSetHeight,
-			ConfirmationTimestamp: block.Timestamp,
-		}
-		relevant := false
-		for i, mp := range block.MinerPayouts {
-			_, exists := w.keys[mp.UnlockHash]
-			if exists {
-				relevant = true
-			}
-			minerPT.Outputs = append(minerPT.Outputs, modules.ProcessedOutput{
-				FundType:       types.SpecifierMinerPayout,
-				MaturityHeight: w.consensusSetHeight + types.MaturityDelay,
-				WalletAddress:  exists,
-				RelatedAddress: mp.UnlockHash,
-				Value:          mp.Value,
-			})
-			w.historicOutputs[types.OutputID(block.MinerPayoutID(uint64(i)))] = mp.Value
-		}
-		if relevant {
-			w.processedTransactions = append(w.processedTransactions, minerPT)
-			w.processedTransactionMap[minerPT.TransactionID] = &w.processedTransactions[len(w.processedTransactions)-1]
-		}
-		for _, txn := range block.Transactions {
-			relevant := false
-			pt := modules.ProcessedTransaction{
-				Transaction:           txn,
-				TransactionID:         txn.ID(),
+	err := w.db.Update(func(tx *bolt.Tx) error {
+		for _, block := range cc.AppliedBlocks {
+			w.consensusSetHeight++
+			// Apply the miner payout transaction if applicable.
+			minerPT := modules.ProcessedTransaction{
+				Transaction:           types.Transaction{},
+				TransactionID:         types.TransactionID(block.ID()),
 				ConfirmationHeight:    w.consensusSetHeight,
 				ConfirmationTimestamp: block.Timestamp,
 			}
-			for _, sci := range txn.SiacoinInputs {
-				_, exists := w.keys[sci.UnlockConditions.UnlockHash()]
+			relevant := false
+			for i, mp := range block.MinerPayouts {
+				_, exists := w.keys[mp.UnlockHash]
 				if exists {
 					relevant = true
 				}
-				pt.Inputs = append(pt.Inputs, modules.ProcessedInput{
-					FundType:       types.SpecifierSiacoinInput,
-					WalletAddress:  exists,
-					RelatedAddress: sci.UnlockConditions.UnlockHash(),
-					Value:          w.historicOutputs[types.OutputID(sci.ParentID)],
-				})
-			}
-			for i, sco := range txn.SiacoinOutputs {
-				_, exists := w.keys[sco.UnlockHash]
-				if exists {
-					relevant = true
-				}
-				pt.Outputs = append(pt.Outputs, modules.ProcessedOutput{
-					FundType:       types.SpecifierSiacoinOutput,
-					MaturityHeight: w.consensusSetHeight,
-					WalletAddress:  exists,
-					RelatedAddress: sco.UnlockHash,
-					Value:          sco.Value,
-				})
-				w.historicOutputs[types.OutputID(txn.SiacoinOutputID(uint64(i)))] = sco.Value
-			}
-			for _, sfi := range txn.SiafundInputs {
-				_, exists := w.keys[sfi.UnlockConditions.UnlockHash()]
-				if exists {
-					relevant = true
-				}
-				sfiValue := w.historicOutputs[types.OutputID(sfi.ParentID)]
-				pt.Inputs = append(pt.Inputs, modules.ProcessedInput{
-					FundType:       types.SpecifierSiafundInput,
-					WalletAddress:  exists,
-					RelatedAddress: sfi.UnlockConditions.UnlockHash(),
-					Value:          sfiValue,
-				})
-				claimValue := w.siafundPool.Sub(w.historicClaimStarts[sfi.ParentID]).Mul(sfiValue)
-				pt.Outputs = append(pt.Outputs, modules.ProcessedOutput{
-					FundType:       types.SpecifierClaimOutput,
+				minerPT.Outputs = append(minerPT.Outputs, modules.ProcessedOutput{
+					FundType:       types.SpecifierMinerPayout,
 					MaturityHeight: w.consensusSetHeight + types.MaturityDelay,
 					WalletAddress:  exists,
-					RelatedAddress: sfi.ClaimUnlockHash,
-					Value:          claimValue,
+					RelatedAddress: mp.UnlockHash,
+					Value:          mp.Value,
 				})
-			}
-			for i, sfo := range txn.SiafundOutputs {
-				_, exists := w.keys[sfo.UnlockHash]
-				if exists {
-					relevant = true
+				err := dbPutHistoricOutput(tx, types.OutputID(block.MinerPayoutID(uint64(i))), mp.Value)
+				if err != nil {
+					return err
 				}
-				pt.Outputs = append(pt.Outputs, modules.ProcessedOutput{
-					FundType:       types.SpecifierSiafundOutput,
-					MaturityHeight: w.consensusSetHeight,
-					WalletAddress:  exists,
-					RelatedAddress: sfo.UnlockHash,
-					Value:          sfo.Value,
-				})
-				w.historicOutputs[types.OutputID(txn.SiafundOutputID(uint64(i)))] = sfo.Value
-				w.historicClaimStarts[txn.SiafundOutputID(uint64(i))] = sfo.ClaimStart
-			}
-			for _, fee := range txn.MinerFees {
-				pt.Outputs = append(pt.Outputs, modules.ProcessedOutput{
-					FundType: types.SpecifierMinerFee,
-					Value:    fee,
-				})
 			}
 			if relevant {
-				w.processedTransactions = append(w.processedTransactions, pt)
-				w.processedTransactionMap[pt.TransactionID] = &w.processedTransactions[len(w.processedTransactions)-1]
+				w.processedTransactions = append(w.processedTransactions, minerPT)
+				w.processedTransactionMap[minerPT.TransactionID] = &w.processedTransactions[len(w.processedTransactions)-1]
+			}
+			for _, txn := range block.Transactions {
+				relevant := false
+				pt := modules.ProcessedTransaction{
+					Transaction:           txn,
+					TransactionID:         txn.ID(),
+					ConfirmationHeight:    w.consensusSetHeight,
+					ConfirmationTimestamp: block.Timestamp,
+				}
+
+				for _, sci := range txn.SiacoinInputs {
+					_, exists := w.keys[sci.UnlockConditions.UnlockHash()]
+					if exists {
+						relevant = true
+					}
+					val, err := dbGetHistoricOutput(tx, types.OutputID(sci.ParentID))
+					if err != nil {
+						return err
+					}
+					pt.Inputs = append(pt.Inputs, modules.ProcessedInput{
+						FundType:       types.SpecifierSiacoinInput,
+						WalletAddress:  exists,
+						RelatedAddress: sci.UnlockConditions.UnlockHash(),
+						Value:          val,
+					})
+				}
+
+				for i, sco := range txn.SiacoinOutputs {
+					_, exists := w.keys[sco.UnlockHash]
+					if exists {
+						relevant = true
+					}
+					pt.Outputs = append(pt.Outputs, modules.ProcessedOutput{
+						FundType:       types.SpecifierSiacoinOutput,
+						MaturityHeight: w.consensusSetHeight,
+						WalletAddress:  exists,
+						RelatedAddress: sco.UnlockHash,
+						Value:          sco.Value,
+					})
+					err := dbPutHistoricOutput(tx, types.OutputID(txn.SiacoinOutputID(uint64(i))), sco.Value)
+					if err != nil {
+						return err
+					}
+				}
+
+				for _, sfi := range txn.SiafundInputs {
+					_, exists := w.keys[sfi.UnlockConditions.UnlockHash()]
+					if exists {
+						relevant = true
+					}
+					sfiValue, err := dbGetHistoricOutput(tx, types.OutputID(sfi.ParentID))
+					if err != nil {
+						return err
+					}
+					pt.Inputs = append(pt.Inputs, modules.ProcessedInput{
+						FundType:       types.SpecifierSiafundInput,
+						WalletAddress:  exists,
+						RelatedAddress: sfi.UnlockConditions.UnlockHash(),
+						Value:          sfiValue,
+					})
+					startVal, err := dbGetHistoricClaimStart(tx, sfi.ParentID)
+					if err != nil {
+						return err
+					}
+					claimValue := w.siafundPool.Sub(startVal).Mul(sfiValue)
+					pt.Outputs = append(pt.Outputs, modules.ProcessedOutput{
+						FundType:       types.SpecifierClaimOutput,
+						MaturityHeight: w.consensusSetHeight + types.MaturityDelay,
+						WalletAddress:  exists,
+						RelatedAddress: sfi.ClaimUnlockHash,
+						Value:          claimValue,
+					})
+				}
+
+				for i, sfo := range txn.SiafundOutputs {
+					_, exists := w.keys[sfo.UnlockHash]
+					if exists {
+						relevant = true
+					}
+					pt.Outputs = append(pt.Outputs, modules.ProcessedOutput{
+						FundType:       types.SpecifierSiafundOutput,
+						MaturityHeight: w.consensusSetHeight,
+						WalletAddress:  exists,
+						RelatedAddress: sfo.UnlockHash,
+						Value:          sfo.Value,
+					})
+					id := txn.SiafundOutputID(uint64(i))
+					err := dbPutHistoricOutput(tx, types.OutputID(id), sfo.Value)
+					if err != nil {
+						return err
+					}
+					err = dbPutHistoricClaimStart(tx, id, sfo.ClaimStart)
+					if err != nil {
+						return err
+					}
+				}
+
+				for _, fee := range txn.MinerFees {
+					pt.Outputs = append(pt.Outputs, modules.ProcessedOutput{
+						FundType: types.SpecifierMinerFee,
+						Value:    fee,
+					})
+				}
+				if relevant {
+					w.processedTransactions = append(w.processedTransactions, pt)
+					w.processedTransactionMap[pt.TransactionID] = &w.processedTransactions[len(w.processedTransactions)-1]
+				}
 			}
 		}
+
+		return nil
+	})
+	if err != nil {
+		w.log.Println("ERROR: failed to add consensus change:", err)
 	}
 }
 
@@ -246,32 +284,44 @@ func (w *Wallet) ReceiveUpdatedUnconfirmedTransactions(txns []types.Transaction,
 			ConfirmationHeight:    types.BlockHeight(math.MaxUint64),
 			ConfirmationTimestamp: types.Timestamp(math.MaxUint64),
 		}
-		for _, sci := range txn.SiacoinInputs {
-			_, exists := w.keys[sci.UnlockConditions.UnlockHash()]
-			if exists {
-				relevant = true
+		w.db.Update(func(tx *bolt.Tx) error {
+			for _, sci := range txn.SiacoinInputs {
+				_, exists := w.keys[sci.UnlockConditions.UnlockHash()]
+				if exists {
+					relevant = true
+				}
+				val, err := dbGetHistoricOutput(tx, types.OutputID(sci.ParentID))
+				if err != nil {
+					return err
+				}
+				pt.Inputs = append(pt.Inputs, modules.ProcessedInput{
+					FundType:       types.SpecifierSiacoinInput,
+					WalletAddress:  exists,
+					RelatedAddress: sci.UnlockConditions.UnlockHash(),
+					Value:          val,
+				})
 			}
-			pt.Inputs = append(pt.Inputs, modules.ProcessedInput{
-				FundType:       types.SpecifierSiacoinInput,
-				WalletAddress:  exists,
-				RelatedAddress: sci.UnlockConditions.UnlockHash(),
-				Value:          w.historicOutputs[types.OutputID(sci.ParentID)],
-			})
-		}
-		for i, sco := range txn.SiacoinOutputs {
-			_, exists := w.keys[sco.UnlockHash]
-			if exists {
-				relevant = true
+
+			for i, sco := range txn.SiacoinOutputs {
+				_, exists := w.keys[sco.UnlockHash]
+				if exists {
+					relevant = true
+				}
+				pt.Outputs = append(pt.Outputs, modules.ProcessedOutput{
+					FundType:       types.SpecifierSiacoinOutput,
+					MaturityHeight: types.BlockHeight(math.MaxUint64),
+					WalletAddress:  exists,
+					RelatedAddress: sco.UnlockHash,
+					Value:          sco.Value,
+				})
+				err := dbPutHistoricOutput(tx, types.OutputID(txn.SiacoinOutputID(uint64(i))), sco.Value)
+				if err != nil {
+					return err
+				}
 			}
-			pt.Outputs = append(pt.Outputs, modules.ProcessedOutput{
-				FundType:       types.SpecifierSiacoinOutput,
-				MaturityHeight: types.BlockHeight(math.MaxUint64),
-				WalletAddress:  exists,
-				RelatedAddress: sco.UnlockHash,
-				Value:          sco.Value,
-			})
-			w.historicOutputs[types.OutputID(txn.SiacoinOutputID(uint64(i)))] = sco.Value
-		}
+
+			return nil
+		})
 		for _, fee := range txn.MinerFees {
 			pt.Outputs = append(pt.Outputs, modules.ProcessedOutput{
 				FundType: types.SpecifierMinerFee,
