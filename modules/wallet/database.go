e431789970924d6b6b5f19637e6d2306e69591ed
lukechampine <luke.champine@gmail.com>
2016-08-02 03:14:47 -0400

    add database docstrings

--- modules/wallet/database.go
+++ modules/wallet/database.go
@@ -11,20 +11,34 @@ var (
 	bucketHistoricOutputs     = []byte("bucketHistoricOutputs")
 )
 
+// dbPutHistoricClaimStart stores the number of siafunds corresponding to the
+// specified SiafundOutputID. The wallet builds a mapping of output -> value
+// so that it can determine the value of siafund outputs that were not created
+// by the wallet.
 func dbPutHistoricClaimStart(tx *bolt.Tx, id types.SiafundOutputID, c types.Currency) error {
 	return tx.Bucket(bucketHistoricClaimStarts).Put(encoding.Marshal(id), encoding.Marshal(c))
 }
 
+// dbGetHistoricClaimStart retrieves the number of siafunds corresponding to
+// the specified SiafundOutputID. The wallet uses this mapping to determine
+// the value of siafund outputs that were not created by the wallet.
 func dbGetHistoricClaimStart(tx *bolt.Tx, id types.SiafundOutputID) (c types.Currency, err error) {
 	b := tx.Bucket(bucketHistoricClaimStarts).Get(encoding.Marshal(id))
 	err = encoding.Unmarshal(b, &c)
 	return
 }
 
+// dbPutHistoricOutput stores the number of coins corresponding to the
+// specified OutputID. The wallet builds a mapping of output -> value so that
+// it can determine the value of outputs that were not created
+// by the wallet.
 func dbPutHistoricOutput(tx *bolt.Tx, id types.OutputID, c types.Currency) error {
 	return tx.Bucket(bucketHistoricOutputs).Put(encoding.Marshal(id), encoding.Marshal(c))
 }
 
+// dbGetHistoricOutput retrieves the number of coins corresponding to the
+// specified OutputID. The wallet uses this mapping to determine the value of
+// outputs that were not created by the wallet.
 func dbGetHistoricOutput(tx *bolt.Tx, id types.OutputID) (c types.Currency, err error) {
 	b := tx.Bucket(bucketHistoricOutputs).Get(encoding.Marshal(id))
 	err = encoding.Unmarshal(b, &c)
