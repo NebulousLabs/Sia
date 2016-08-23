c93e7cc2a4e3a77d6e7aad60cba5350f46c26c02
lukechampine <luke.champine@gmail.com>
2016-08-23 17:09:02 -0400

    add types.Ed25519PublicKey helper

--- types/signatures_test.go
+++ types/signatures_test.go
@@ -1,10 +1,26 @@
 package types
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/NebulousLabs/Sia/crypto"
 )
+
+// TestEd25519PublicKey tests the Ed25519PublicKey function.
+func TestEd25519PublicKey(t *testing.T) {
+	_, pk, err := crypto.GenerateKeyPair()
+	if err != nil {
+		t.Fatal(err)
+	}
+	spk := Ed25519PublicKey(pk)
+	if spk.Algorithm != SignatureEd25519 {
+		t.Error("Ed25519PublicKey created key with wrong algorithm specifier:", spk.Algorithm)
+	}
+	if !bytes.Equal(spk.Key, pk[:]) {
+		t.Error("Ed25519PublicKey created key with wrong data")
+	}
+}
 
 // TestUnlockHash runs the UnlockHash code.
 func TestUnlockHash(t *testing.T) {
