81c6e8a899df840fc18efabdb2f4099f673dc59a
David Vorick <david@nebulouslabs.com>
2016-10-27 19:24:47 +0000

    add benchmarks for Read and Perm as well

--- crypto/rand_bench_test.go
+++ crypto/rand_bench_test.go
@@ -1,6 +1,7 @@
 package crypto
 
 import (
+	"crypto/rand"
 	"testing"
 )
 
@@ -26,6 +27,64 @@ func BenchmarkRandIntnSmall(b *testing.B) {
 		}
 		if i < 0 || i >= 4e3 {
 			b.Fatal("bad value for i:", i)
+		}
+	}
+}
+
+// BenchmarkRead benchmarks the speed of Read for small slices.
+func BenchmarkRead(b *testing.B) {
+	buf := make([]byte, 32)
+	for i := 0; i < b.N; i++ {
+		Read(buf)
+	}
+}
+
+// BenchmarkRead64K benchmarks the speed of Read for larger slices.
+func BenchmarkRead64K(b *testing.B) {
+	buf := make([]byte, 64e3)
+	for i := 0; i < b.N; i++ {
+		Read(buf)
+	}
+}
+
+// BenchmarkRandRead benchmarks the speed of rand.Read for small slices.
+func BenchmarkRandRead(b *testing.B) {
+	buf := make([]byte, 32)
+	for i := 0; i < b.N; i++ {
+		_, err := rand.Read(buf)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+// BenchmarkRandRead64K benchmarks the speed of rand.Read for larger slices.
+func BenchmarkRandRead64K(b *testing.B) {
+	buf := make([]byte, 64e3)
+	for i := 0; i < b.N; i++ {
+		_, err := rand.Read(buf)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+// BenchmarkPerm benchmarks the speed of Perm for small slices.
+func BenchmarkPerm(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, err := Perm(32)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+// BenchmarkPermLarge benchmarks the speed of Perm for large slices.
+func BenchmarkPermLarge(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, err := Perm(4e3)
+		if err != nil {
+			b.Fatal(err)
 		}
 	}
 }
