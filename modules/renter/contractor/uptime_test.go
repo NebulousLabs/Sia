01398e329053c6d7d3fc7b572723d9bb314a2b3c
lukechampine <luke.champine@gmail.com>
2016-12-05 13:42:18 -0500

    fixup uptime tests

--- modules/renter/contractor/uptime_test.go
+++ modules/renter/contractor/uptime_test.go
@@ -9,16 +9,18 @@ import (
 	"github.com/NebulousLabs/Sia/types"
 )
 
-// uptimeHostDB overrides an existing hostDB so that it always returns
+// offlineHostDB overrides an existing hostDB so that it always returns
 // isOffline == true for a specified address.
-type uptimeHostDB struct {
+type offlineHostDB struct {
 	hostDB
 	addr modules.NetAddress
 }
 
-func (u uptimeHostDB) Host(addr modules.NetAddress) (modules.HostDBEntry, bool) {
-	host, ok := u.hostDB.Host(addr)
-	if ok && addr == u.addr {
+// Host returns the host with address addr. If addr matches hdb.addr, the
+// host's scan history will be modified to make the host appear offline.
+func (hdb offlineHostDB) Host(addr modules.NetAddress) (modules.HostDBEntry, bool) {
+	host, ok := hdb.hostDB.Host(addr)
+	if ok && addr == hdb.addr {
 		// fake three scans over the past uptimeWindow, all of which failed
 		badScan1 := modules.HostDBScan{Timestamp: time.Now().Add(-uptimeWindow * 2), Success: false}
 		badScan2 := modules.HostDBScan{Timestamp: time.Now().Add(-uptimeWindow), Success: false}
@@ -42,7 +44,7 @@ func TestIntegrationReplaceOffline(t *testing.T) {
 	defer h.Close()
 
 	// override IsOffline to always return true for h
-	c.hdb = uptimeHostDB{c.hdb, h.ExternalSettings().NetAddress}
+	c.hdb = offlineHostDB{c.hdb, h.ExternalSettings().NetAddress}
 
 	// create another host
 	dir := build.TempDir("contractor", "TestIntegrationMonitorUptime", "Host2")
@@ -103,9 +105,11 @@ func TestIntegrationReplaceOffline(t *testing.T) {
 func TestIsOffline(t *testing.T) {
 	now := time.Now()
 	oldBadScan := modules.HostDBScan{Timestamp: now.Add(-uptimeWindow * 2), Success: false}
+	oldGoodScan := modules.HostDBScan{Timestamp: now.Add(-uptimeWindow * 2), Success: true}
 	newBadScan := modules.HostDBScan{Timestamp: now.Add(-uptimeWindow / 2), Success: false}
 	newGoodScan := modules.HostDBScan{Timestamp: now.Add(-uptimeWindow / 2), Success: true}
 	currentBadScan := modules.HostDBScan{Timestamp: now, Success: false}
+	currentGoodScan := modules.HostDBScan{Timestamp: now, Success: true}
 
 	tests := []struct {
 		scans   []modules.HostDBScan
@@ -121,6 +125,10 @@ func TestIsOffline(t *testing.T) {
 		{[]modules.HostDBScan{oldBadScan, newGoodScan, currentBadScan}, false},
 		// data covers large range, no scans succeded
 		{[]modules.HostDBScan{oldBadScan, newBadScan, currentBadScan}, true},
+		// recent scan was good (and within uptimeWindow of oldBadScan, but that shouldn't matter)
+		{[]modules.HostDBScan{oldBadScan, oldBadScan, oldBadScan, oldGoodScan}, false},
+		// recent scan was good (and outside uptimeWindow of oldBadScan, but that shouldn't matter)
+		{[]modules.HostDBScan{oldBadScan, oldBadScan, oldBadScan, currentGoodScan}, false},
 	}
 	for i, test := range tests {
 		h := modules.HostDBEntry{ScanHistory: test.scans}
