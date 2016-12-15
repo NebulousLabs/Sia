45268ada33712a8b4a1b894d69ab2bac9e1b25f4
lukechampine <luke.champine@gmail.com>
2016-12-15 16:41:53 -0500

    check for cachedRevisions in TestSaveLoad

--- modules/renter/contractor/persist_test.go
+++ modules/renter/contractor/persist_test.go
@@ -20,33 +20,42 @@ func (m memPersist) load(data *contractorPersist) error     { *data = contractor
 func TestSaveLoad(t *testing.T) {
 	// create contractor with mocked persist dependency
 	c := &Contractor{
-		contracts:  make(map[types.FileContractID]modules.RenterContract),
-		renewedIDs: make(map[types.FileContractID]types.FileContractID),
+		contracts:       make(map[types.FileContractID]modules.RenterContract),
+		renewedIDs:      make(map[types.FileContractID]types.FileContractID),
+		cachedRevisions: make(map[types.FileContractID]cachedRevision),
 	}
 	c.persist = new(memPersist)
 
 	// add some fake contracts
 	c.contracts = map[types.FileContractID]modules.RenterContract{
-		{0}: {NetAddress: "foo"},
-		{1}: {NetAddress: "bar"},
-		{2}: {NetAddress: "baz"},
+		{0}: {ID: types.FileContractID{0}, NetAddress: "foo"},
+		{1}: {ID: types.FileContractID{1}, NetAddress: "bar"},
+		{2}: {ID: types.FileContractID{2}, NetAddress: "baz"},
 	}
 	c.renewedIDs = map[types.FileContractID]types.FileContractID{
 		{0}: {1},
 		{1}: {2},
 		{2}: {3},
 	}
+	c.cachedRevisions = map[types.FileContractID]cachedRevision{
+		{0}: {Revision: types.FileContractRevision{ParentID: types.FileContractID{0}}},
+		{1}: {Revision: types.FileContractRevision{ParentID: types.FileContractID{1}}},
+		{2}: {Revision: types.FileContractRevision{ParentID: types.FileContractID{2}}},
+	}
 
-	// save and reload
+	// save, clear, and reload
 	err := c.save()
 	if err != nil {
 		t.Fatal(err)
 	}
+	c.contracts = make(map[types.FileContractID]modules.RenterContract)
+	c.renewedIDs = make(map[types.FileContractID]types.FileContractID)
+	c.cachedRevisions = make(map[types.FileContractID]cachedRevision)
 	err = c.load()
 	if err != nil {
 		t.Fatal(err)
 	}
-	// check that contracts were restored
+	// check that all fields were restored
 	_, ok0 := c.contracts[types.FileContractID{0}]
 	_, ok1 := c.contracts[types.FileContractID{1}]
 	_, ok2 := c.contracts[types.FileContractID{2}]
@@ -59,21 +68,30 @@ func TestSaveLoad(t *testing.T) {
 	if !ok0 || !ok1 || !ok2 {
 		t.Fatal("renewed IDs were not restored properly:", c.renewedIDs)
 	}
+	_, ok0 = c.cachedRevisions[types.FileContractID{0}]
+	_, ok1 = c.cachedRevisions[types.FileContractID{1}]
+	_, ok2 = c.cachedRevisions[types.FileContractID{2}]
+	if !ok0 || !ok1 || !ok2 {
+		t.Fatal("cached revisions were not restored properly:", c.cachedRevisions)
+	}
 
 	// use stdPersist instead of mock
 	c.persist = newPersist(build.TempDir("contractor", "TestSaveLoad"))
 	os.MkdirAll(build.TempDir("contractor", "TestSaveLoad"), 0700)
 
-	// save and reload
+	// save, clear, and reload
 	err = c.save()
 	if err != nil {
 		t.Fatal(err)
 	}
+	c.contracts = make(map[types.FileContractID]modules.RenterContract)
+	c.renewedIDs = make(map[types.FileContractID]types.FileContractID)
+	c.cachedRevisions = make(map[types.FileContractID]cachedRevision)
 	err = c.load()
 	if err != nil {
 		t.Fatal(err)
 	}
-	// check that contracts were restored
+	// check that all fields were restored
 	_, ok0 = c.contracts[types.FileContractID{0}]
 	_, ok1 = c.contracts[types.FileContractID{1}]
 	_, ok2 = c.contracts[types.FileContractID{2}]
@@ -85,5 +103,11 @@ func TestSaveLoad(t *testing.T) {
 	_, ok2 = c.renewedIDs[types.FileContractID{2}]
 	if !ok0 || !ok1 || !ok2 {
 		t.Fatal("renewed IDs were not restored properly:", c.renewedIDs)
+	}
+	_, ok0 = c.cachedRevisions[types.FileContractID{0}]
+	_, ok1 = c.cachedRevisions[types.FileContractID{1}]
+	_, ok2 = c.cachedRevisions[types.FileContractID{2}]
+	if !ok0 || !ok1 || !ok2 {
+		t.Fatal("cached revisions were not restored properly:", c.cachedRevisions)
 	}
 }
