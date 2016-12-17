18e4a1bf5266e8ee3d75190c8cbd0980a3ef5c5f
David Vorick <david@nebulouslabs.com>
2016-12-17 11:10:34 -0500

    add missing downloadqueue file

---
+++ modules/renter/downloadqueue.go
@@ -0,0 +1,58 @@
+package renter
+
+import (
+	"errors"
+	"sync/atomic"
+
+	"github.com/NebulousLabs/Sia/modules"
+)
+
+// Download downloads a file, identified by its path, to the destination
+// specified.
+func (r *Renter) Download(path, destination string) error {
+	// Lookup the file associated with the nickname.
+	lockID := r.mu.RLock()
+	file, exists := r.files[path]
+	r.mu.RUnlock(lockID)
+	if !exists {
+		return errors.New("no file with that path")
+	}
+
+	// Create the download object and add it to the queue.
+	d := newDownload(file, destination)
+	lockID = r.mu.Lock()
+	r.downloadQueue = append(r.downloadQueue, d)
+	r.mu.Unlock(lockID)
+	r.newDownloads <- d
+
+	// Block until the download has completed.
+	//
+	// TODO: Eventually just return the channel to the error instead of the
+	// error itself.
+	select {
+	case err := <-d.downloadFinished:
+		return err
+	case <-r.tg.StopChan():
+		return errors.New("download interrupted by shutdown")
+	}
+}
+
+// DownloadQueue returns the list of downloads in the queue.
+func (r *Renter) DownloadQueue() []modules.DownloadInfo {
+	lockID := r.mu.RLock()
+	defer r.mu.RUnlock(lockID)
+
+	// order from most recent to least recent
+	downloads := make([]modules.DownloadInfo, len(r.downloadQueue))
+	for i := range r.downloadQueue {
+		d := r.downloadQueue[len(r.downloadQueue)-i-1]
+		downloads[i] = modules.DownloadInfo{
+			SiaPath:     d.siapath,
+			Destination: d.destination,
+			Filesize:    d.fileSize,
+			StartTime:   d.startTime,
+		}
+		downloads[i].Received = atomic.LoadUint64(&d.atomicDataReceived)
+	}
+	return downloads
+}
