d6a036f0b9641a3863f6febae2b7732827fa9ccb
Johnathan Howell <me@johnathanhowell.com>
2017-01-10 22:46:31 -0500

    address review comments

--- modules/wallet/defrag.go
+++ modules/wallet/defrag.go
@@ -10,6 +10,9 @@ const (
 	// defragThreshold is the number of outputs a wallet is allowed before it is
 	// defragmented.
 	defragThreshold = 20
+
+	// defragBatchSize defines how many outputs are combined during one defrag.
+	defragBatchSize = 15
 )
 
 // defragWallet computes the sum of the 15 largest outputs in the wallet and
@@ -21,6 +24,8 @@ func (w *Wallet) defragWallet() {
 		return
 	}
 
+	// grab all the outputs from the wallet and sort them from
+	// largest -> smallest
 	var so sortedOutputs
 	for scoid, sco := range w.siacoinOutputs {
 		so.ids = append(so.ids, scoid)
@@ -28,7 +33,8 @@ func (w *Wallet) defragWallet() {
 	}
 	sort.Sort(sort.Reverse(so))
 
-	defragOutputs := so.outputs[:15]
+	// choose the defragBatchSizeth largest outputs from the wallet and sum them
+	defragOutputs := so.outputs[:defragBatchSize]
 	var totalOutputValue types.Currency
 	for _, output := range defragOutputs {
 		totalOutputValue = totalOutputValue.Add(output.Value)
@@ -36,14 +42,17 @@ func (w *Wallet) defragWallet() {
 
 	w.log.Printf("defragmenting wallet: %v outputs, %v total value\n", len(defragOutputs), totalOutputValue)
 
-	var dest types.UnlockHash
-	for h := range w.keys {
-		dest = h
-		break
+	// grab a new address from the wallet
+	addr, err := w.nextPrimarySeedAddress()
+	if err != nil {
+		w.log.Println("error getting an address for defrag: ", err)
+		return
 	}
 
+	// send the sum of the outputs to this wallet. This operation is done in a
+	// goroutine since defragWallet() is called under lock.
 	go func() {
-		_, err := w.SendSiacoins(totalOutputValue, dest)
+		_, err := w.SendSiacoins(totalOutputValue, addr.UnlockHash())
 		if err != nil {
 			w.log.Println("error after call to SendSiacoins: ", err)
 		}
