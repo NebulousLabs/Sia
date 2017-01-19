75bd4aefbde3d978287198d6f18e6b28f2d41d0a
lukechampine <luke.champine@gmail.com>
2017-01-19 01:51:18 -0500

    bump version to v1.1.0

--- build/version.go
+++ build/version.go
@@ -7,7 +7,7 @@ import (
 
 const (
 	// Version is the current version of siad.
-	Version = "1.0.4"
+	Version = "1.1.0"
 
 	// MaxEncodedVersionLength is the maximum length of a version string encoded
 	// with the encode package. 100 is much larger than any version number we send
