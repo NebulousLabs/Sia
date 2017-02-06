5b01951283c70dafc2cc6cd54ee635e5d65bbcfc
lukechampine <luke.champine@gmail.com>
2017-02-06 15:23:21 -0500

    don't ping in managedAcceptConnNewPeer

--- modules/gateway/peers.go
+++ modules/gateway/peers.go
@@ -182,10 +182,6 @@ func (g *Gateway) managedAcceptConnNewPeer(conn net.Conn, remoteVersion string) 
 		return err
 	}
 
-	// Attempt to ping the supplied address. If successful, we will add
-	// remoteAddr to our node list after accepting the peer.
-	pingSucceeded := g.pingNode(remoteAddr) == nil
-
 	g.mu.Lock()
 	defer g.mu.Unlock()
 
@@ -205,11 +201,12 @@ func (g *Gateway) managedAcceptConnNewPeer(conn net.Conn, remoteVersion string) 
 		},
 		sess: muxado.Server(conn),
 	})
-	if pingSucceeded {
-		g.addNode(remoteAddr)
-		return g.save()
-	}
-	return nil
+	// Add remoteAddr to the node list. The peer may have provided an
+	// unreachable address, but if so, it will eventually be filtered out by
+	// permanentNodePurger. The alternative is to ping remoteAddr immediately,
+	// which feels more susceptible to DOS attacks.
+	g.addNode(remoteAddr)
+	return g.save()
 }
 
 // acceptPeer makes room for the peer if necessary by kicking out existing
