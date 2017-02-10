60964006ae006ddd9a8dd7c3bbf6a6c38a148a7c
lukechampine <luke.champine@gmail.com>
2017-02-09 21:28:39 -0500

    add TestLoadContractPubKeys

--- modules/renter/contractor/persist_test.go
+++ modules/renter/contractor/persist_test.go
@@ -128,3 +128,60 @@ func TestSaveLoad(t *testing.T) {
 		t.Fatal("oldContracts were not restored properly:", c.oldContracts)
 	}
 }
+
+// allHostDB is a hostDB that implements the AllHosts method via a simple
+// slice.
+type allHostDB struct {
+	stubHostDB
+	hosts []modules.HostDBEntry
+}
+
+func (a allHostDB) AllHosts() []modules.HostDBEntry { return a.hosts }
+
+// TestLoadContractPubKeys tests that the contractor correctly fills in the
+// public keys of old contracts.
+func TestLoadContractPubKeys(t *testing.T) {
+	// create set of contracts and hosts
+	var hdb allHostDB
+	contracts := make(map[types.FileContractID]modules.RenterContract)
+	strs := []string{"foo", "bar", "baz"}
+	for i, str := range strs {
+		var entry modules.HostDBEntry
+		entry.NetAddress = modules.NetAddress(str)
+		entry.PublicKey = types.SiaPublicKey{Key: []byte(str)}
+		hdb.hosts = append(hdb.hosts, entry)
+
+		id := types.FileContractID{byte(i)}
+		contracts[id] = modules.RenterContract{ID: id, NetAddress: modules.NetAddress(str)}
+	}
+
+	// create contractor with mocked persist and hostdb dependencies
+	c := &Contractor{
+		persist:   new(memPersist),
+		hdb:       hdb,
+		contracts: contracts,
+	}
+
+	// save, clear, and reload
+	err := c.save()
+	if err != nil {
+		t.Fatal(err)
+	}
+	c.contracts = make(map[types.FileContractID]modules.RenterContract)
+	err = c.load()
+	if err != nil {
+		t.Fatal(err)
+	}
+	// check that contracts were loaded
+	for i, str := range strs {
+		id := types.FileContractID{byte(i)}
+		contract, ok := c.contracts[id]
+		if !ok {
+			t.Fatal("contracts were not restored properly:", c.contracts)
+		}
+		// check that pubkey was filled in
+		if string(contract.HostPublicKey.Key) != str {
+			t.Errorf("contract has wrong pubkey: expected %q, got %q", str, contract.HostPublicKey.Key)
+		}
+	}
+}
