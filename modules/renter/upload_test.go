263965b3e1f26b312ccb07a674869d21bd6cce5e
Johnathan Howell <me@johnathanhowell.com>
2017-02-13 19:05:08 -0500

    add upload_test.go

---
+++ modules/renter/upload_test.go
@@ -0,0 +1,33 @@
+package renter
+
+import (
+	"testing"
+)
+
+// TestRenterSiapathValidate verifies that the validateSiapath function correctly validates SiaPaths.
+func TestRenterSiapathValidate(t *testing.T) {
+	var pathtests = []struct {
+		in    string
+		valid bool
+	}{
+		{"valid/siapath", true},
+		{"../../../directory/traversal", false},
+		{"testpath", true},
+		{"valid/siapath/../with/directory/traversal", false},
+		{"validpath/test", true},
+		{"..validpath/..test", true},
+		{"./valid/path", true},
+		{"test/path", true},
+		{"/leading/slash", false},
+		{"", false},
+	}
+	for _, pathtest := range pathtests {
+		err := validateSiapath(pathtest.in)
+		if err != nil && pathtest.valid {
+			t.Fatal("validateSiapath failed on valid path: ", pathtest.in)
+		}
+		if err == nil && !pathtest.valid {
+			t.Fatal("validateSiapath succeeded on invalid path: ", pathtest.in)
+		}
+	}
+}
