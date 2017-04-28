449ae1027870022b1975b4d1499c153b59beb4bc
Bjorn Dolk <debian@bjorndolk.com>
2017-04-28 23:49:53 +0200

    removed source entry

--- siac/mangencmd.go
+++ siac/mangencmd.go
@@ -10,12 +10,19 @@ var (
 		Use:   "man-generation [path]",
 		Short: "Creates unix style manpages.",
 		Long: "Creates a man pages at the specified " +
-			"directory.\n\n" 
+			"directory.\n\n",
 
 		Run: wrap(mangencmd),
 	}
 )
 
 func mangencmd(path string) {
-	rootCmd.GenManTree(path)
+	header := &doc.GenManHeader{
+		Section: "1",
+		Manual:  "siac Manual",
+		Source:  "",
+	}
+
+
+	doc.GenManTree(rootCmd, header, "/tmp")
 }
