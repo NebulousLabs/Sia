9634555f736a712563fc026a976e4c8f7f864475
David Vorick <david@nebulouslabs.com>
2017-05-27 05:59:35 -0400

    clean up tpool raw testing

--- api/transactionpool.go
+++ api/transactionpool.go
@@ -17,11 +17,12 @@ type (
 	// format, along with the id of that transaction.
 	TpoolRawGET struct {
 		ID          types.TransactionID `json:"id"`
-		Parents     string              `json:"parents"`
-		Transaction string              `json:"transaction"`
+		Parents     []byte              `json:"parents"`
+		Transaction []byte              `json:"transaction"`
 	}
 )
 
+// decodeTransactionID will decode a transaction id from a string.
 func decodeTransactionID(txidStr string) (types.TransactionID, error) {
 	txid := new(crypto.Hash)
 	err := txid.LoadString(txidStr)
@@ -47,8 +48,8 @@ func (api *API) tpoolRawHandlerGET(w http.ResponseWriter, req *http.Request, ps 
 
 	WriteJSON(w, TpoolRawGET{
 		ID:          txid,
-		Parents:     base64.StdEncoding.EncodeToString(encoding.Marshal(parents)),
-		Transaction: base64.StdEncoding.EncodeToString(encoding.Marshal(txn)),
+		Parents:     encoding.Marshal(parents),
+		Transaction: encoding.Marshal(txn),
 	})
 }
 
@@ -56,17 +57,18 @@ func (api *API) tpoolRawHandlerGET(w http.ResponseWriter, req *http.Request, ps 
 // it to the transaction pool, relaying it to the transaction pool's peers
 // regardless of if the set is accepted.
 func (api *API) tpoolRawHandlerPOST(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	// Try accepting the transactions both as base64 and as clean values.
 	rawParents, err := base64.StdEncoding.DecodeString(req.FormValue("parents"))
 	if err != nil {
-		WriteError(w, Error{"error decoding parents:" + err.Error()}, http.StatusBadRequest)
-		return
+		rawParents = []byte(req.FormValue("parents"))
 	}
 	rawTransaction, err := base64.StdEncoding.DecodeString(req.FormValue("transaction"))
 	if err != nil {
-		WriteError(w, Error{"error decoding transaction:" + err.Error()}, http.StatusBadRequest)
-		return
+		rawTransaction = []byte(req.FormValue("transaction"))
 	}
 
+	// Decode the transaction and parents into a transaction set that can be
+	// given to the transaction pool.
 	var parents []types.Transaction
 	var txn types.Transaction
 	err = encoding.Unmarshal(rawParents, &parents)
@@ -79,15 +81,15 @@ func (api *API) tpoolRawHandlerPOST(w http.ResponseWriter, req *http.Request, _ 
 		WriteError(w, Error{"error decoding transaction:" + err.Error()}, http.StatusBadRequest)
 		return
 	}
-
 	txnSet := append(parents, txn)
 
+	// Re-broadcast the transactions, so that they are passed to any peers that
+	// may have rejected them earlier.
 	api.tpool.Broadcast(txnSet)
 	err = api.tpool.AcceptTransactionSet(txnSet)
 	if err != nil && err != modules.ErrDuplicateTransactionSet {
 		WriteError(w, Error{"error accepting transaction set:" + err.Error()}, http.StatusBadRequest)
 		return
 	}
-
 	WriteSuccess(w)
 }
--- api/transactionpool_test.go
+++ api/transactionpool_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"bytes"
 	"encoding/base64"
 	"net/url"
 	"testing"
@@ -10,220 +11,126 @@ import (
 	"github.com/NebulousLabs/Sia/types"
 )
 
-// TestTransactionPoolRawHandlerGET verifies that the transaction pool's raw
-// transaction getter endpoint works correctly.
-func TestTransactionPoolRawHandlerGET(t *testing.T) {
-	if testing.Short() {
-		t.SkipNow()
-	}
-	t.Parallel()
-
-	st, err := createServerTester(t.Name())
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer st.server.panicClose()
-
-	// test getting a nonexistent transaction
-	nonexistentID := types.Transaction{}.ID()
-	var trg TpoolRawGET
-	err = st.getAPI("/tpool/raw/"+nonexistentID.String(), &trg)
-	if err == nil {
-		t.Fatal("expected invalid transaction pool id to return an error")
-	}
-	if err.Error() != "transaction not found in transaction pool" {
-		t.Fatal("/tpool/raw should return not found with nonexistent transaction ID")
-	}
-
-	// submit a wallet transaction
-	sentValue := types.SiacoinPrecision.Mul64(1000)
-	txns, err := st.wallet.SendSiacoins(sentValue, types.UnlockHash{})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// verify the txns are in the pool
-	for _, txn := range txns {
-		err = st.getAPI("/tpool/raw/"+txn.ID().String(), &trg)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-
-	// verify correct parents and txn are returned
-	lastTxn := txns[len(txns)-1]
-	err = st.getAPI("/tpool/raw/"+lastTxn.ID().String(), &trg)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	parentBytes, err := base64.StdEncoding.DecodeString(trg.Parents)
-	if err != nil {
-		t.Fatal(err)
-	}
-	var decodedParents []types.Transaction
-	err = encoding.Unmarshal(parentBytes, &decodedParents)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(decodedParents) != len(txns)-1 {
-		t.Fatal("returned the incorrect number of parents")
-	}
-	txnBytes, err := base64.StdEncoding.DecodeString(trg.Transaction)
-	if err != nil {
-		t.Fatal(err)
-	}
-	var decodedTxn types.Transaction
-	err = encoding.Unmarshal(txnBytes, &decodedTxn)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if decodedTxn.ID() != lastTxn.ID() {
-		t.Fatal("tpool raw get returned the wrong transaction")
-	}
-
-	// mine a block, removing the txn from the txn pool
-	_, err = st.miner.AddBlock()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = st.getAPI("/tpool/raw/"+lastTxn.ID().String(), &trg)
-	if err.Error() != "transaction not found in transaction pool" {
-		t.Fatal("transaction should be gone from the pool after mining a block")
-	}
-}
-
-// TestTransactionPoolRawHandlerPOST verifies that the transaction pools' raw
+// TestTransactionPoolRawHandler verifies that the transaction pools' raw
 // transaction post endpoint works correctly.
 func TestTransactionPoolRawHandlerPOST(t *testing.T) {
 	if testing.Short() {
 		t.SkipNow()
 	}
 	t.Parallel()
-
 	st, err := createServerTester(t.Name())
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer st.server.panicClose()
 
+	// Spin up a second and fourth server tester, and get them all on the same
+	// block. The fourth server tester will be used later, after a third is
+	// created and used.
+	st2, err := blankServerTester(t.Name() + "-st2")
+	if err != nil {
+		t.Fatal(err)
+	}
+	st4, err := blankServerTester(t.Name() + "-st4")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = fullyConnectNodes([]*serverTester{st, st2, st4})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Reset the peers, giving them different ip addresses, preventing them
+	// from connecting to eachother.
+	err = st.server.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = st2.server.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = st4.server.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+	st, err = assembleServerTester(st.walletKey, st.dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer st.panicClose()
+	st2, err = assembleServerTester(st2.walletKey, st2.dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer st2.panicClose()
+	st4, err = assembleServerTester(st4.walletKey, st4.dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer st4.panicClose()
+
+	// Create a transaction on one node and fetch it.
 	sentValue := types.SiacoinPrecision.Mul64(1000)
 	txns, err := st.wallet.SendSiacoins(sentValue, types.UnlockHash{})
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	// spin up a new server tester that hasn't seen this txn
-	st2, err := blankServerTester(t.Name() + "-st2")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer st2.server.panicClose()
-
-	err = fullyConnectNodes([]*serverTester{st, st2})
-	if err != nil {
-		t.Fatal(err)
-	}
-
 	lastTxn := txns[len(txns)-1]
-
 	var trg TpoolRawGET
-	err = st2.getAPI("/tpool/raw/"+lastTxn.ID().String(), &trg)
-	if err.Error() != "transaction not found in transaction pool" {
-		t.Fatal("transaction should be missing initially from the second tpool")
-	}
-
-	// should be able to get and rebroadcast this transaction
 	err = st.getAPI("/tpool/raw/"+lastTxn.ID().String(), &trg)
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	// Verify the correctness of the transaction.
+	var decodedTxn types.Transaction
+	err = encoding.Unmarshal(trg.Transaction, &decodedTxn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if decodedTxn.ID() != lastTxn.ID() {
+		t.Fatal("tpool raw get returned the wrong transaction")
+	}
+	// Verify the correctness of the parents.
+	var decodedParents []types.Transaction
+	err = encoding.Unmarshal(trg.Parents, &decodedParents)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(decodedParents) != len(txns)-1 {
+		t.Fatal("returned the incorrect number of parents")
+	}
+
+	// Transaction should not be visible on node 2.
+	var trg2 TpoolRawGET
+	err = st2.getAPI("/tpool/raw/"+lastTxn.ID().String(), &trg2)
+	if err.Error() != "transaction not found in transaction pool" {
+		t.Fatal("transaction should be missing initially from the second tpool")
+	}
+
+	// Try posting the transaction to node 2.
 	postValues := url.Values{}
-	postValues.Set("parents", trg.Parents)
-	postValues.Set("transaction", trg.Transaction)
-	err = st.stdPostAPI("/tpool/raw", postValues)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	retry(60, time.Second, func() error {
-		err = st2.getAPI("/tpool/raw/"+lastTxn.ID().String(), &trg)
-		if err != nil {
-			return err
-		}
-		return nil
-	})
-}
-
-// TestTransactionPoolRawHandlersVerification verifies that the transaction
-// pool's raw endpoints get and broadcast transactions correctly.
-func TestTransactionPoolRawHandlersVerification(t *testing.T) {
-	if testing.Short() {
-		t.SkipNow()
-	}
-	t.Parallel()
-
-	// create two servers, connect them, mine a few blocks
-	st, err := createServerTester(t.Name())
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer st.server.panicClose()
-	st2, err := blankServerTester(t.Name() + "-st2")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer st2.server.panicClose()
-
-	testGroup := []*serverTester{st, st2}
-
-	err = fullyConnectNodes(testGroup)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for i := 0; i < 5; i++ {
-		_, err = st.miner.AddBlock()
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-	_, err = synchronizationCheck(testGroup)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// disconnect the servers, isolating them
-	for _, tester := range testGroup {
-		for _, peer := range tester.gateway.Peers() {
-			tester.gateway.Disconnect(peer.NetAddress)
-		}
-	}
-
-	// make a transaction on the disconnected server
-	sentValue := types.SiacoinPrecision.Mul64(1000)
-	txns, err := st.wallet.SendSiacoins(sentValue, types.UnlockHash{})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// use the tpool handlers to show the transaction to the other disconnected
-	// server
-	txn := txns[len(txns)-1]
-	var trg TpoolRawGET
-	err = st.getAPI("/tpool/raw/"+txn.ID().String(), &trg)
-	if err != nil {
-		t.Fatal(err)
-	}
-	postValues := url.Values{}
-	postValues.Set("parents", trg.Parents)
-	postValues.Set("transaction", trg.Transaction)
+	postValues.Set("parents", string(trg.Parents))
+	postValues.Set("transaction", string(trg.Transaction))
 	err = st2.stdPostAPI("/tpool/raw", postValues)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// make a new server and connect/sync it with st2
+	// Verify that the two transactions returned from each server are
+	// identical.
+	err = st2.getAPI("/tpool/raw/"+lastTxn.ID().String(), &trg2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(trg2.Parents, trg.Parents) {
+		t.Error("transaction parents mismatch")
+	}
+	if !bytes.Equal(trg2.Transaction, trg.Transaction) {
+		t.Error("transaction parents mismatch")
+	}
+
+	// Create a third server tester, connect it to the second one.
 	st3, err := blankServerTester(t.Name() + "-st3")
 	if err != nil {
 		t.Fatal(err)
@@ -233,15 +140,39 @@ func TestTransactionPoolRawHandlersVerification(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	// post the transaction to st2 again
+	// Posting the raw transaction to the second server again should cause it
+	// to be broadcast to the third server.
 	err = st2.stdPostAPI("/tpool/raw", postValues)
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	// st3 should eventually see the transaction
-	retry(60, time.Second, func() error {
-		return st3.getAPI("/tpool/raw/"+txn.ID().String(), &trg)
+	retry(100, time.Millisecond*100, func() error {
+		return st3.getAPI("/tpool/raw/"+lastTxn.ID().String(), &trg)
 	})
+
+	// Mine a block on the first server, which should clear its transaction
+	// pool.
+	_, err = st.miner.AddBlock()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = st.getAPI("/tpool/raw/"+lastTxn.ID().String(), &trg)
+	if err.Error() != "transaction not found in transaction pool" {
+		t.Fatal("transaction should be gone from the pool after mining a block")
+	}
+
+	// Convert the returned transactions to base64, which is how they will be
+	// presetned to someone using curl. Submit those to the POST endpoint. The
+	// POST endpoint should gracefully handle that submission as base64.
+	//
+	// The first 3 st's all have the transactions already, so now we'll use st4.
+	b64Parents := base64.StdEncoding.EncodeToString(trg.Parents)
+	b64Transaction := base64.StdEncoding.EncodeToString(trg.Transaction)
+	postValues = url.Values{}
+	postValues.Set("parents", b64Parents)
+	postValues.Set("transaction", b64Transaction)
+	err = st4.stdPostAPI("/tpool/raw", postValues)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
