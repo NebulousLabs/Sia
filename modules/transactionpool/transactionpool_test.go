532f129bdf85c53aae4d4136f00671845434eb76
marcinja <marcinja@mit.edu>
2017-07-13 19:49:09 -0400

    add tests with lots of swapping

--- modules/transactionpool/transactionpool_test.go
+++ modules/transactionpool/transactionpool_test.go
@@ -655,3 +655,411 @@ func TestHeapFees(t *testing.T) {
 		t.Error("Expected highest fee from second block to be greater than lowest fee from second block.")
 	}
 }
+
+func TestBigTpool(t *testing.T) {
+	if testing.Short() || !build.VLONG {
+		t.SkipNow()
+	}
+	tpt, err := createTpoolTester(t.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer tpt.Close()
+
+	// Mine a few more blocks to get some extra funding.
+	for i := 0; i < 4; i++ {
+		_, err := tpt.miner.AddBlock()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// Create transaction graph setup.
+	coinFrac := types.SiacoinPrecision.Div64(10)
+	feeFrac := types.SiacoinPrecision.Div64(10000)
+	numGraphsPerChunk := 5500
+
+	graphFund1 := types.SiacoinPrecision.Mul64(8).Add(coinFrac.Mul64(2))  //80.02 SC for chunk 1
+	graphFund2 := types.SiacoinPrecision.Mul64(44).Add(coinFrac.Mul64(2)) //200.02 SC for chunk 2
+	graphFund3 := types.SiacoinPrecision.Mul64(64).Add(coinFrac.Mul64(2)) //320.02 SC for chunk 1
+
+	var outputs1 []types.SiacoinOutput
+	var outputs2 []types.SiacoinOutput
+	var outputs3 []types.SiacoinOutput
+
+	var val1 types.Currency
+	// Create outputs to be spent in the first chunk
+	for i := 1; i <= numGraphsPerChunk; i++ {
+		value := coinFrac.Add(feeFrac.Mul64(uint64(i))).Mul64(2)
+		outputs1 = append(outputs1, types.SiacoinOutput{
+			UnlockHash: types.UnlockConditions{}.UnlockHash(),
+			Value:      value,
+		})
+		val1 = val1.Add(value)
+	}
+
+	if val1.Cmp(graphFund1) != 0 {
+		t.Log("Got: " + val1.String() + "\nExpected: " + graphFund1.String() + "\n")
+	}
+
+	// There's so many outputs that they need to be put into multiple
+	// transactions. We can fit around 500 outputs per transaction.
+	var outputTxns1 [][]types.Transaction
+	for i := 0; i < numGraphsPerChunk/500; i++ {
+		txns, err := tpt.wallet.SendSiacoinsMulti(outputs1[500*i : (500*i)+500])
+		if err != nil {
+			t.Error(err)
+		}
+		outputTxns1 = append(outputTxns1, txns)
+	}
+
+	counter := 1
+	var graphs [][]types.Transaction
+	for _, output := range outputTxns1 {
+		finalTxn := output[len(output)-1]
+		for i := 0; i < 500; i++ { //500 is the the number of outputs
+			var edges []types.TransactionGraphEdge
+			totalValue := coinFrac.Add(feeFrac.Mul64(uint64(counter))).Mul64(2)
+			fee := totalValue.Div64(100)
+			leftOver := totalValue.Sub(fee)
+			//	/println("\nFEE:")
+			//	println(fee.HumanString())
+			edges = append(edges, types.TransactionGraphEdge{
+				Dest:   1,
+				Fee:    fee,
+				Source: 0,
+				Value:  leftOver,
+			})
+			edges = append(edges, types.TransactionGraphEdge{
+				Dest:   2,
+				Fee:    leftOver.Div64(2),
+				Source: 1,
+				Value:  leftOver.Div64(2),
+			})
+
+			graph, err := types.TransactionGraph(finalTxn.SiacoinOutputID(uint64(i)), edges)
+			if err != nil {
+				t.Fatal(err)
+			}
+			graphs = append(graphs, graph)
+
+			counter++
+		}
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+	//						Chunk 2
+	////////////////////////////////////////////////////////////////////////////
+	var val2 types.Currency
+	// Create outputs to be spent in the second chunk
+	for i := 1; i <= numGraphsPerChunk; i++ {
+		value := coinFrac.Mul64(10).Add(feeFrac.Mul64(uint64(i))).Mul64(2)
+		outputs2 = append(outputs2, types.SiacoinOutput{
+			UnlockHash: types.UnlockConditions{}.UnlockHash(),
+			Value:      value,
+		})
+		val2 = val2.Add(value)
+	}
+	if val2.Cmp(graphFund2) != 0 {
+		t.Log("Got: " + val2.String() + "\nExpected: " + graphFund2.String() + "\n")
+	}
+	// There's so many outputs that they need to be put into multiple
+	// transactions. We can fit around 500 outputs per transaction.
+	var outputTxns2 [][]types.Transaction
+	for i := 0; i < numGraphsPerChunk/500; i++ {
+		txns, err := tpt.wallet.SendSiacoinsMulti(outputs2[500*i : (500*i)+500])
+		if err != nil {
+			t.Error(err)
+		}
+		outputTxns2 = append(outputTxns2, txns)
+	}
+
+	counter = 1
+	var graphs2 [][]types.Transaction
+	for _, output := range outputTxns2 {
+		finalTxn := output[len(output)-1]
+		for i := 0; i < 500; i++ { //500 is the the number of outputs
+			var edges []types.TransactionGraphEdge
+			totalValue := coinFrac.Mul64(10).Add(feeFrac.Mul64(uint64(counter))).Mul64(2)
+			fee := totalValue.Div64(100)
+			leftOver := totalValue.Sub(fee)
+			edges = append(edges, types.TransactionGraphEdge{
+				Dest:   1,
+				Fee:    fee,
+				Source: 0,
+				Value:  leftOver,
+			})
+			edges = append(edges, types.TransactionGraphEdge{
+				Dest:   2,
+				Fee:    leftOver.Div64(2),
+				Source: 1,
+				Value:  leftOver.Div64(2),
+			})
+
+			graph, err := types.TransactionGraph(finalTxn.SiacoinOutputID(uint64(i)), edges)
+			if err != nil {
+				t.Fatal(err)
+			}
+			graphs2 = append(graphs2, graph)
+
+			counter++
+		}
+	}
+
+	////////////////////////////////////////////////////////////////////////////
+	//						Chunk 3
+	////////////////////////////////////////////////////////////////////////////
+	var val3 types.Currency
+	// Create outputs to be spent in the third chunk
+	for i := 1; i <= numGraphsPerChunk; i++ {
+		value := coinFrac.Mul64(20).Add(feeFrac.Mul64(uint64(i))).Mul64(2)
+		outputs3 = append(outputs3, types.SiacoinOutput{
+			UnlockHash: types.UnlockConditions{}.UnlockHash(),
+			Value:      value,
+		})
+		val3 = val3.Add(value)
+	}
+	if val3.Cmp(graphFund3) != 0 {
+		t.Log("Got: " + val3.String() + "\nExpected: " + graphFund3.String() + "\n")
+	}
+	// There's so many outputs that they need to be put into multiple
+	// transactions. We can fit around 500 outputs per transaction.
+	var outputTxns3 [][]types.Transaction
+	for i := 0; i < numGraphsPerChunk/500; i++ {
+		txns, err := tpt.wallet.SendSiacoinsMulti(outputs3[500*i : (500*i)+500])
+		if err != nil {
+			t.Error(err)
+		}
+		outputTxns3 = append(outputTxns3, txns)
+	}
+
+	counter = 1
+	var graphs3 [][]types.Transaction
+	for _, output := range outputTxns3 {
+		finalTxn := output[len(output)-1]
+		for i := 0; i < 500; i++ { //500 is the the number of outputs
+			var edges []types.TransactionGraphEdge
+			totalValue := coinFrac.Mul64(20).Add(feeFrac.Mul64(uint64(counter))).Mul64(2)
+			fee := totalValue.Div64(100)
+			leftOver := totalValue.Sub(fee)
+			edges = append(edges, types.TransactionGraphEdge{
+				Dest:   1,
+				Fee:    fee,
+				Source: 0,
+				Value:  leftOver,
+			})
+			edges = append(edges, types.TransactionGraphEdge{
+				Dest:   2,
+				Fee:    leftOver.Div64(2),
+				Source: 1,
+				Value:  leftOver.Div64(2),
+			})
+
+			graph, err := types.TransactionGraph(finalTxn.SiacoinOutputID(uint64(i)), edges)
+			if err != nil {
+				t.Fatal(err)
+			}
+			graphs3 = append(graphs3, graph)
+
+			counter++
+		}
+	}
+
+	block, err := tpt.miner.AddBlock()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Accept the parent node of each graph so that its outputs we can test
+	// spending its outputs after mining the next block.
+	c := 0
+	for _, graph := range graphs {
+		err := tpt.tpool.AcceptTransactionSet([]types.Transaction{graph[0]})
+		if err != nil {
+			println("\nSUP")
+			println(len(graphs))
+			println(c)
+			t.Fatal(err)
+		}
+		c++
+	}
+	_, err = tpt.miner.AddBlock()
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, graph := range graphs2 {
+		err := tpt.tpool.AcceptTransactionSet([]types.Transaction{graph[0]})
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	_, err = tpt.miner.AddBlock()
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, graph := range graphs3 {
+		err := tpt.tpool.AcceptTransactionSet([]types.Transaction{graph[0]})
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	_, err = tpt.miner.AddBlock()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Accept the first half of the first chunk of transactions in a random order.
+	firstIndices := fastrand.Perm(2750)
+	for _, i := range firstIndices {
+		graph := graphs[i]
+		for _, txn := range graph[1:] {
+			err := tpt.tpool.AcceptTransactionSet([]types.Transaction{txn})
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+	}
+	// Accept the first 2000 transactions of the second chunk in a random order.
+	mixSecond := fastrand.Perm(2000)
+	for _, i := range mixSecond {
+		graph := graphs2[i]
+		for _, txn := range graph[1:] {
+			err := tpt.tpool.AcceptTransactionSet([]types.Transaction{txn})
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+	}
+	// Accept the rest of the first chunk in random order.
+	finishFirstChunk := fastrand.Perm(2750)
+	for _, i := range finishFirstChunk {
+		graph := graphs[i+2750]
+		for _, txn := range graph[1:] {
+			err := tpt.tpool.AcceptTransactionSet([]types.Transaction{txn})
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+	}
+	// Accept the next 1000 transactions of the second chunk in a random order.
+	moreSecond := fastrand.Perm(1000)
+	for _, i := range moreSecond {
+		graph := graphs2[i+2000]
+		for _, txn := range graph[1:] {
+			err := tpt.tpool.AcceptTransactionSet([]types.Transaction{txn})
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+	}
+	// Mix in the first 1000 transactions of the third chunk in a random order.
+	mixThird := fastrand.Perm(1000)
+	for _, i := range mixThird {
+		graph := graphs3[i]
+		for _, txn := range graph[1:] {
+			err := tpt.tpool.AcceptTransactionSet([]types.Transaction{txn})
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+	}
+	// Accept the last remaining transactions of the second chunk in a random order.
+	finishSecond := fastrand.Perm(2500)
+	for _, i := range finishSecond {
+		graph := graphs2[i+3000]
+		for _, txn := range graph[1:] {
+			err := tpt.tpool.AcceptTransactionSet([]types.Transaction{txn})
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+	}
+	// Accept the remaining transactions from the last chunk.
+	finishThird := fastrand.Perm(4500)
+	for _, i := range finishThird {
+		graph := graphs3[i+1000]
+		for _, txn := range graph[1:] {
+			err := tpt.tpool.AcceptTransactionSet([]types.Transaction{txn})
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+	}
+
+	block, err = tpt.miner.AddBlock()
+
+	var size int
+	var numTxns int
+	var totalFee types.Currency
+	minFee := types.SiacoinPrecision.Mul64(10000000000) // All the fees are much smaller than 1 SC.
+	for _, tx := range block.Transactions {
+		for _, fee := range tx.MinerFees {
+			totalFee = totalFee.Add(fee)
+			if fee.Cmp(minFee) < 0 {
+				minFee = fee
+			}
+		}
+		size += tx.MarshalSiaSize()
+		numTxns++
+	}
+	println("TOTAL: " + totalFee.HumanString())
+	println("Min fee: " + minFee.HumanString())
+	println("NUMTXNS: ")
+	println(numTxns)
+	println(size)
+
+	block, err = tpt.miner.AddBlock()
+
+	size = 0
+	numTxns = 0
+	totalFee = types.ZeroCurrency
+	maxFee := types.ZeroCurrency
+	minFee = types.SiacoinPrecision.Mul64(10000000000) // All the fees are much smaller than this.
+	for _, tx := range block.Transactions {
+		for _, fee := range tx.MinerFees {
+			totalFee = totalFee.Add(fee)
+			if fee.Cmp(minFee) < 0 {
+				minFee = fee
+			}
+			if fee.Cmp(maxFee) > 0 {
+				maxFee = fee
+			}
+		}
+		size += tx.MarshalSiaSize()
+		numTxns++
+	}
+	println("\nTOTAL: " + totalFee.HumanString())
+	println("Min fee: " + minFee.HumanString())
+	println("Max fee: " + maxFee.HumanString())
+	println("NUMTXNS: ")
+	println(numTxns)
+	println(size)
+
+	block, err = tpt.miner.AddBlock()
+
+	size = 0
+	numTxns = 0
+	totalFee = types.ZeroCurrency
+	maxFee = types.ZeroCurrency
+	minFee = types.SiacoinPrecision.Mul64(10000000000) // All the fees are much smaller than this.
+	for _, tx := range block.Transactions {
+		for _, fee := range tx.MinerFees {
+			totalFee = totalFee.Add(fee)
+			if fee.Cmp(minFee) < 0 {
+				minFee = fee
+			}
+			if fee.Cmp(maxFee) > 0 {
+				maxFee = fee
+			}
+		}
+		size += tx.MarshalSiaSize()
+		numTxns++
+	}
+	println("\nTOTAL: " + totalFee.HumanString())
+	println("Min fee: " + minFee.HumanString())
+	println("Max fee: " + maxFee.HumanString())
+	println("NUMTXNS: ")
+	println(numTxns)
+	println(size)
+
+}
