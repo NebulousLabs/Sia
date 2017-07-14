6b53336e149b6099502e7fa891f04876f097235a
Rick  Duff <rgduff@gmail.com>
2017-07-14 17:10:24 -0500

    Reverted changes to update.go and accept.go as well as path changes in daemon.go

--- modules/transactionpool/accept.go
+++ modules/transactionpool/accept.go
@@ -320,6 +320,8 @@ func (tp *TransactionPool) acceptTransactionSet(ts []types.Transaction, txnFn fu
 // AcceptTransaction adds a transaction to the unconfirmed set of
 // transactions. If the transaction is accepted, it will be relayed to
 // connected peers.
+//
+// TODO: Break into component sets when the set gets accepted.
 func (tp *TransactionPool) AcceptTransactionSet(ts []types.Transaction) error {
 	// assert on consensus set to get special method
 	cs, ok := tp.consensusSet.(interface {
