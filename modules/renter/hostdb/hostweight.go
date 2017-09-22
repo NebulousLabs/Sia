9144f20926e65dc2a7411c64029a67b9ccd7249f
lukechampine <luke.champine@gmail.com>
2017-09-22 14:35:48 -0400

    decrement v1.3.0 weight

--- modules/renter/hostdb/hostweight.go
+++ modules/renter/hostdb/hostweight.go
@@ -237,7 +237,7 @@ func versionAdjustments(entry modules.HostDBEntry) float64 {
 		base = base * 0.99999 // Safety value to make sure we update the version penalties every time we update the host.
 	}
 	if build.VersionCmp(entry.Version, "1.3.1") < 0 {
-		base = base * 0.9
+		base = base * 0.7
 	}
 	if build.VersionCmp(entry.Version, "1.3.0") < 0 {
 		base = base * 0.9
