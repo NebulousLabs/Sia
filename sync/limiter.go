e7a1543262abc2af73ff3dec4abc5d1a2e1018d0
lukechampine <luke.champine@gmail.com>
2017-09-28 01:14:08 -0400

    first stab at limiter

---
+++ sync/limiter.go
@@ -0,0 +1,87 @@
+package sync
+
+import (
+	"sync"
+)
+
+// A Limiter restricts access to a resource.
+type Limiter struct {
+	limit    int
+	current  int
+	requests chan struct{}
+	cond     *sync.Cond
+}
+
+// Request blocks until n units are available. If n is greater than m's limit,
+// Request blocks until all of m's units have been released.
+//
+// Request is unbiased with respect to n: calls with small n do not starve
+// calls with large n.
+//
+// Request returns true if the request was canceled, and false otherwise.
+func (l *Limiter) Request(n int, cancel <-chan struct{}) bool {
+	// wait until our request is "first in line"
+	select {
+	case <-cancel:
+		return true
+	case token := <-l.requests:
+		// return token when we're done
+		defer func() { l.requests <- token }()
+	}
+
+	// spawn goroutine to handle cancellation
+	var cancelled bool
+	done := make(chan struct{})
+	defer close(done)
+	go func() {
+		select {
+		case <-cancel:
+			l.cond.L.Lock()
+			cancelled = true
+			l.cond.L.Unlock()
+			l.cond.Signal()
+		case <-done:
+		}
+	}()
+
+	// wait until request can be satisfied
+	l.cond.L.Lock()
+	for l.current+n > l.limit && l.current != 0 && !cancelled {
+		l.cond.Wait()
+	}
+	defer l.cond.L.Unlock()
+	if !cancelled {
+		l.current += n
+	}
+	return cancelled
+}
+
+// Release returns n units to l, making them available to future callers. It
+// is legal for n to be larger than l's limit, as long as n was previously
+// passed to Request.
+func (l *Limiter) Release(n int) {
+	l.cond.L.Lock()
+	l.current -= n
+	l.cond.L.Unlock()
+	l.cond.Signal()
+}
+
+// SetLimit sets the limit of l. It is legal to interpose calls to SetLimit
+// between Request/Release pairs.
+func (l *Limiter) SetLimit(limit int) {
+	l.cond.L.Lock()
+	l.limit = limit
+	l.cond.L.Unlock()
+	l.cond.Signal()
+}
+
+// NewLimiter returns a Limiter with the supplied limit.
+func NewLimiter(limit int) *Limiter {
+	l := &Limiter{
+		limit:    limit,
+		requests: make(chan struct{}, 1),
+		cond:     sync.NewCond(new(sync.Mutex)),
+	}
+	l.requests <- struct{}{}
+	return l
+}
