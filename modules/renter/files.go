cb1d32b34686401fb233169dfd9c5c7fcacf9e07
Thomas Bennett <tbenz9@gmail.com>
2017-09-29 14:50:44 -0700

    Delete .sia_temp files when .sia file is deleted. #1713
    * Delete .sia_temp files when .sia file is deleted.
    
    This PR fixes issue #1713.
    
    1: Delete the .sia_temp file when it's matching .sia file is deleted.
    2: Import the Sia/persist module to get the "tempSuffix" variable.

--- modules/renter/files.go
+++ modules/renter/files.go
@@ -9,6 +9,7 @@ import (
 	"github.com/NebulousLabs/Sia/build"
 	"github.com/NebulousLabs/Sia/crypto"
 	"github.com/NebulousLabs/Sia/modules"
+	"github.com/NebulousLabs/Sia/persist"
 	"github.com/NebulousLabs/Sia/types"
 )
 
@@ -190,6 +191,12 @@ func (r *Renter) DeleteFile(nickname string) error {
 	err := os.RemoveAll(filepath.Join(r.persistDir, f.name+ShareExtension))
 	if err != nil {
 		r.log.Println("WARN: couldn't remove .sia file during delete:", err)
+	}
+
+	// delete the temporary file
+	err := os.RemoveAll(filepath.Join(r.persistDir, f.name+ShareExtension+tempSuffix))
+	if err != nil {
+		r.log.Println("WARN: couldn't remove .sia_temp file during delete:", err)
 	}
 	r.saveSync()
 	r.mu.Unlock(lockID)
