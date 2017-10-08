9b3b99974bb3161bb7700de58a547891dce39fb4
Boris Nagaev <nagaev@google.com>
2017-10-08 04:58:56 +0200

    release.sh: build with -tags 'netgo'
    Sync it with what `make release-std` does.
    See https://github.com/NebulousLabs/Sia/pull/2357

--- release.sh
+++ release.sh
@@ -36,7 +36,7 @@ for os in darwin linux windows; do
 		if [ "$os" == "windows" ]; then
 			bin=${pkg}.exe
 		fi
-		GOOS=${os} go build -ldflags="-s -w" -o $folder/$bin ./$pkg
+		GOOS=${os} go build -tags 'netgo' -ldflags="-s -w" -o $folder/$bin ./$pkg
 		openssl dgst -sha256 -sign $keyfile -out $folder/${bin}.sig $folder/$bin
 		# verify signature
 		if [[ -n $pubkeyfile ]]; then
