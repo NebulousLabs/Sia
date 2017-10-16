6039f224843d377c63769fd6c3c38821106eb8e9
David Vorick <david@nebulouslabs.com>
2017-10-16 02:12:29 -0400

    cleanup for contractor uptime.go and update.go

--- modules/renter/contractor/update.go
+++ modules/renter/contractor/update.go
@@ -5,6 +5,46 @@ import (
 	"github.com/NebulousLabs/Sia/types"
 )
 
+// managedArchiveContracts will figure out which contracts are no longer needed
+// and move them to the historic set of contracts.
+//
+// TODO: This function should be performed by threadedContractMaintenance.
+// threadedContractMaintenance will currently quit if there are no hosts, but it
+// should at least run this code before quitting.
+func (c *Contractor) managedArchiveContracts() {
+	// Determine the current block height.
+	c.mu.RLock()
+	currentHeight := c.blockHeight
+	c.mu.RUnlock()
+
+	// Loop through the current set of contracts and migrate any expired ones to
+	// the set of old contracts.
+	for _, contract := range c.contracts.ViewAll() {
+		if currentHeight > contract.EndHeight() {
+			id := contract.ID
+			c.mu.Lock()
+			c.oldContracts[id] = contract
+			c.mu.Unlock()
+			if contract, ok := c.contracts.Acquire(id); ok {
+				c.contracts.Delete(contract)
+			}
+			c.log.Println("INFO: archived expired contract", id)
+		}
+	}
+
+	// Save.
+	//
+	// TODO: There's potentially an issue here in the future where the
+	// contractSet and the set of oldContracts could desync if there's a power
+	// outage between calling Delete and adding the contract to oldContracts. To
+	// prevent that potential inconsistency, when the contract persistence gets
+	// migreated to the contractSet, we'll need some strategy for guaranteeing
+	// that the set of oldContracts stays consistent.
+	c.mu.Lock()
+	c.save()
+	c.mu.Unlock()
+}
+
 // ProcessConsensusChange will be called by the consensus set every time there
 // is a change in the blockchain. Updates will always be called in order.
 func (c *Contractor) ProcessConsensusChange(cc modules.ConsensusChange) {
@@ -17,23 +57,6 @@ func (c *Contractor) ProcessConsensusChange(cc modules.ConsensusChange) {
 	for _, block := range cc.AppliedBlocks {
 		if block.ID() != types.GenesisID {
 			c.blockHeight++
-		}
-	}
-
-	// archive expired contracts
-	for _, contract := range c.contracts.ViewAll() {
-		if c.blockHeight > contract.EndHeight() {
-			id := contract.ID
-			// Move the contract to oldContracts. No need to wait for extra
-			// confirmations - any processes which depend on this contract
-			// should have taken care of any issues already.
-			c.oldContracts[id] = contract
-			// TODO: this pattern may be tricky. We are potentially acquiring
-			// multiple contracts while holding the contractor lock.
-			if contract, ok := c.contracts.Acquire(id); ok {
-				c.contracts.Delete(contract)
-			}
-			c.log.Println("INFO: archived expired contract", id)
 		}
 	}
 
@@ -57,11 +80,11 @@ func (c *Contractor) ProcessConsensusChange(cc modules.ConsensusChange) {
 	}
 	c.mu.Unlock()
 
-	// Only attempt contract formation/renewal if we are synced
-	// (harmless if not synced, since hosts will reject our renewal attempts,
-	// but very slow).
+	// Perform contract maintenance if our blockchain is synced. Use a separate
+	// goroutine so that the rest of the contractor is not blocked during
+	// maintenance.
 	if cc.Synced {
-		// Perform the contract maintenance in a separate thread.
 		go c.threadedContractMaintenance()
+		go c.managedArchiveContracts()
 	}
 }
