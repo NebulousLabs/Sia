f6abcf21186ce98e158a27bd85c0c0d68628ffcf
dnetguru <farzade@gmail.com>
2017-12-10 18:42:22 -0800

    Added debug messages for sessionHeader netAddress.

--- modules/gateway/peers.go
+++ modules/gateway/peers.go
@@ -170,6 +170,7 @@ func acceptableSessionHeader(ourHeader, remoteHeader sessionHeader, remoteAddr s
 // The requesting peer is added as a node and a peer. The peer is only added if
 // a nil error is returned.
 func (g *Gateway) managedAcceptConnv130Peer(conn net.Conn, remoteVersion string) error {
+	g.log.Debugln("Sending sessionHeader with address", g.myAddr, g.myAddr.IsLocal())
 	// Perform header handshake.
 	ourHeader := sessionHeader{
 		GenesisID:  types.GenesisID,
@@ -435,6 +436,7 @@ func exchangeRemoteHeader(conn net.Conn, ourHeader sessionHeader) (sessionHeader
 // managedConnectv130Peer connects to peers >= v1.3.0. The peer is added as a
 // node and a peer. The peer is only added if a nil error is returned.
 func (g *Gateway) managedConnectv130Peer(conn net.Conn, remoteVersion string, remoteAddr modules.NetAddress) error {
+	g.log.Debugln("Sending sessionHeader with address", g.myAddr, g.myAddr.IsLocal())
 	// Perform header handshake.
 	ourHeader := sessionHeader{
 		GenesisID:  types.GenesisID,
