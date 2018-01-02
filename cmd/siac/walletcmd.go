139894c970ad32c7710837d5fe6f31f36e6c42f4
Christopher Schinnerl <chris@sia.tech>
2018-01-02 12:12:35 -0500

    fix build

--- cmd/siac/walletcmd.go
+++ cmd/siac/walletcmd.go
@@ -180,7 +180,7 @@ const newPasswordText = "New Password: "
 // passwordPrompt securely reads a password from stdin.
 func passwordPrompt(prompt string) (string, error) {
 	fmt.Print(prompt)
-	pw, err := terminal.ReadPassword(syscall.Stdin)
+	pw, err := terminal.ReadPassword(int(syscall.Stdin))
 	fmt.Println()
 	return string(pw), err
 }
