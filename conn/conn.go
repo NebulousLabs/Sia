92ed97f07bdc32da978b7c3b72dc7870716fabd8
Christopher Schinnerl <chris@sia.tech>
2018-02-27 12:30:57 -0500

    simplify code

--- conn/conn.go
+++ conn/conn.go
@@ -10,36 +10,27 @@ type (
 	// the user to limit the connection throughput.
 	RLConnection struct {
 		conn             net.Conn  // underlying net.Conn
-		packetSize       uint64    // size of single packet in bytes
-		packetsPerSecond uint64    // packets that are sent per second
-		writtenBytes     uint64    // number of bytes that have been written to the latest packet
+		packetSize       int64     // size of single packet in bytes
+		packetsPerSecond int64     // packets that are sent per second
+		writtenBytes     int64     // number of bytes that have been written to the latest packet
 		lastWrite        time.Time // time when last packet was started
 	}
 )
 
 // Dial creates a RateLimitedDialer.
-func Dial(dialer *net.Dialer, network string, address string) (*RLConnection, error) {
-	conn, err := dialer.Dial(network, address)
-	if err != nil {
-		return nil, err
-	}
+func newRLConn(conn net.Conn, packetSize int64, packetsPerSecond int64) net.Conn {
 	rlc := &RLConnection{
-		conn: conn,
+		conn:             conn,
+		packetSize:       packetSize,
+		packetsPerSecond: packetsPerSecond,
+		writtenBytes:     packetSize,
 	}
-	return rlc, nil
+	return rlc
 }
 
 // Read calls the underlying connection's Read method.
 func (rlc *RLConnection) Read(b []byte) (n int, err error) {
 	return rlc.conn.Read(b)
-}
-
-// SetLimit sets the rate limit of the connection.
-func (rlc *RLConnection) SetLimit(packetSize uint64, packetsPerSecond uint64) {
-	rlc.packetSize = packetSize
-	rlc.packetsPerSecond = packetsPerSecond
-	rlc.lastWrite = time.Now()
-	rlc.writtenBytes = rlc.packetSize
 }
 
 // Write writes data to the underlying connection without exceeding the rate
@@ -50,52 +41,30 @@ func (rlc *RLConnection) Write(b []byte) (n int, err error) {
 		return rlc.conn.Write(b)
 	}
 
-	// Start a feeder thread that feeds us the data at the right speed.
-	packets := make(chan []byte)
-	go func() {
-		defer close(packets)
-		if rlc.writtenBytes < rlc.packetSize {
-			// We have previously started writing a packet that has some space
-			// left. We can write right away.
-			remainingBytes := rlc.packetSize - rlc.writtenBytes
+	for len(b) > 0 {
+		// Check if we need to sleep
+		if rlc.writtenBytes >= rlc.packetSize {
+			time.Sleep(time.Second / time.Duration(rlc.packetsPerSecond))
+			rlc.writtenBytes = 0
+		}
+		// Write data
+		writableBytes := rlc.packetSize - rlc.writtenBytes
+		var written int
+		if int64(len(b)) <= writableBytes {
+			written, err = rlc.conn.Write(b)
+			b = b[:0]
+			rlc.writtenBytes += int64(len(b))
+		} else {
+			written, err = rlc.conn.Write(b[:writableBytes])
+			b = b[writableBytes:]
 			rlc.writtenBytes = rlc.packetSize
-			if uint64(len(b)) <= remainingBytes {
-				packets <- b
-				return
-			}
-			packets <- b[:remainingBytes]
-			b = b[remainingBytes:]
 		}
-		// Write the remaining data packet by packet.
-		timer := time.NewTimer(time.Until(rlc.lastWrite.Add(time.Second)))
-		for len(b) > 0 {
-			// Wait long enough
-			select {
-			case <-timer.C:
-			}
-			// Pass the data on to the writing thread
-			if uint64(len(b)) <= rlc.packetSize {
-				packets <- b
-				b = b[:0]
-				rlc.writtenBytes = uint64(len(b))
-			} else {
-				packets <- b[:rlc.packetSize]
-				b = b[rlc.packetSize:]
-				rlc.writtenBytes = rlc.packetSize
-			}
-			// Remember the last write's timestamp
-			rlc.lastWrite = time.Now()
-			timer.Reset(time.Second)
-		}
-	}()
-
-	// Write data one packet at a time.
-	for packet := range packets {
-		written, err := rlc.conn.Write(packet)
 		n += written
 		if err != nil {
-			return 0, err
+			return
 		}
+		// Remember the last write's timestamp
+		rlc.lastWrite = time.Now()
 	}
 	return
 }
--- conn/conn_test.go
+++ conn/conn_test.go
@@ -11,11 +11,14 @@ import (
 
 // TestRLConnectionSingleWrite tests a single write to a RLConnection.
 func TestRLConnectionSingleWrite(t *testing.T) {
-	// Create server side
+	// Create server
 	ln, err := net.Listen("tcp", ":1234")
-	dataLen := 1000
-	data := fastrand.Bytes(dataLen)
+	kill := make(chan struct{})
 	wait := make(chan struct{})
+	defer func() {
+		defer close(kill)
+		<-wait
+	}()
 	go func() {
 		defer close(wait)
 		server, err := ln.Accept()
@@ -23,42 +26,60 @@ func TestRLConnectionSingleWrite(t *testing.T) {
 			t.Fatal(err)
 		}
 		defer server.Close()
-		readBytes := 0
-		buf := make([]byte, dataLen)
-		for readBytes < dataLen {
-			n, err := io.ReadFull(server, buf)
-			if err != nil {
+		buf := make([]byte, 100)
+		for {
+			select {
+			case <-kill:
+				break
+			default:
+			}
+			_, err := server.Read(buf)
+			if err != nil && err != io.EOF {
 				t.Fatal(err)
 			}
-			readBytes += n
+			if err == io.EOF {
+				break
+			}
 		}
 	}()
 
-	client, err := Dial(&net.Dialer{}, "tcp", ":1234")
+	// Create client
+	client, err := (&net.Dialer{}).Dial("tcp", ":1234")
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer client.Close()
 
+	// Run tests
+	tests := []func(client net.Conn, t *testing.T){
+		testSingleWrite,
+	}
+	for _, test := range tests {
+		test(client, t)
+	}
+}
+
+// testSingleWrite tests if a single rate-limited write works as expected.
+func testSingleWrite(conn net.Conn, t *testing.T) {
 	// Set limit
-	packetSize := uint64(250)
-	packetsPerSecond := uint64(1)
-	client.SetLimit(packetSize, packetsPerSecond)
+	packetSize := int64(250)
+	packetsPerSecond := int64(4)
+	client := newRLConn(conn, packetSize, packetsPerSecond)
+
+	// Create data to send.
+	dataLen := 1000
+	data := fastrand.Bytes(dataLen)
 
 	// Write data and time how long it takes.
 	start := time.Now()
-	_, err = client.Write(data)
+	_, err := client.Write(data)
 	if err != nil {
 		t.Error(err)
 	}
 	d := time.Since(start)
-
 	// It should have taken at least dataLen / (packetSize * packetsPerSecond)
 	// seconds.
 	if d.Seconds() < float64(dataLen)/float64(packetSize*packetsPerSecond) {
-		t.Errorf("Transmission finished too soon. %v seconds.", d.Seconds())
+		t.Fatalf("Transmission finished too soon. %v seconds.", d.Seconds())
 	}
-
-	// Wait for server to stop
-	<-wait
 }
