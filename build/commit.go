c0b1e64ede7712ce6bc615f119dc51866d2da25e
Thomas Bennett <tbenz9@gmail.com>
2018-03-09 15:00:52 -0800

    fix golint error

--- build/commit.go
+++ build/commit.go
@@ -2,5 +2,8 @@ package build
 
 // GitRevision and BuildTime get assigned via the Makefile when built.
 var (
-	GitRevision, BuildTime string
+	// GitRevision is the git commit hash used when built
+	GitRevision string
+	// BuildTime is the date and time the build was completed
+	BuildTime string
 )
