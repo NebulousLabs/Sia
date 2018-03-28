84b55812aedc6cbbcff051265b03606c8414bc84
Christopher Schinnerl <chris@sia.tech>
2018-03-28 17:04:33 -0400

    fix build

--- node/api/client/consensus.go
+++ node/api/client/consensus.go
@@ -15,12 +15,12 @@ func (c *Client) ConsensusGet() (cg api.ConsensusGET, err error) {
 
 // ConsensusBlocksIDGet requests the /consensus/blocks api resource
 func (c *Client) ConsensusBlocksIDGet(id types.BlockID) (block types.Block, err error) {
-	err = c.Get("/consensus/blocks?id="+id.String(), &block)
+	err = c.get("/consensus/blocks?id="+id.String(), &block)
 	return
 }
 
 // ConsensusBlocksHeightGet requests the /consensus/blocks api resource
 func (c *Client) ConsensusBlocksHeightGet(height types.BlockHeight) (block types.Block, err error) {
-	err = c.Get("/consensus/blocks?height="+fmt.Sprint(height), &block)
+	err = c.get("/consensus/blocks?height="+fmt.Sprint(height), &block)
 	return
 }
