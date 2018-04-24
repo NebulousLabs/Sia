50c8410fb665ec730d7fd243a77ed10cd7bfdb4f
Christopher Schinnerl <chris@sia.tech>
2018-04-24 15:22:54 -0400

    tmp

--- modules/renter/proto/renew.go
+++ modules/renter/proto/renew.go
@@ -258,11 +258,6 @@ func (cs *ContractSet) Renew(oldContract *SafeContract, params ContractParams, t
 	txn, parentTxns = txnBuilder.View()
 	txnSet = append(parentTxns, txn)
 
-	// Disrupt here before broadcasting and adding the contract to the set.
-	if cs.deps.Disrupt("RenewCrashBeforeBroadcast") {
-		return modules.RenterContract{}, errors.New("RenewCrashBeforeBroadcast disrupt")
-	}
-
 	// Submit to blockchain.
 	err = tpool.AcceptTransactionSet(txnSet)
 	if err == modules.ErrDuplicateTransactionSet {
