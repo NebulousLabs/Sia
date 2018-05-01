1718f70d2a1064554387146714359b1e9e6019d8
Christopher Schinnerl <chris@sia.tech>
2018-05-01 17:28:32 -0400

    add tpool/raw endpoint to client package and add siatest/wallet to Makefile

--- node/api/client/transactionpool.go
+++ node/api/client/transactionpool.go
@@ -1,9 +1,25 @@
 package client
 
-import "github.com/NebulousLabs/Sia/node/api"
+import (
+	"net/url"
+
+	"github.com/NebulousLabs/Sia/encoding"
+	"github.com/NebulousLabs/Sia/node/api"
+	"github.com/NebulousLabs/Sia/types"
+)
 
 // TransactionPoolFeeGet uses the /tpool/fee endpoint to get a fee estimation.
 func (c *Client) TransactionPoolFeeGet() (tfg api.TpoolFeeGET, err error) {
 	err = c.get("/tpool/fee", &tfg)
+	return
+}
+
+// TransactionPoolRawPost uses the /tpool/raw endpoint to send a raw
+// transaction to the transaction pool.
+func (c *Client) TransactionPoolRawPost(txn types.Transaction, parents types.Transaction) (err error) {
+	values := url.Values{}
+	values.Set("transaction", string(encoding.Marshal(txn)))
+	values.Set("parents", string(encoding.Marshal(parents)))
+	err = c.post("/tpool/raw", values.Encode(), nil)
 	return
 }
