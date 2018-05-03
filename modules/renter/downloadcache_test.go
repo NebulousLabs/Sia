c5cfd642028c543eeea58c7a630e5f1f1c40d1bb
MSevey <mjsevey@gmail.com>
2018-05-03 15:31:19 -0400

    update TestAddChunkToCache to fix failing test.  Test-long now passing as well

--- modules/renter/downloadcache_test.go
+++ modules/renter/downloadcache_test.go
@@ -1,41 +1,38 @@
 package renter
 
 import (
+	"strconv"
+	"sync"
 	"testing"
 	"time"
 )
 
 // TestAddChunkToCache tests that the oldest chunk is removed
 func TestAddChunkToCache(t *testing.T) {
-	var udc unfinishedDownloadChunk
-	udc.download = &download{
-		staticDestinationType: "httpseekstream",
+	udc := &unfinishedDownloadChunk{
+		download: &download{
+			staticDestinationType: "httpseekstream",
+		},
+		chunkCache: make(map[string]*cacheData),
+		cacheMu:    new(sync.Mutex),
 	}
 
 	data := []byte{1, 2, 3, 4}
 
 	// Fill Cache
 	for i := 0; i < downloadCacheSize; i++ {
+		udc.staticCacheID = strconv.Itoa(i)
 		udc.addChunkToCache(data)
 		time.Sleep(1 * time.Millisecond)
 	}
 
-	// Get oldest key
-	var oldestKey string
-	oldestTime := time.Now()
-
-	for id, chunk := range udc.chunkCache {
-		if chunk.lastAccess.Before(oldestTime) {
-			oldestTime = chunk.lastAccess
-			oldestKey = id
-		}
-	}
-
 	// Add additional chunk to force deletion of a chunk
+	udc.staticCacheID = strconv.Itoa(downloadCacheSize)
 	udc.addChunkToCache(data)
 
-	// check if the chunk with the oldestKey was removed
-	if _, ok := udc.chunkCache[oldestKey]; ok {
+	// check if the chunk with staticCacheID = "0" was removed
+	// as that would have been the first to be added
+	if _, ok := udc.chunkCache["0"]; ok {
 		t.Errorf("Expected ok to be false instead it was %v", ok)
 	}
 
