1ee9da0b5728309091cc1c162aea25f77c91635f
MSevey <mjsevey@gmail.com>
2018-05-03 13:52:45 -0400

    TestAddChunkToCache is now passing

--- modules/renter/downloadcache_test.go
+++ modules/renter/downloadcache_test.go
@@ -5,11 +5,15 @@ import (
 	"time"
 )
 
-// Stream uses the streaming endpoint to download a file.
+// TestAddChunkToCache tests that the oldest chunk is removed
 func TestAddChunkToCache(t *testing.T) {
-	var udc *unfinishedDownloadChunk
+	var udc unfinishedDownloadChunk
+	udc.download = &download{
+		staticDestinationType: "httpseekstream",
+	}
 
 	data := []byte{1, 2, 3, 4}
+
 	// Fill Cache
 	for i := 0; i < downloadCacheSize; i++ {
 		udc.addChunkToCache(data)
@@ -27,9 +31,10 @@ func TestAddChunkToCache(t *testing.T) {
 		}
 	}
 
+	// Add additional chunk to force deletion of a chunk
 	udc.addChunkToCache(data)
 
-	// check if the oldestKey was removed
+	// check if the chunk with the oldestKey was removed
 	if _, ok := udc.chunkCache[oldestKey]; ok {
 		t.Errorf("Expected ok to be false instead it was %v", ok)
 	}
