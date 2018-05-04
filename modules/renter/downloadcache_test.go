d1694d9247774883ad0782cbf4e13c538b60ad5c
MSevey <mjsevey@gmail.com>
2018-05-04 13:35:27 -0400

    resolve PR edits from chris

--- modules/renter/downloadcache_test.go
+++ modules/renter/downloadcache_test.go
@@ -11,29 +11,26 @@ import (
 func TestAddChunkToCache(t *testing.T) {
 	udc := &unfinishedDownloadChunk{
 		download: &download{
-			staticDestinationType: "httpseekstream",
+			staticDestinationType: destinationTypeSeekStream,
 		},
 		chunkCache: make(map[string]*cacheData),
 		cacheMu:    new(sync.Mutex),
 	}
 
-	data := []byte{1, 2, 3, 4}
-
 	// Fill Cache
 	for i := 0; i < downloadCacheSize; i++ {
 		udc.staticCacheID = strconv.Itoa(i)
-		udc.addChunkToCache(data)
+		udc.addChunkToCache([]byte{})
 		time.Sleep(1 * time.Millisecond)
 	}
 
 	// Add additional chunk to force deletion of a chunk
 	udc.staticCacheID = strconv.Itoa(downloadCacheSize)
-	udc.addChunkToCache(data)
+	udc.addChunkToCache([]byte{})
 
 	// check if the chunk with staticCacheID = "0" was removed
 	// as that would have been the first to be added
 	if _, ok := udc.chunkCache["0"]; ok {
-		t.Errorf("Expected ok to be false instead it was %v", ok)
+		t.Error("The least recently accessed chunk wasn't pruned from the cache")
 	}
-
 }
