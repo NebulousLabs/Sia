63ae081b7579bdcabecbc771c661dfc3eef3205b
Christopher Schinnerl <chris@sia.tech>
2018-05-30 18:20:32 -0400

    Fix storage spending reporting after renewing contracts

--- modules/renter/proto/renew.go
+++ modules/renter/proto/renew.go
@@ -274,13 +274,14 @@ func (cs *ContractSet) Renew(oldContract *SafeContract, params ContractParams, t
 
 	// Construct contract header.
 	header := contractHeader{
-		Transaction: revisionTxn,
-		SecretKey:   ourSK,
-		StartHeight: startHeight,
-		TotalCost:   funding,
-		ContractFee: host.ContractPrice,
-		TxnFee:      txnFee,
-		SiafundFee:  types.Tax(startHeight, fc.Payout),
+		Transaction:     revisionTxn,
+		SecretKey:       ourSK,
+		StartHeight:     startHeight,
+		TotalCost:       funding,
+		ContractFee:     host.ContractPrice,
+		TxnFee:          txnFee,
+		SiafundFee:      types.Tax(startHeight, fc.Payout),
+		StorageSpending: basePrice,
 	}
 
 	// Get old roots
