cd48856c953a05f46e96b2ba3c0761c5e1093dc7
Matthew Sevey <mjsevey@gmail.com>
2018-06-19 15:50:51 -0400

    Address Comments on PR

--- modules/renter/uploadheap.go
+++ modules/renter/uploadheap.go
@@ -237,38 +237,44 @@ func (r *Renter) buildUnfinishedChunks(f *file, hosts map[string]struct{}) []*un
 func (r *Renter) managedBuildChunkHeap(hosts map[string]struct{}) {
 	// Loop through the whole set of files and get a list of chunks to add to
 	// the heap.
-	id := r.mu.Lock()
+	id := r.mu.RLock()
+	goodForRenew := make(map[types.FileContractID]bool)
+	offline := make(map[types.FileContractID]bool)
 	for _, file := range r.files {
-		// check for local file
-		tf, exists := r.persist.Tracking[file.name]
-		if exists {
-			// Build maps that map contract id to its offline and goodForRenew
-			// status
-			contractIDs := make(map[types.FileContractID]struct{})
-			for cid := range file.contracts {
-				contractIDs[cid] = struct{}{}
-			}
-			goodForRenew := make(map[types.FileContractID]bool)
-			offline := make(map[types.FileContractID]bool)
-			for cid := range contractIDs {
-				resolvedID := r.hostContractor.ResolveID(cid)
-				cu, ok := r.hostContractor.ContractUtility(resolvedID)
-				goodForRenew[cid] = ok && cu.GoodForRenew
-				offline[cid] = r.hostContractor.IsOffline(resolvedID)
-			}
-			// Check if local file is missing and redundancy is less than 1
-			// log warning to renter log
-			if _, err := os.Stat(tf.RepairPath); os.IsNotExist(err) && file.redundancy(offline, goodForRenew) < 1 {
-				r.log.Println("File not found on disk:", tf.RepairPath)
-			}
+		file.mu.RLock()
+		// Build maps that map contract id to its offline and goodForRenew
+		// status
+		contractIDs := make(map[types.FileContractID]struct{})
+		for cid := range file.contracts {
+			contractIDs[cid] = struct{}{}
 		}
+		for cid := range contractIDs {
+			resolvedID := r.hostContractor.ResolveIDToPubKey(cid)
+			cu, ok := r.hostContractor.ContractUtility(resolvedID)
+			goodForRenew[cid] = ok && cu.GoodForRenew
+			offline[cid] = r.hostContractor.IsOffline(resolvedID)
+		}
+		file.mu.RUnlock()
 
 		unfinishedUploadChunks := r.buildUnfinishedChunks(file, hosts)
 		for i := 0; i < len(unfinishedUploadChunks); i++ {
 			r.uploadHeap.managedPush(unfinishedUploadChunks[i])
 		}
 	}
-	r.mu.Unlock(id)
+	for _, file := range r.files {
+		file.mu.RLock()
+		// check for local file
+		tf, exists := r.persist.Tracking[file.name]
+		if exists {
+			// Check if local file is missing and redundancy is less than 1
+			// log warning to renter log
+			if _, err := os.Stat(tf.RepairPath); os.IsNotExist(err) && file.redundancy(offline, goodForRenew) < 1 {
+				r.log.Println("File not found on disk and possibly unrecoverable:", tf.RepairPath)
+			}
+		}
+		file.mu.RUnlock()
+	}
+	r.mu.RUnlock(id)
 }
 
 // managedPrepareNextChunk takes the next chunk from the chunk heap and prepares
